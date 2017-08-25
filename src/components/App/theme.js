3a92aa5644e718cc69b6d2101163438d9fb45fd1
Ivo Paunov <ivo.paunov@gmail.com>
2017-08-25 18:20:31 +0300

    add files for react-toolbox global theme

--- src/components/App/theme.js
+++ src/components/App/theme.js
@@ -1,5 +1,57 @@
+import RTAppBar from './../../styles/RTAppBar.css';
+import RTAutocomplete from './../../styles/RTAutocomplete.css';
+import RTAvatar from './../../styles/RTAvatar.css';
+import RTCard from './../../styles/RTCard.css';
+import RTChip from './../../styles/RTChip.css';
+import RTCheckbox from './../../styles/RTCheckbox.css';
+import RTDatePicker from './../../styles/RTDatePicker.css';
+import RTDialog from './../../styles/RTDialog.css';
+import RTDrawer from './../../styles/RTDrawer.css';
+import RTDropdown from './../../styles/RTDropdown.css';
+import RTInput from './../../styles/RTInput.css';
+import RTLayout from './../../styles/RTLayout.css';
+import RTLink from './../../styles/RTLink.css';
+import RTList from './../../styles/RTList.css';
 import RTMenu from './../../styles/RTMenu.css';
+import RTNavigation from './../../styles/RTNavigation.css';
+import RTOverlay from './../../styles/RTOverlay.css';
+import RTProgressBar from './../../styles/RTProgressBar.css';
+import RTRadio from './../../styles/RTRadio.css';
+import RTRipple from './../../styles/RTRipple.css';
+import RTSlider from './../../styles/RTSlider.css';
+import RTSnackbar from './../../styles/RTSnackbar.css';
+import RTSwitch from './../../styles/RTSwitch.css';
+import RTTable from './../../styles/RTTable.css';
+import RTTabs from './../../styles/RTTabs.css';
+import RTTooltip from './../../styles/RTTooltip.css';
+import RTTimePicker from './../../styles/RTTimePicker.css';
  
 export default {
-    RTMenu: RTMenu
+    RTAppBar: RTAppBar,
+    RTAutocomplete: RTAutocomplete,
+    RTAvatar: RTAvatar,
+    RTCard: RTCard,
+    RTChip: RTChip,
+    RTCheckbox: RTCheckbox,
+    RTDatePicker: RTDatePicker,
+    RTDialog: RTDialog,
+    RTDrawer: RTDrawer,
+    RTDropdown: RTDropdown,
+    RTInput: RTInput,
+    RTLayout: RTLayout,
+    RTLink: RTLink,
+    RTList: RTList,
+    RTMenu: RTMenu,
+    RTNavigation: RTNavigation,
+    RTOverlay: RTOverlay,
+    RTProgressBar: RTProgressBar,
+    RTRadio: RTRadio,
+    RTRipple: RTRipple,
+    RTSlider: RTSlider,
+    RTSnackbar: RTSnackbar,
+    RTSwitch: RTSwitch,
+    RTTable: RTTable,
+    RTTabs: RTTabs,
+    RTTooltip: RTTooltip,
+    RTTimePicker: RTTimePicker,
 };
