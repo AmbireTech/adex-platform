f43886b97a72f64f7ab28c3191dedc53562508f3
Ivo paunov <ivo.paunov@gmail.com>
2018-03-16 11:38:55 +0200

    work on image cropping

--- src/services/images/crop.js
+++ src/services/images/crop.js
@@ -1,35 +1,52 @@
 /**
- * @param {File} image - Image File Object
+ * @param {String} objUrl - Image blob url URL.createObjectURL
  * @param {Object} pixelCrop - pixelCrop Object provided by react-image-crop
  * @param {String} fileName - Name of the returned file in Promise
  */
-export const getCroppedImg = (image, pixelCrop, fileName) => {
-    
+export const getCroppedImgUrl = (objUrl, pixelCrop, fileName) => {
     const canvas = document.createElement('canvas')
-    canvas.width = pixelCrop.width
-    canvas.height = pixelCrop.height
-    const ctx = canvas.getContext('2d')
-  
-    ctx.drawImage(
-      image,
-      pixelCrop.x,
-      pixelCrop.y,
-      pixelCrop.width,
-      pixelCrop.height,
-      0,
-      0,
-      pixelCrop.width,
-      pixelCrop.height
-    )
-  
-    // As Base64 string
-    // const base64Image = canvas.toDataURL('image/jpeg');
+
+    let img = new Image()
+    img.src = objUrl
   
     // As a blob
     return new Promise((resolve, reject) => {
-      canvas.toBlob(file => {
-        file.name = fileName
-        resolve(file)
-      }, 'image/jpeg')
+        img.onload = () => {
+            let width = img.width
+            let height = img.height
+
+            // pixelCrop come in %
+            let crop = {
+                x: width * (pixelCrop.x / 100),
+                y: height * (pixelCrop.y / 100),
+                width: width * (pixelCrop.width / 100),
+                height: height * (pixelCrop.height / 100),
+            }
+
+            canvas.width = crop.width
+            canvas.height = crop.height
+            const ctx = canvas.getContext('2d')        
+
+            // console.log('crop', crop)
+            // console.log('width', width)
+            // console.log('height', height)
+
+            ctx.drawImage(
+                img,
+                crop.x,
+                crop.y,
+                crop.width,
+                crop.height,
+                0,
+                0,
+                crop.width,
+                crop.height
+              )
+
+              canvas.toBlob(file => {
+                file.name = fileName
+                resolve(URL.createObjectURL(file))
+              }, 'image/jpeg')
+        }      
     })
   }
