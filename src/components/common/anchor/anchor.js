feeee5b5ccc4ce1d1d96dbcdd8ae4ccb743ac211
Ivo paunov <ivo.paunov@gmail.com>
2018-03-19 16:52:29 +0200

    fix anchor tatget

--- src/components/common/anchor/anchor.js
+++ src/components/common/anchor/anchor.js
@@ -14,6 +14,7 @@ const Anchor = ({href, target, children, ...rest}) => {
             draggable='false'
             rel='noopener noreferrer'
             {...rest}
+            target={target}
             href={url}
         >
             { children }
