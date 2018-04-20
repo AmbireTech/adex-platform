0197f2a4fe3186da439d04e1a3996af440993bc3
Ivo paunov <ivo.paunov@gmail.com>
2018-04-20 14:07:42 +0300

    use common components for unit bids

--- src/components/dashboard/containers/UnitBids.js
+++ src/components/dashboard/containers/UnitBids.js
@@ -5,70 +5,21 @@ import { bindActionCreators } from 'redux'
 import actions from 'actions'
 import theme from './theme.css'
 import { Tab, Tabs } from 'react-toolbox'
-import { TableHead, TableRow, TableCell } from 'react-toolbox/lib/table'
 import { IconButton } from 'react-toolbox/lib/button'
 import ItemsList from './ItemsList'
 import Rows from 'components/dashboard/collection/Rows'
 import Translate from 'components/translate/Translate'
 import Tooltip from 'react-toolbox/lib/tooltip'
 import RTButtonTheme from 'styles/RTButton.css'
-import { adxToFloatView } from 'services/smart-contracts/utils'
-import { Item } from 'adex-models'
 import { exchange as ExchangeConstants } from 'adex-constants'
 import { CancelBid, VerifyBid, RefundBid } from 'components/dashboard/forms/web3/transactions'
 import classnames from 'classnames'
-import moment from 'moment'
 import Anchor from 'components/common/anchor/anchor'
 import { SORT_PROPERTIES_BIDS, FILTER_PROPERTIES_BIDS } from 'constants/misc'
-import { PropRow, ContentBox, ContentBody } from 'components/common/dialog/content'
-import WithDialog from 'components/common/dialog/WithDialog'
-import { FontIcon } from 'react-toolbox/lib/font_icon'
+import { getCommonBidData, renderCommonTableRow, renderTableHead, searchMatch } from './BidsCommon'
 
 const TooltipIconButton = Tooltip(IconButton)
-const { BID_STATES, BidStatesLabels } = ExchangeConstants
-
-// TODO: use colors with css 
-const StateIcons = {
-    [BID_STATES.DoesNotExist.id]: { icon: 'more_horiz', color: '#0277BD' },
-    [BID_STATES.Accepted.id]: { icon: 'done', color: '#0277BD' },
-    [BID_STATES.Canceled.id]: { icon: 'close', color: '#787878' },
-    [BID_STATES.Expired.id]: { icon: 'access_time', color: '#FF5722' },
-    [BID_STATES.Completed.id]: { icon: 'done_all', color: '#00ffbf' },
-    [BID_STATES.ConfirmedAdv.id]: { icon: 'done', color: '#00E5FF' },
-    [BID_STATES.ConfirmedPub.id]: { icon: 'done', color: '#00E5FF' },
-}
-
-const bidDetails = ({ bidData, t }) => {
-    return (
-        <ContentBox>
-            <ContentBody>
-                <PropRow left={t('BID_ID')} right={bidData._id} />
-                <PropRow left={t('BID_AMOUNT')} right={bidData._amount} />
-                <PropRow left={t('BID_TARGET')} right={bidData._target} />
-                <PropRow left={t('BID_UNIQUE_CLICKS')} right={bidData.clicksCount} />
-                <PropRow left={t('BID_STATE')} right={bidData._state} />
-                <PropRow left={t('PUBLISHER')} right={bidData._publisher} />
-                <PropRow left={t('AD_SLOT')} right={bidData._adSlot} />
-                <PropRow left={t('TIMEOUT')} right={bidData.timeout} />
-                <PropRow left={t('ACCEPTED')} right={bidData.accepted} />
-                <PropRow left={t('EXPIRES')} right={bidData.bidExpires} />
-                <PropRow left={t('REPORT_ADVERTISER')} right={bidData._advertiserConfirmation} />
-                <PropRow left={t('REPORT_PUBLISHER')} right={bidData._publisherConfirmation} />
-                <PropRow left={t('')}
-                    right={
-
-                        <div>
-                            {bidData.cancelBtn}
-                            {bidData.pendingAcceptByPub}
-                            {bidData.verifyBtn}
-                            {bidData.refundBtn}
-                        </div>} />
-            </ContentBody>
-        </ContentBox>
-    )
-}
-
-const BidDetailWithDialog = WithDialog(bidDetails)
+const { BID_STATES } = ExchangeConstants
 
 export class UnitBids extends Component {
     constructor(props) {
@@ -92,23 +43,6 @@ export class UnitBids extends Component {
         this.props.getUnitBids()
     }
 
-    renderTableHead = () => {
-        let t = this.props.t
-        return (
-            <TableHead>
-                <TableCell> {t('DETAILS')} </TableCell>
-                <TableCell> {t('BID_AMOUNT')} </TableCell>
-                <TableCell> {t('BID_TARGET')} / {t('BID_UNIQUE_CLICKS')} </TableCell>
-                <TableCell> {t('BID_STATE')} </TableCell>
-                <TableCell> {t('PUBLISHER')} </TableCell>
-                <TableCell> {t('AD_SLOT')} </TableCell>
-                <TableCell> {t('TIMEOUT')} / {t('ACCEPTED')} / {t('EXPIRES')}  </TableCell>
-                <TableCell> {t('REPORTS')}  </TableCell>
-                <TableCell> {t('ACTIONS')} </TableCell>
-            </TableHead>
-        )
-    }
-
     getBidData = (bid) => {
         const t = this.props.t
         const transactions = this.props.transactions
@@ -127,67 +61,54 @@ export class UnitBids extends Component {
         const pendingVerify = (pendingState === BID_STATES.ConfirmedAdv.id) || (bid.unconfirmedStateId === BID_STATES.Completed.id)
         const pendingAcceptByPub = bid.unconfirmedStateId === BID_STATES.Accepted.id
 
-        const bidData = {
-            _id: bid._id || '-',
-            _amount: adxToFloatView(bid._amount) + ' ADX',
-            _target: bid._target,
-            clicksCount: bid.clicksCount || '-',
-            _state:
-                <span className={theme.bidState}>
-                    <FontIcon value={StateIcons[bid._state].icon} style={{ marginRight: 5, color: StateIcons[bid._state].color }} />
-                    <span>{t(BidStatesLabels[bid._state])}</span>
-                </span>,
-            _publisher: <Anchor target='_blank' href={process.env.ETH_SCAN_ADDR_HOST + bid._publisher} > {bid._publisher || '-'} </Anchor>,
-            _adSlot: <Anchor target='_blank' href={Item.getIpfsMetaUrl(bid._adSlot, process.env.IPFS_GATEWAY)} > {bid._adSlot || '-'} </Anchor>,
-            timeout: moment.duration(timeout, 'ms').humanize(),
-            accepted: accepted ? moment(accepted).format('MMMM Do, YYYY, HH:mm:ss') : '-',
-            bidExpires: bidExpires ? moment(bidExpires).format('MMMM Do, YYYY, HH:mm:ss') : '-',
-            _publisherConfirmation: bid._publisherConfirmation ? <Anchor target='_blank' href={Item.getIpfsMetaUrl(bid._publisherConfirmation, process.env.IPFS_GATEWAY)} > {t('PUBLISHER')} </Anchor> : '-',
-            _advertiserConfirmation: bid._advertiserConfirmation ? <Anchor target='_blank' href={Item.getIpfsMetaUrl(bid._advertiserConfirmation, process.env.IPFS_GATEWAY)} > {t('ADVERTISER')} </Anchor> : '-',
-            cancelBtn: canCancel ? <CancelBid
-                icon={pendingCancel ? 'hourglass_empty' : ''}
+        let bidData = getCommonBidData({ bid, t })
+
+        bidData.cancelBtn = canCancel ? <CancelBid
+            icon={pendingCancel ? 'hourglass_empty' : ''}
+            adUnitId={bid._adUnitId}
+            bidId={bid._id}
+            placedBid={bid}
+            acc={this.props.account}
+            raised
+            className={classnames(theme.actionBtn, RTButtonTheme.inverted, RTButtonTheme.dark)}
+            onSave={this.onSave}
+            disabled={pendingCancel}
+        /> : null
+
+        bidData.verifyBtn = canVerify ?
+            <VerifyBid
+                noTargetsReached
+                icon={pendingVerify ? 'hourglass_empty' : (noTargetsReached ? '' : '')}
+                itemId={bid._adUnitId}
+                bidId={bid._id}
+                placedBid={bid}
+                acc={this.props.account}
+                raised
+                className={classnames(theme.actionBtn, RTButtonTheme.inverted, { [RTButtonTheme.warning]: noTargetsReached, [RTButtonTheme.success]: !noTargetsReached })}
+                onSave={this.onSave}
+                disabled={pendingVerify}
+            /> : null
+
+        bidData.refundBtn = canRefund ?
+            <RefundBid
+                icon={pendingRefund ? 'hourglass_empty' : ''}
                 adUnitId={bid._adUnitId}
                 bidId={bid._id}
                 placedBid={bid}
                 acc={this.props.account}
                 raised
-                className={classnames(theme.actionBtn, RTButtonTheme.inverted, RTButtonTheme.dark)}
+                className={classnames(theme.actionBtn, RTButtonTheme.inverted, RTButtonTheme.danger)}
                 onSave={this.onSave}
-                disabled={pendingCancel}
-            /> : null,
-            verifyBtn: canVerify ?
-                <VerifyBid
-                    noTargetsReached
-                    icon={pendingVerify ? 'hourglass_empty' : (noTargetsReached ? '' : '')}
-                    itemId={bid._adUnitId}
-                    bidId={bid._id}
-                    placedBid={bid}
-                    acc={this.props.account}
-                    raised
-                    className={classnames(theme.actionBtn, RTButtonTheme.inverted, { [RTButtonTheme.warning]: noTargetsReached, [RTButtonTheme.success]: !noTargetsReached })}
-                    onSave={this.onSave}
-                    disabled={pendingVerify}
-                /> : null,
-            refundBtn: canRefund ?
-                <RefundBid
-                    icon={pendingRefund ? 'hourglass_empty' : ''}
-                    adUnitId={bid._adUnitId}
-                    bidId={bid._id}
-                    placedBid={bid}
-                    acc={this.props.account}
-                    raised
-                    className={classnames(theme.actionBtn, RTButtonTheme.inverted, RTButtonTheme.danger)}
-                    onSave={this.onSave}
-                    disabled={pendingRefund}
-                /> : null,
-            pendingAcceptByPub: canCancel && pendingAcceptByPub ?
-                <TooltipIconButton
-                    icon='warning'
-                    tooltip={t('WARNING_PENDING_ACCEPT_BY_PUB')}
-                    className={RTButtonTheme.warning}
-                /> : null
+                disabled={pendingRefund}
+            /> : null
 
-        }
+        bidData.pendingAcceptByPub = canCancel && pendingAcceptByPub ?
+            <TooltipIconButton
+                icon='warning'
+                tooltip={t('WARNING_PENDING_ACCEPT_BY_PUB')}
+                className={RTButtonTheme.warning}
+            /> : null
+
 
         return bidData
 
@@ -197,53 +118,7 @@ export class UnitBids extends Component {
         const t = this.props.t
         const bidData = this.getBidData(bid)
 
-        return (
-            <TableRow key={bid._id}>
-                <TableCell>
-                    <BidDetailWithDialog
-                        btnLabel=''
-                        title={bid._id}
-                        t={t}
-                        bidData={bidData}
-                        icon='open_in_new'
-                        iconButton
-                    />
-                </TableCell>
-                <TableCell> {bidData._amount} </TableCell>
-                <TableCell>
-                    {bidData._target} / {bidData.clicksCount}
-                </TableCell>
-                <TableCell> {bidData._state} </TableCell>
-                <TableCell
-                    className={classnames(theme.compactCol, theme.ellipsis)}
-                >
-                    {bidData._publisher}
-                </TableCell>
-                <TableCell
-                    className={classnames(theme.compactCol, theme.ellipsis)}
-                >
-                    {bidData._adSlot}
-                </TableCell>
-                <TableCell>
-                    <div> {bidData.timeout} </div>
-                    <div> {bidData.accepted} </div>
-                    <div> {bidData.bidExpires} </div>
-                </TableCell>
-                <TableCell
-                    className={classnames(theme.compactCol, theme.ellipsis)}
-                >
-                    <div> {bidData._publisherConfirmation} </div>
-                    <div> {bidData._advertiserConfirmation} </div>
-                </TableCell>
-                <TableCell>
-                    {bidData.cancelBtn}
-                    {bidData.pendingAcceptByPub}
-                    {bidData.verifyBtn}
-                    {bidData.refundBtn}
-                </TableCell>
-            </TableRow >
-        )
-
+        return renderCommonTableRow({ bidData, t })
     }
 
     renderRows = (items) =>
@@ -254,17 +129,8 @@ export class UnitBids extends Component {
             item={{}}
             rows={items}
             rowRenderer={this.renderTableRow.bind(this)}
-            tableHeadRenderer={this.renderTableHead.bind(this)}
+            tableHeadRenderer={renderTableHead.bind(this, { t: this.props.t })}
         />
-
-    searchMatch = (bid) => {
-        return (bid._id || '') +
-            (bid._amount || '') +
-            (bid._advertiser || '') +
-            (bid._publisher || '') +
-            (bid._timeout || '') +
-            (bid._target || '')
-    }
 
     sortBids = (bids) => {
         const sorted = bids.reduce((memo, bid) => {
@@ -305,7 +171,7 @@ export class UnitBids extends Component {
                             listMode='rows'
                             renderRows={this.renderRows.bind(this)}
                             sortProperties={SORT_PROPERTIES_BIDS}
-                            searchMatch={this.searchMatch}
+                            searchMatch={searchMatch}
                             filterProperties={FILTER_PROPERTIES_BIDS}
                         />
                     </Tab>
@@ -315,7 +181,7 @@ export class UnitBids extends Component {
                             listMode='rows'
                             renderRows={this.renderRows.bind(this)}
                             sortProperties={SORT_PROPERTIES_BIDS}
-                            searchMatch={this.searchMatch}
+                            searchMatch={searchMatch}
                             filterProperties={FILTER_PROPERTIES_BIDS}
                         />
                     </Tab>
@@ -325,7 +191,7 @@ export class UnitBids extends Component {
                             listMode='rows'
                             renderRows={this.renderRows.bind(this)}
                             sortProperties={SORT_PROPERTIES_BIDS}
-                            searchMatch={this.searchMatch}
+                            searchMatch={searchMatch}
                             filterProperties={FILTER_PROPERTIES_BIDS}
                         />
                     </Tab>
