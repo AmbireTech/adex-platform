12a3099527dad873367c1732dc7dbd1e7690099b
Ivo paunov <ivo.paunov@gmail.com>
2018-04-24 16:10:03 +0300

    Item ipf details for bid report

--- src/components/dashboard/containers/ItemIpfsDetails.js
+++ src/components/dashboard/containers/ItemIpfsDetails.js
@@ -3,88 +3,128 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import actions from 'actions'
-import { Grid, Row, Col } from 'react-flexbox-grid'
-import { AdSlot } from 'adex-models'
-import ItemHoc from './ItemHoc'
-import theme from './theme.css'
-import SlotBids from './SlotBids'
+import { Grid } from 'react-flexbox-grid'
 import { items as ItemsConstants } from 'adex-constants'
-import { BasicProps } from './ItemCommon'
-import Helper from 'helpers/miscHelpers'
-import ImgDialog from './ImgDialog'
-import { Item as ItemModel } from 'adex-models'
-import { AVATAR_MAX_WIDTH, AVATAR_MAX_HEIGHT } from 'constants/misc'
 import { Item } from 'adex-models'
-import { PropRow, ContentBox, ContentBody, ContentStickyTop, FullContentSpinner } from 'components/common/dialog/content'
+import { PropRow, ContentBox, ContentBody, FullContentSpinner } from 'components/common/dialog/content'
 import Anchor from 'components/common/anchor/anchor'
 
-const { ItemsTypes, AdSizesByValue, AdTypesByValue } = ItemsConstants
-const ADVIEW_URL = process.env.ADVIEW_HOST || 'https://view.adex.network'
+const { AdSizesByValue, AdTypesByValue } = ItemsConstants
 
 export class ItemIpfsDetails extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            itemData: null,
+            details: null,
             itemIpfsUrl: Item.getIpfsMetaUrl(this.props.itemIpfs, process.env.IPFS_GATEWAY)
         }
     }
 
     componentWillMount() {
-        this.props.actions.updateSpinner(this.props.itemIpfs, true)
+        if (this.props.details) {
+            this.setState({ details: this.props.details })
+        } else {
+            this.props.actions.updateSpinner(this.props.itemIpfs, true)
 
-        fetch(this.state.itemIpfsUrl)
-            .then((res) => {
-                return res.json()
-            })
-            .then((res) => {
-                this.setState({ itemData: res })
-                this.props.actions.updateSpinner(this.props.itemIpfs, false)
-            })
-            .catch((err) => {
-                console.log('err', err)
-            })
+            fetch(this.state.itemIpfsUrl)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((res) => {
+                    this.setState({ details: res })
+                    this.props.actions.updateSpinner(this.props.itemIpfs, false)
+                })
+                .catch((err) => {
+                    console.log('err', err)
+                })
+        }
     }
 
-    slotProps
+    itemDetails = ({ details, t }) => {
+        return (
+            <Grid fluid style={{ padding: 0 }}>
+                <PropRow
+                    left={t('PROP_FULLNAME')}
+                    right={details.fullName}
+                />
+                <PropRow
+                    left={t('PROP_OWNER')}
+                    right={<Anchor target='_blank' href={process.env.ETH_SCAN_ADDR_HOST + details.owner} > {details.owner || '-'} </Anchor>}
+                />
+                <PropRow
+                    left={t('PROP_ADTYPE')}
+                    right={AdTypesByValue[details.adType].label}
+                />
+                <PropRow
+                    left={t('PROP_SIZE')}
+                    right={t(AdSizesByValue[details.size].label, { args: AdSizesByValue[details.size].labelArgs })}
+                />
+                <PropRow
+                    left={t('SLOT_IPFS')}
+                    right={<Anchor target='_blank' href={this.state.itemIpfsUrl} > {this.props.itemIpfs} </Anchor>}
+                />
+            </Grid>
+        )
+    }
+
+    reportDetails = ({ report, t }) => {
+        return (
+            <Grid fluid style={{ padding: 0 }}>
+                <PropRow
+                    left={t('REPORT_IPFS')}
+                    right={<Anchor target='_blank' href={this.state.itemIpfsUrl} > {this.props.itemIpfs} </Anchor>}
+                />
+                <PropRow
+                    left={t('PROP_BIDID')}
+                    right={report.bidId}
+                />
+                <PropRow
+                    left={t('PROP_ADVERTISER')}
+                    right={<Anchor target='_blank' href={process.env.ETH_SCAN_ADDR_HOST + report.advertiser} > {report.advertiser || '-'} </Anchor>}
+                />
+                <PropRow
+                    left={t('PROP_PUBLISHER')}
+                    right={<Anchor target='_blank' href={process.env.ETH_SCAN_ADDR_HOST + report.publisher} > {report.publisher || '-'} </Anchor>}
+                />
+                <PropRow
+                    left={t('PROP_ADUNITIPFS')}
+                    right={<Anchor target='_blank' href={Item.getIpfsMetaUrl(report.adUnitIpfs, process.env.IPFS_GATEWAY)} >{report.adUnitIpfs}</Anchor>}
+                />
+                <PropRow
+                    left={t('PROP_ADSLOTIPFS')}
+                    right={<Anchor target='_blank' href={Item.getIpfsMetaUrl(report.adSlotIpfs, process.env.IPFS_GATEWAY)} >{report.adSlotIpfs}</Anchor>}
+                />
+                <PropRow
+                    left={t('PROP_ALLCLICKS')}
+                    right={report.allClicks}
+                />
+                <PropRow
+                    left={t('PROP_VERIFIEDUNIQUECLICKS')}
+                    right={report.verifiedUniqueClicks}
+                />
+            </Grid>
+        )
+    }
+
 
     render() {
-        let item = this.props.item || {}
         let t = this.props.t
 
-        const itemData = this.state.itemData
+        const details = this.state.details
 
-        console.log('slotData', itemData)
         return (
             <ContentBox>
-                {!!this.props.spinner || !itemData ?
+                {!!this.props.spinner || !details ?
                     <FullContentSpinner />
                     :
                     <ContentBody>
-                        <Grid fluid style={{ padding: 0 }}>
-                            <PropRow
-                                left={t('PROP_FULLNAME')}
-                                right={itemData.fullName}
-                            />
-                            <PropRow
-                                left={t('PROP_OWNER')}
-                                right={<Anchor target='_blank' href={process.env.ETH_SCAN_ADDR_HOST + itemData.owner} > {itemData.owner || '-'} </Anchor>}
-                            />
-                            <PropRow
-                                left={t('PROP_ADTYPE')}
-                                right={AdTypesByValue[itemData.adType].label}
-                            />
-                            <PropRow
-                                left={t('PROP_SIZE')}
-                                right={t(AdSizesByValue[itemData.size].label, { args: AdSizesByValue[itemData.size].labelArgs })}
-                            />
-                            <PropRow
-                                left={t('SLOT_IPFS')}
-                                right={<Anchor target='_blank' href={this.state.itemIpfsUrl} > {this.props.itemIpfs} </Anchor>}
-                            />
-                        </Grid>
-
+                        {this.props.detailsType === 'item' ?
+                            <this.itemDetails itemData={details} t={t} /> : null
+                        }
+                        {this.props.detailsType === 'report' ?
+                            <this.reportDetails report={details} t={t} /> : null
+                        }
                     </ContentBody>
                 }
             </ContentBox>
