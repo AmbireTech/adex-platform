fb2659d282ccadeec4ea6bb3d355b6e54a9c6551
Ivo paunov <ivo.paunov@gmail.com>
2018-05-31 18:54:01 +0300

    play with material-ui

---
+++ src/components/common/dialog/theme.js
@@ -0,0 +1,13 @@
+
+export const styles = theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+    dialog: {
+        minHeight: '70vh',
+        minWidth: 800,
+        maxWidth: 1080
+    },
+})
