4c0b384a6ab721c8102a65e2134336c36806fbf0
Ivo <ivo.paunov@gmail.com>
2018-06-20 00:09:31 +0300

    Bids statistics date picker styled

--- src/components/common/DatePicker/DatePicker.js
+++ src/components/common/DatePicker/DatePicker.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils'
 import MuiDatePicker from 'material-ui-pickers/DatePicker'
+import { withStyles } from '@material-ui/core/styles'
+import { styles } from './styles'
 
 export class DatePicker extends Component {
 
@@ -13,3 +15,28 @@ export class DatePicker extends Component {
 }
 
 export default DatePicker
+
+const datePickerStyled = ({ classes, ...rest }) => {
+    return (
+        <DatePicker
+            InputLabelProps={{
+                classes: {
+                    root: classes.datepickerContrastLabel,
+                },
+                FormLabelClasses: {
+                    root: classes.datepickerContrastLabel,
+                    focused: classes.datepickerContrastLabelFocused,
+                }
+            }}
+            InputProps={{
+                classes: {
+                    root: classes.datepickerContrastInput,
+                    underline: classes.datepickerContrastUnderline
+                }
+            }}
+            {...rest}
+        />
+    )
+}
+
+export const DatePickerContrast = withStyles(styles)(datePickerStyled)
