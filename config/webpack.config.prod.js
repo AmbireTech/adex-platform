62354c19ef91a1af525bdf993a96850f19324803
Ivo paunov <ivo.paunov@gmail.com>
2018-06-26 18:14:41 +0300

    fix webpack.dev cfg - stopped loading css

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -250,9 +250,11 @@ module.exports = {
       sourceMap: shouldUseSourceMap,
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    new ExtractTextPlugin({
+    // NOTE: Temp disabled because we do not have any imported css
+    /*new ExtractTextPlugin({
       filename: cssFilename,
-    }),
+    }),*/
+
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
