b74edea16718df9e405e9165324b654fab14ca23
simzzz <swagb3ast@hotmail.com>
2018-07-31 12:46:39 +0300

    Changed default value of ad url to empty string so it doesn't log an error

--- src/components/dashboard/forms/items/NewItemFormTags.js
+++ src/components/dashboard/forms/items/NewItemFormTags.js
@@ -23,10 +23,10 @@ class NewItemFormTags extends Component {
     componentWillMount() {
         this.props.validate('tags', {isValid: this.props.item.meta.tags && this.props.item.meta.tags.length > 0, err: {msg: noTagsErrMsg}})                             
             getTags()
-            .then((res) => {
-                const tags = res.reduce((o, key) => ({ ...o, [key._id]: key._id}), {})
-                this.props.actions.updateTags({tags: tags})
-            })
+                .then((res) => {
+                    const tags = res.reduce((o, key) => ({ ...o, [key._id]: key._id}), {})
+                    this.props.actions.updateTags({tags: tags})
+                })
     }
 
     
