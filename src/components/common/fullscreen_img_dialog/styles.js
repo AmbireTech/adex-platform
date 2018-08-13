223a472b2930f835458c58881245254959f61ff0
simzzz <swagb3ast@hotmail.com>
2018-08-13 14:36:38 +0300

    Made fullscreen dialog look a lot better on mobile

--- src/components/common/fullscreen_img_dialog/styles.js
+++ src/components/common/fullscreen_img_dialog/styles.js
@@ -2,18 +2,18 @@
 export const styles = theme => {
     return {
         dialog: {
-            '@media(max-width:1080px)': {
+            '@media(max-width:744px)': {
                 maxWidth: '100%',
                 minWidth: `calc(100vw - ${theme.spacing.unit * 2}px)`,
             },
             '@media(max-height:823px)': {
-                minHeight: `calc(100vh - ${theme.spacing.unit * 2}px)`,
+                minHeight: 'auto',
             }
         },
         dialogImage: {
             '@media(max-width:1080px)': {
-                maxWidth: '75vw',
-                maxHeight: '100%',
+                maxWidth: '80vw',
+                maxHeight: '80vw',
                 display: 'block',
                 margin: 'auto'
             }
