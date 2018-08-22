924f645ec2aad3174be5f7bfae7e617f2bfe2331
simzzz <simeon_nakov@hotmail.com>
2018-08-22 13:57:27 +0300

    Finished dashboard legend component

---
+++ src/components/dashboard/charts/slot/bidsLegend.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { styles } from './styles'
+
+class BidsLegend extends Component {
+    toggleChartItem(e, index) {
+        const chart = this.props.chart
+
+        chart.legend.options.onClick.call(chart, e, chart.legend.legendItems[index])
+    }
+
+    toggleLabelCross(label) {
+        label.style.textDecoration === 'line-through' ? label.style.textDecoration = 'none' : label.style.textDecoration = 'line-through'
+    }
+
+    render() {
+        const { chart, classes } = this.props
+        return (
+            <div>
+                <ul className={classes.legendList}>
+                    {chart.data.datasets[0].data.map((item, i) => {
+                        return (
+                            <li className={classes.legendListItem} onClick={(event) => {
+                                this.toggleChartItem(event, i)
+                                this.toggleLabelCross(event.currentTarget)}}
+                            >
+                                <span
+                                    style={{backgroundColor: chart.data.datasets[0].backgroundColor[i]}}
+                                    className={classes.legendSpan}
+                                >
+                                </span>
+                                {chart.data.labels[i] ? chart.data.labels[i] : null}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+export default withStyles(styles)(BidsLegend)
