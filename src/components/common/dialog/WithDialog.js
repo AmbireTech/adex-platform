1329fb72be261904c85107d98b8ac6bfe6cf8437
Ivo paunov <ivo.paunov@gmail.com>
2018-10-16 16:42:44 +0300

    demo mode

--- src/components/common/dialog/WithDialog.js
+++ src/components/common/dialog/WithDialog.js
@@ -14,6 +14,8 @@ import Icon from '@material-ui/core/Icon'
 import CancelIcon from '@material-ui/icons/Cancel'
 import DialogActions from '@material-ui/core/DialogActions'
 import Typography from '@material-ui/core/Typography'
+import { ContentBox, ContentBody, ContentStickyTop } from 'components/common/dialog/content'
+import { logOut, isDemoMode } from 'services/store-data/auth'
 
 const textBtn = ({ label, className, classes, style, onClick, ...rest }) => {
     return <span className={classnames(classes.textBtn, className)} style={style} onClick={onClick}> {label} </span>
@@ -65,6 +67,28 @@ export default function ItemHoc(Decorated) {
             return onSave
         }
 
+        renderDemoModeAction = () => {
+            const t = this.props.t
+            return (
+                <ContentBox>
+                    <ContentStickyTop>
+                        <Typography paragraph variant='subheading'>
+                            {t('DEMO_MODE_ACTION_DESCRIPTION')}
+                        </Typography>
+                    </ContentStickyTop>
+                    <ContentBody>
+                        <Button
+                            onClick={logOut}
+                            variant='raised'
+                            color='primary'
+                        >
+                            {t('DEMO_GO_AUTH_BTN')}
+                        </Button>
+                    </ContentBody>
+                </ContentBox>
+            )
+        }
+
         render() {
 
             let ButtonComponent = Button
@@ -89,6 +113,7 @@ export default function ItemHoc(Decorated) {
             const btnLabel = this.props.t(this.props.btnLabel, { args: this.props.btnLabelArgs || [''] })
             // TODO: fix it for fab wit text
             const isIconBtn = (this.props.variant == 'fab') || this.props.iconButton
+            const isDemo = isDemoMode()
 
             return (
                 <div >
@@ -146,7 +171,13 @@ export default function ItemHoc(Decorated) {
                         <DialogContent
                             classes={{ root: classes.content }}
                         >
-                            <Decorated {...this.props} onSave={this.onSave()} />
+                            {isDemo ?
+                                // NOTE: ugly but it's temp and saves a lot of work!
+                                this.renderDemoModeAction()
+                                :
+                                <Decorated {...this.props} onSave={this.onSave()} />
+                            }
+
                         </DialogContent>
                         {this.props.dialogActions &&
                             <DialogActions>
