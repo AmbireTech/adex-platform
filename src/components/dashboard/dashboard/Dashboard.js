b83ceacbfadee2e4b57aeb83552c6c6633e09449
simzzz <simeon_nakov@hotmail.com>
2018-10-23 17:23:56 +0300

    Set fixed dynamic width iin dashboard info stats

--- src/components/dashboard/dashboard/Dashboard.js
+++ src/components/dashboard/dashboard/Dashboard.js
@@ -222,21 +222,18 @@ class Dashboard extends React.Component {
 
                         <Switch locatiom={this.props.location}>
                             TODO: Make things dynamic if easier
-                        <PrivateRoute auth={this.props.auth} exact path='/dashboard/advertiser/campaigns' component={this.renderCampaigns} />
+                            <PrivateRoute auth={this.props.auth} exact path='/dashboard/advertiser/campaigns' component={this.renderCampaigns} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/advertiser/units' component={this.renderAdUnits} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/advertiser/Campaign/:itemId' component={Campaign} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/advertiser/AdUnit/:itemId' component={Unit} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/advertiser/bids/:tab?' component={UnitBids} />
-
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/publisher/channels' component={this.renderChannels} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/publisher/slots' component={this.renderAdSlots} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/publisher/Channel/:itemId' component={Channel} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/publisher/AdSlot/:itemId' component={Slot} />
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/publisher/bids/:tab?' component={SlotBids} />
-
                             <PrivateRoute auth={this.props.auth} exact path={'/dashboard/:side/account'} component={Account} />
                             <PrivateRoute auth={this.props.auth} exact path={'/dashboard/:side/transactions'} component={Transactions} />
-
                             <PrivateRoute auth={this.props.auth} exact path='/dashboard/:side' component={DashboardStats} />
                             <PrivateRoute auth={this.props.auth} component={() => <h1>404 at {side} side</h1>} />
                         </Switch>
