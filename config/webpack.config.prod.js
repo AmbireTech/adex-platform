ca452d2b54aa9791923ac0a0514065e176b9ff44
IvoPaunov <ivo.paunov@gmail.com>
2018-12-13 19:05:18 +0200

    work on sw fix

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -121,8 +121,8 @@ module.exports = {
     // Generated JS file names (with nested folders).
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
-    filename: 'static/js/[name].[chunkhash:8].js',
-    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    filename: 'static/js/[name].js',
+    // chunkFilename: 'static/js/[name].chunk.js', // attempt to make it work with previous service worker
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
@@ -279,7 +279,7 @@ module.exports = {
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
-              name: 'static/media/[name].[hash:8].[ext]',
+              name: 'static/media/[name].[ext]',
             },
           },
           // Process application JS with Babel.
@@ -429,6 +429,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: true,
       template: paths.appHtml,
+      hash: true,
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -463,8 +464,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: 'static/css/[name].[contenthash:8].css',
-      chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+      filename: 'static/css/[name].css',
+      // chunkFilename: 'static/css/[name].chunk.css', // prev ver compatability attempt
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
@@ -485,7 +486,7 @@ module.exports = {
       clientsClaim: true,
       skipWaiting: true,
       exclude: [/\.map$/, /asset-manifest\.json$/],
-      importWorkboxFrom: 'cdn',
+      importWorkboxFrom: 'local',
       navigateFallback: publicUrl + '/index.html',
       navigateFallbackBlacklist: [
         // Exclude URLs starting with /_, as they're likely an API call
