398e985a8325b526db69832bba0ba5ac37a76280
Ivo paunov <ivo.paunov@gmail.com>
2019-01-07 09:50:45 +0200

    update we3 node in env cfg

--- config/env.js
+++ config/env.js
@@ -78,7 +78,7 @@ function getClientEnvironment(publicUrl) {
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
         IPFS_GATEWAY: process.env.IPFS_GATEWAY || 'https://gateway.ipfs.io/ipfs/',
-        WEB3_NODE: process.env.WEB3_NODE || 'https://parity.wings.ai',
+        WEB3_NODE: process.env.WEB3_NODE || 'https://mainnet.infura.io/v3/',
         ADX_TOKEN_ADDR: process.env.ADX_TOKEN_ADDR || '0x4470BB87d77b963A013DB939BE332f927f2b992e',
         ADX_EXCHANGE_ADDR: process.env.ADX_EXCHANGE_ADDR || '0x912b8f85E28B9ec196b48228159E2f13546836e6',
         ADVIEW_HOST: process.env.ADVIEW_HOST || (process.env.NODE_ENV === 'production' ? 'https://view.adex.network' : 'http://localhost:15900'),
