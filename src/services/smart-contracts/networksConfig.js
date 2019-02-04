98d89d00f4a8b5f1beea5eedfe70c51e5566d216
IvoPaunov <ivo.paunov@gmail.com>
2019-02-04 18:21:00 +0200

    add networks and tokens config

---
+++ src/services/smart-contracts/networksConfig.js
@@ -0,0 +1,38 @@
+const networks = {
+    Mainnet: {
+        name: 'Mainnet',
+        nodes: [
+            {
+                name: 'infura.io',
+                addr: 'https://mainnet.infura.io/v3/'
+            }
+        ]
+    },
+    Kovan: {
+        name: 'Kovan',
+        nodes: [
+            {
+                name: 'infura.io',
+                addr: 'https://kovan.infura.io/v3/'
+            }
+        ]
+    },
+    Ropsten: {
+        name: 'Ropsten',
+        nodes: [
+            {
+                name: 'infura.io',
+                addr: 'https://ropsten.infura.io/v3/'
+            }
+        ]
+    },
+    Rinkeby: {
+        name: 'Rinkeby',
+        nodes: [
+            {
+                name: 'infura.io',
+                addr: 'https://rinkeby.infura.io/v3/'
+            }
+        ]
+    }
+}
