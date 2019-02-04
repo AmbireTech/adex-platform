0736073b1c69b5cc1e59071a622d9d07c76e2621
IvoPaunov <ivo.paunov@gmail.com>
2019-02-04 18:21:00 +0200

    add networks and tokens config

---
+++ src/services/smart-contracts/tokensConfig.js
@@ -0,0 +1,42 @@
+const tokens = [
+    {
+        symbol: 'ADX',
+        decimals: 4,
+        contractAddress: {
+            mainnet: '0x4470BB87d77b963A013DB939BE332f927f2b992e',
+            ropsten: null,
+            kovan: null,
+            rinkeby: null
+        }
+    },
+    {
+        symbol: 'DAI',
+        decimals: 18,
+        contractAddress: {
+            mainnet: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+            ropsten: null,
+            kovan: '0xC4375B7De8af5a38a93548eb8453a498222C4fF2',
+            rinkeby: null
+        }
+    },
+    {
+        symbol: 'MKR',
+        decimals: 18,
+        contractAddress: {
+            mainnet: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
+            ropsten: null,
+            kovan: '0x1Dad4783cf3fe3085C1426157aB175A6119A04bA',
+            rinkeby: null
+        }
+    },
+    {
+        symbol: 'ZRX',
+        decimals: 18,
+        contractAddress: {
+            mainnet: '0xe41d2489571d322189246dafa5ebde1f4699f498',
+            ropsten: null,
+            kovan: '0x6Ff6C0Ff1d68b964901F986d4C9FA3ac68346570',
+            rinkeby: null
+        }
+    },
+]
