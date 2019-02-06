57ef481578c60ef6e971de085447db579f6ee2d0
IvoPaunov <ivo.paunov@gmail.com>
2019-02-06 17:45:16 +0200

    identity contract  feed token select

--- src/components/signin/identity/IdentityContractAddress.js
+++ src/components/signin/identity/IdentityContractAddress.js
@@ -8,7 +8,14 @@ import Translate from 'components/translate/Translate'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
 import { getDeployTx, getRandomAddressForDeployTx } from 'services/idenity/contract-address'
+import { tokens } from 'services/smart-contracts/tokensConfig'
+
+const RELAYER_ADDR = '0x0A8fe6e91eaAb3758dF18f546f7364343667E957'
 
 class IdentityContractAddress extends Component {
 
@@ -27,11 +34,13 @@ class IdentityContractAddress extends Component {
     }
 
     getIdentityContracAddress = () => {
+        const { identityContractOwner } = this.props.identity
+
         const deployTx = getDeployTx({
-            addr: '0x0A8fe6e91eaAb3758dF18f546f7364343667E957',
+            addr: identityContractOwner,
             privLevel: 3,
-            feeTokenAddr: '0x4470BB87d77b963A013DB939BE332f927f2b992e',
-            feeBeneficiery: '0x0A8fe6e91eaAb3758dF18f546f7364343667E957',
+            feeTokenAddr: this.props.identity.feeTokenAddr, // '0x4470BB87d77b963A013DB939BE332f927f2b992e',
+            feeBeneficiery: RELAYER_ADDR,
             feeTokenAmount: '10000'
         })
 
@@ -44,6 +53,28 @@ class IdentityContractAddress extends Component {
         })
     }
 
+    tokenSelect = () =>
+        <FormControl >
+            <InputLabel htmlFor='fee-token'>Fee Token</InputLabel>
+            <Select
+                value={this.props.identity.feeTokenAddr || ''}
+                onChange={(event) => this.props.handleChange('feeTokenAddr', event.target.value)}
+                inputProps={{
+                    name: 'fee-token',
+                    id: 'fee-token',
+                }}
+            >
+                <MenuItem value=''>
+                    <em>None</em>
+                </MenuItem>
+                {tokens.map((token) =>
+                    <MenuItem key={token.symbol}  value={token.contractAddress.kovan}>
+                        {token.symbol}
+                    </MenuItem>
+                )}
+            </Select>
+        </FormControl>
+
     render() {
         const { identity, t } = this.props
         const { extraEntropyIdentityContract, identityContractAddress } = identity || {}
@@ -54,6 +85,9 @@ class IdentityContractAddress extends Component {
                     container
                     spacing={16}
                 >
+                    <Grid item sm={6}>
+                        <this.tokenSelect />
+                    </Grid>
                     <Grid item sm={6}>
                         <TextField
                             fullWidth
