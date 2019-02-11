e76400cb5ce4159f4147a40edab061a3d5630f75
IvoPaunov <ivo.paunov@gmail.com>
2019-02-11 17:41:08 +0200

    add contracts config

---
+++ src/services/smart-contracts/smartContractsConfig.js
@@ -0,0 +1,10 @@
+const contracts = {
+    AdexCore: {
+        contractAddress: {
+            mainnet: null,
+            ropsten: null,
+            kovan: '0xe6aa464334a067f52e44f7b6dabb91804371376c',
+            rinkeby: null
+        }
+    }
+}
