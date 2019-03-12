4e23cbca5335af3b4fae6e6a7b6e65edcf5ece1d
IvoPaunov <ivo.paunov@gmail.com>
2019-03-12 17:48:07 +0200

    wallet init step

--- src/components/signin/identity/IdentitySteps.js
+++ src/components/signin/identity/IdentitySteps.js
@@ -3,14 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import actions from 'actions'
-import Button from '@material-ui/core/Button'
-import IdentityHoc from './IdentityHoc'
-import IdentityContractAddressEthDeploy from './IdentityContractAddressEthDeploy'
-import IdentityContractAddressEthTransaction from './IdentityContractAddressEthTransaction'
-import IdentityContractOwner from './IdentityContractOwner'
 import ValidItemHoc from 'components/common/stepper/ValidItemHoc'
-import MaterialStepper from 'components/common/stepper/MaterialUiStepper'
-import SaveIcon from '@material-ui/icons/Save'
 import Translate from 'components/translate/Translate'
 import { withStyles } from '@material-ui/core/styles'
 import { styles } from './styles'
@@ -27,7 +20,7 @@ class IdentitySteps extends Component {
             pages.push({
                 title: t(page.title),
                 cancelBtn: cancelButton,
-                component: ValidItemHoc(page.page || page),
+                component: ValidItemHoc(page.page),
                 props: { ...this.props, validateId: validateId + '-' + index }
             })
         })
--- src/components/signin/identity/WalletInit.js
+++ src/components/signin/identity/WalletInit.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import actions from 'actions'
-import WalletHoc from './WalletHoc'
+import IdentityHoc from './IdentityHoc'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 import Translate from 'components/translate/Translate'
 import { withStyles } from '@material-ui/core/styles'
 import { styles } from './styles'
@@ -14,24 +16,49 @@ class WalletInit extends Component {
     constructor(props, context) {
         super(props, context)
         this.state = {
-            mnemonic = props.wallet.mnemonic
+            mnemonic: props.wallet.mnemonic
         }
     }
 
+    componentDidMount() {
+        this.props.validate('mnemonic', {
+            isValid: !!this.props.identity.identityContractAddress,
+            err: { msg: 'ERR_NO_MNEMONIC' },
+            dirty: false
+        })
+    }
+
+    getMnemonic = () => {
+        const mnemonic = getRandomMnemonic()
+        this.props.handleChange('mnemonic', mnemonic)
+        this.props.validate('mnemonic', {
+            isValid: true
+        })
+    } 
+
     render() {
+        const {t, identity} = this.props 
+        console.log('props', this.props)
         return (
             <div>
                 <Button
-                    onClick={() => this.setState({mnemonic: getRandomMnemonic()})}
+                    onClick={this.getMnemonic}
                     variant='raised'
                     color='primary'
                     disabled={this.state.waitingAddrData}
                 >
-                    {t('WALLET_GET_RANDOM_MNEMONIC')}
+                    {t('WALLET_GET_RANDOM_MNEMONIC_BTN')}
                 </Button>
                 <Typography paragraph variant='subheading'>
-                    {t('METAMASK_INFO')}
+                    {t('WALLET_GET_RANDOM_MNEMONIC_INFO')}
                 </Typography>
+                <Typography paragraph variant='subheading'>
+                    {identity.mnemonic}
+                </Typography>
+                {identity.mnemonic &&
+                <Typography paragraph variant='subheading'>
+                    {t('WALLET_GET_RANDOM_MNEMONIC_WRITE_DOWN_INFO')}
+                </Typography>}
             </div>
         )
     }
@@ -57,8 +84,8 @@ function mapDispatchToProps(dispatch) {
     }
 }
 
-const WalletInitStep = WalletHoc(WalletInit)
+const IdentityWalletInitStep = IdentityHoc(WalletInit)
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(Translate(withStyles(styles)(WalletInitStep)))
+)(Translate(withStyles(styles)(IdentityWalletInitStep)))
