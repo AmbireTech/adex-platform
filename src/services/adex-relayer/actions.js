6705e5078159b8d55f666a7bdbeb30931dbeda70
IvoPaunov <ivo.paunov@gmail.com>
2019-03-18 19:16:42 +0200

    Add adex relayer request actions

---
+++ src/services/adex-relayer/actions.js
@@ -0,0 +1,45 @@
+import Requester from 'services/requester'
+
+const ADEX_RELAYER_HOST = process.env.ADEX_RELAYER_HOST
+
+const requester = new Requester({ baseUrl: ADEX_RELAYER_HOST })
+
+const processResponse = (res) => {
+
+    if(res.status >= 200 && res.status < 400) {
+        return res.json()
+    } else {
+        return res.text()
+        .then((text) => {
+            throw { status: res.status, error: res.statusText + ' - ' + text }
+        })
+    }
+}
+
+export const identityToEmail = ({identity, privileges, mail }) => {
+    return requester.fetch({
+        route: 'identity-info-mail',
+        method: 'POST',
+        body: JSON.stringify({
+            identity,
+            privileges,
+            mail
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(processResponse)
+}
+
+export const quickAccount = ({ownerAddr, mail, couponCode}) => {
+    return requester.fetch({
+        route: 'quick-account',
+        method: 'POST',
+        body: JSON.stringify({
+            mail,
+            identityAddr
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(processResponse)
+}
+
