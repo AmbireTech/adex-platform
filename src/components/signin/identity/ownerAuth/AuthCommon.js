6b0675a1023994ffa9b42a31c97503326b7d04c6
Ivo <ivo.paunov@gmail.com>
2019-04-03 23:44:58 +0300

    Update AuthCommon to use the new stats format

--- src/components/signin/identity/ownerAuth/AuthCommon.js
+++ src/components/signin/identity/ownerAuth/AuthCommon.js
@@ -1,39 +1,24 @@
 import React from 'react'
 import ListItemText from '@material-ui/core/ListItemText'
-import { adxToFloatView } from 'services/smart-contracts/utils'
-import { web3Utils } from 'services/smart-contracts/ADX'
 import { withStyles } from '@material-ui/core/styles'
 import { styles } from './styles'
+import { utils } from 'ethers'
 
-export const addrItem = ({ addr, stats, t, classes }) => {
-	const addrBalanceAdx = adxToFloatView(stats.balanceAdx || 0)
-	const addrBalanceEth = web3Utils.fromWei(stats.balanceEth || '0', 'ether')
-	const exchBal = stats.exchangeBalance || {}
-	const adxOnBids = adxToFloatView(exchBal.onBids || 0)
-	const exchangeAvailable = adxToFloatView(exchBal.available || 0)
+export const addrItem = ({ stats, t, classes }) => {
+	const { walletBalanceEth, walletBalanceDai, address } = stats
 
 	return (
 		<ListItemText
-			primary={addr}
+			primary={address}
 			secondary={
 				<span className={classes.addrInfo}>
 					<span>
 						<span> ETH </span>
-						<strong> {addrBalanceEth} </strong>
+						<strong> {utils.formatEther(walletBalanceEth)} </strong>
 					</span>
 					<span>
-						<span> ADX </span>
-						<strong> {addrBalanceAdx} </strong>
-					</span>
-					<span>
-						<span className={classes.addrInfoLabel}> {t('ADDR_INFO_ON_BIDS')} </span>
-						<span> ADX </span>
-						<strong> {adxOnBids} </strong>
-					</span>
-					<span>
-						<span className={classes.addrInfoLabel}> {t('ADDR_INFO_XCH_AVAILABLE')} </span>
-						<span> ADX </span>
-						<strong> {exchangeAvailable}  &nbsp;</strong>
+						<span> DAI </span>
+						<strong> {utils.formatEther(walletBalanceDai)} </strong>
 					</span>
 				</span>
 			}
