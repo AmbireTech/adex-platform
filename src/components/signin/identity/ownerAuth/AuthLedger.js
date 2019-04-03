b640805e9ce65582e5468a3029360fc04ee6a6b9
Ivo <ivo.paunov@gmail.com>
2019-04-04 00:51:06 +0300

    update AuthLedger

--- src/components/signin/identity/ownerAuth/AuthLedger.js
+++ src/components/signin/identity/ownerAuth/AuthLedger.js
@@ -10,22 +10,21 @@ import Img from 'components/common/img/Img'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import Typography from '@material-ui/core/Typography'
-import { getAddrs } from 'services/hd-wallet/utils'
-import scActions from 'services/smart-contracts/actions'
-import ledgerTransportU2f from '@ledgerhq/hw-transport-u2f'
-import ledgerEth from "@ledgerhq/hw-app-eth"
+import { getAddressBalances } from 'services/smart-contracts/actions/stats'
 import AuthHoc from './AuthHoc'
 import { AUTH_TYPES } from 'constants/misc'
 import { AddrItem } from './AuthCommon'
-import { ContentBox, ContentBody, ContentStickyTop, TopLoading } from 'components/common/dialog/content'
-import Helper from 'helpers/miscHelpers'
+import {
+	ContentBox,
+	ContentBody,
+	ContentStickyTop,
+	TopLoading
+} from 'components/common/dialog/content'
 import LEDGER_DL_IMG from 'resources/ledger_logo_header.png'
 import { withStyles } from '@material-ui/core/styles'
 import { styles } from './styles'
-
-const { getAccountStats } = scActions
-
-const HD_PATH = "m/44'/60'/0'"
+import { getEthers } from 'services/smart-contracts/ethers'
+import { getSigner } from 'services/smart-contracts/actions/ethers'
 
 class AuthLedger extends Component {
 	constructor(props) {
@@ -35,158 +34,171 @@ class AuthLedger extends Component {
 			sideSelect: false,
 			addresses: [],
 			waitingLedgerAction: false,
-			waitingAddrsData: false
+			waitingAddrsData: false,
+			selectedAddress: null,
+			hdPath: ''
 		}
 	}
 
-	componentWillUnmount() {
+	connectLedger = async () => {
+
+		this.setState({ waitingLedgerAction: true }, async () => {
+
+			const {
+				provider
+			} = await getEthers(AUTH_TYPES.LEDGER.name)
+
+			const account = {
+				_wallet: {
+					authType: AUTH_TYPES.LEDGER.name
+				}
+			}
+			const signer = await getSigner({ account, provider })
+			const addresses = await signer.getAddresses()
+
+			const allAddressesData = addresses.map(address =>
+				getAddressBalances({ address, authType: AUTH_TYPES.LEDGER.name })
+			)
+
+			this.setState({ waitingAddrsData: true }, async () => {
+				const results = await Promise.all(allAddressesData)
+
+				this.setState({
+					hdPath: signer.path,
+					addresses: results,
+					waitingAddrsData: false,
+					waitingLedgerAction: false
+				})
+
+				// TODO: catch
+				// .catch((err) => {
+				// 	this.setState({ waitingLedgerAction: false, waitingAddrsData: false })
+				// 	this.props.actions.addToast({ type: 'cancel', action: 'X', label: this.props.t('ERR_AUTH_LEDGER', { args: [Helper.getErrMsg(err)] }), timeout: 5000 })
+				// })
+			})
+		})
+		// .catch((err) => {
+		// 	this.setState({ waitingLedgerAction: false, waitingAddrsData: false })
+		// 	this.props.actions.addToast({ type: 'cancel', action: 'X', label: this.props.t('ERR_AUTH_LEDGER', { args: [Helper.getErrMsg(err)] }), timeout: 5000 })
+		// })
+
 	}
 
-    connectLedger = () => {
+	AddressSelect = ({ addresses, waitingLedgerAction, t, classes, ...rest }) => {
+		return (
+			<ContentBox className={classes.tabBox}>
+				<ContentStickyTop>
+					{waitingLedgerAction ?
+						<TopLoading msg={t('LEDGER_WAITING_ACTION')} />
+						:
+						t('SELECT_ADDR_LEDGER')
+					}
+				</ContentStickyTop>
+				<ContentBody>
+					<List >
+						{addresses.map((res, index) =>
+							<ListItem
+								classes={{ root: classes.addrListItem }}
+								key={res.address}
+								onClick={() => this.onAddrSelect(res.address, index)}
+								selected={this.state.selectedAddress === res.address}
+							>
+								<AddrItem stats={res} t={t} address={res.address} />
+							</ListItem>
+						)}
+					</List>
+				</ContentBody>
+			</ContentBox>
+		)
+	}
 
-    	this.setState({ waitingLedgerAction: true }, () => {
+	onAddrSelect = (address, hdWalletAddrIdx) => {
+		this.props.updateWallet({
+			address,
+			authType: AUTH_TYPES.LEDGER.name,
+			hdWalletAddrPath: this.state.hdPath, // TODO: get it from address result
+			hdWalletAddrIdx,
+			signType: AUTH_TYPES.LEDGER.signType
+		})
 
-    		ledgerTransportU2f.create()
-    			.then((transport) => {
-    				var eth = new ledgerEth(transport)
+		this.setState({ selectedAddress: address })
+	}
 
-    				return eth.getAddress(HD_PATH, false, true)
-    			})
-    			.then((resp) => {
-    				let addresses = getAddrs(resp.publicKey, resp.chainCode)
+	render() {
+		let { t, classes } = this.props
 
-    				let allStatsPr = []
+		return (
+			<div>
+				{this.state.addresses.length ?
+					<this.AddressSelect
+						waitingLedgerAction={this.state.waitingLedgerAction}
+						addresses={this.state.addresses}
+						t={t}
+						classes={classes}
+					/>
+					:
+					<ContentBox className={classes.tabBox}>
+						{this.state.waitingAddrsData ?
+							<ContentStickyTop>
+								<TopLoading msg={t('LEDGER_WAITING_ADDRS_INFO')} />
+							</ContentStickyTop>
+							:
+							this.state.waitingLedgerAction ?
+								<ContentStickyTop>
+									<TopLoading msg={t('LEDGER_WAITING_ACTION')} />
+								</ContentStickyTop> : null
+						}
 
-    				addresses.forEach((addr) => {
-    					allStatsPr.push(getAccountStats({ _addr: addr, authType: AUTH_TYPES.LEDGER.name }))
-    				})
+						<ContentBody>
+							<Typography paragraph variant='subheading'>
+								{t('LEDGER_INFO')}
+							</Typography>
+							<Typography paragraph>
+								<span
+									dangerouslySetInnerHTML={
+										{
+											__html: t('LEDGER_BASIC_USAGE_INFO',
+												{
+													args: [{
+														component:
+															<Anchor
+																href='https://www.ledgerwallet.com/'
+																target='_blank'
+															>
+																LEDGER
+															   </Anchor>
+													}]
+												})
+										}
+									}
+								/>
+							</Typography>
+							<Typography paragraph>
+								<Anchor href='https://www.ledgerwallet.com/' target='_blank'>
+									<Img
+										src={LEDGER_DL_IMG}
+										alt={'https://www.ledgerwallet.com/'}
+										className={classes.dlBtnImg}
+									/>
+								</Anchor>
+							</Typography>
 
-    				this.setState({ waitingAddrsData: true }, () => {
-    					Promise.all(allStatsPr)
-    						.then((results) => {
-    							this.setState({ addresses: results, waitingAddrsData: false, waitingLedgerAction: false })
-    						})
-    						.catch((err) => {
-    							this.setState({ waitingLedgerAction: false, waitingAddrsData: false })
-    							this.props.actions.addToast({ type: 'cancel', action: 'X', label: this.props.t('ERR_AUTH_LEDGER', { args: [Helper.getErrMsg(err)] }), timeout: 5000 })
-    						})
-    				})
-    			})
-    			.catch((err) => {
-    				this.setState({ waitingLedgerAction: false, waitingAddrsData: false })
-    				this.props.actions.addToast({ type: 'cancel', action: 'X', label: this.props.t('ERR_AUTH_LEDGER', { args: [Helper.getErrMsg(err)] }), timeout: 5000 })
-    			})
-    	})
-    }
+							{(!this.state.waitingAddrsData && !this.state.waitingLedgerAction) &&
+								<Button
+									onClick={this.connectLedger}
+									variant='raised'
+									color='primary'
+								>
+									{t('CONNECT_WITH_LEDGER')}
+								</Button>
+							}
 
-    AddressSelect = ({ addresses, waitingLedgerAction, t, classes, ...rest }) => {
-    	return (
-    		<ContentBox className={classes.tabBox}>
-    			<ContentStickyTop>
-    				{waitingLedgerAction ?
-    					<TopLoading msg={t('LEDGER_WAITING_ACTION')} />
-    					:
-    					t('SELECT_ADDR_LEDGER')
-    				}
-    			</ContentStickyTop>
-    			<ContentBody>
-    				<List >
-    					{addresses.map((res, index) =>
-    						<ListItem
-    							classes={{ root: classes.addrListItem }}
-    							key={res.addr}
-    							onClick={this.onAddrSelect.bind(this, res.addr, index)}
-    						>
-    							<AddrItem stats={res} t={t} addr={res.addr} />
-    						</ListItem>
-    					)}
-    				</List>
-    			</ContentBody>
-    		</ContentBox>
-    	)
-    }
-
-    onAddrSelect = (addr, index) => {
-    	this.setState({ waitingLedgerAction: true }, () => {
-    		let mode = AUTH_TYPES.LEDGER.signType // TEMP?
-    		let authType = AUTH_TYPES.LEDGER.name
-    		this.props.authOnServer({ mode, addr, authType, hdPath: HD_PATH, addrIdx: index })
-    			.then((res) => {
-    			})
-    			.catch((err) => {
-    				console.log(err)
-    				this.setState({ waitingLedgerAction: false, waitingAddrsData: false })
-    				this.props.actions.addToast({ type: 'cancel', action: 'X', label: this.props.t('ERR_AUTH_LEDGER', { args: [(err || {}).error || err] }), timeout: 5000 })
-    			})
-    	})
-    }
-
-    render() {
-    	let { t, classes } = this.props
-
-    	return (
-    		<div>
-    			{this.state.addresses.length ?
-    				<this.AddressSelect
-    					waitingLedgerAction={this.state.waitingLedgerAction}
-    					addresses={this.state.addresses}
-    					t={t}
-    					classes={classes}
-    				/>
-    				:
-    				<ContentBox className={classes.tabBox}>
-    					{this.state.waitingAddrsData ?
-    						<ContentStickyTop>
-    							<TopLoading msg={t('LEDGER_WAITING_ADDRS_INFO')} />
-    						</ContentStickyTop>
-    						:
-    						this.state.waitingLedgerAction ?
-    							<ContentStickyTop>
-    								<TopLoading msg={t('LEDGER_WAITING_ACTION')} />
-    							</ContentStickyTop> : null
-    					}
-
-    					<ContentBody>
-    						<Typography paragraph variant='subheading'>
-    							{t('LEDGER_INFO')}
-    						</Typography>
-    						<Typography paragraph>
-    							<span
-    								dangerouslySetInnerHTML={
-    									{
-    										__html: t('LEDGER_BASIC_USAGE_INFO',
-    											{
-    												args: [{
-    													component:
-                                                            <Anchor href='https://www.ledgerwallet.com/' target='_blank'> LEDGER </Anchor>
-    												}]
-    											})
-    									}
-    								}
-    							/>
-    						</Typography>
-    						<Typography paragraph>
-    							<Anchor href='https://www.ledgerwallet.com/' target='_blank'>
-    								<Img src={LEDGER_DL_IMG} alt={'https://www.ledgerwallet.com/'} className={classes.dlBtnImg} />
-    							</Anchor>
-    						</Typography>
-
-    						{(!this.state.waitingAddrsData && !this.state.waitingLedgerAction) &&
-                                <Button
-                                	onClick={this.connectLedger}
-                                	variant='raised'
-                                	color='primary'
-                                >
-                                	{t('CONNECT_WITH_LEDGER')}
-                                </Button>
-    						}
-
-    					</ContentBody>
-    				</ContentBox>
-    			}
-    		</div>
-    	)
-    }
+						</ContentBody>
+					</ContentBox>
+				}
+			</div>
+		)
+	}
 }
 
 AuthLedger.propTypes = {
@@ -194,8 +206,7 @@ AuthLedger.propTypes = {
 }
 
 function mapStateToProps(state) {
-	const persist = state.persist
-	// const memory = state.memory
+	const { persist } = state
 	return {
 		account: persist.account
 	}
