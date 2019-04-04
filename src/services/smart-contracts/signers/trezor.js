8731fe7058e930717df849b52e3db2eabb94f135
Ivo <ivo.paunov@gmail.com>
2019-04-04 19:05:52 +0300

    trezor sign messages

--- src/services/smart-contracts/signers/trezor.js
+++ src/services/smart-contracts/signers/trezor.js
@@ -1,5 +1,6 @@
-import { Signer, providers, utils } from 'ethers'
+import { Signer, utils } from 'ethers'
 import TrezorConnect from 'trezor-connect'
+import { constants } from 'adex-models'
 
 const DEFAULT_HD_PATH = "m/44'/60'/0'/0"
 
@@ -15,6 +16,9 @@ export default class TrezorSigner extends Signer {
 		this._path = opts.path || DEFAULT_HD_PATH
 
 	}
+
+	get path() { return this._path }
+	get provider() { return this._provider }
 
 	getAddsParams = ({ from = 0, to = 10 } = {}) => {
 		const bundle = []
@@ -35,6 +39,46 @@ export default class TrezorSigner extends Signer {
 		return { success, payload }
 	}
 
-	get path() { return this._path }
-	get provider() { return this._provider }
+	sign = async (transaction) => {
+		const tx = await utils.resolveProperties(transaction)
+		const unsignedTx = utils.serializeTransaction(tx)
+			.substring(2)
+
+		const signature = await TrezorConnect
+			.ethereumSignTransaction({
+				path: this.path,
+				transaction: unsignedTx
+			})
+
+		return signature
+	}
+
+	signMessage = async (message, opts = {}) => {
+		const { success, payload } = await TrezorConnect
+			.ethereumSignMessage({
+				path: this.path,
+				message: message,
+				hex: !!opts.hex
+			})
+
+		if (success) {
+			const res = {
+				/*
+				* NOTE: SignatureTypes.GETH
+				* Current TrezorConnect window does not
+				* accept device with old firmware 
+				*/
+				signature: '0x' + payload.signature,
+				hash: message,
+				mode: constants.SignatureModes.GETH,
+				address: payload.address
+			}
+
+			return res
+		} else {
+			return {
+				error: payload.error
+			}
+		}
+	}
 }
