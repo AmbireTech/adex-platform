0e6a9d202fbd9ce43b40bba705e8645d19a4df4a
Ivo <ivo.paunov@gmail.com>
2019-04-08 11:06:13 +0300

    identity actions errors catch

--- src/actions/identityActions.js
+++ src/actions/identityActions.js
@@ -1,6 +1,12 @@
 import * as types from 'constants/actionTypes'
 import { grantAccount } from 'services/adex-relayer/actions'
 import { updateSpinner } from './uiActions'
+import { deployIdentityContract } from 'services/smart-contracts/actions/identity'
+import { registerFullIdentity } from 'services/adex-relayer/actions'
+import { addToast } from './uiActions'
+import {
+	getIdentityDeployData
+} from 'services/smart-contracts/actions/identity'
 
 // MEMORY STORAGE
 export function updateIdentity(prop, value) {
@@ -43,19 +49,86 @@ export function resetWallet() {
 export function getGrantAccount({ walletAddr, email, coupon }) {
 	return async function (dispatch) {
 		updateSpinner('getting-grant-identity', true)(dispatch)
-		const identityInfo = await grantAccount({
-			ownerAddr: walletAddr,
-			mail: email,
-			couponCode: coupon
-		})
-
-		if (identityInfo) {
-			return dispatch({
-				type: types.UPDATE_IDENTITY,
-				prop: 'identityAddr',
-				value: identityInfo.deployData.idContractAddr
+		try {
+			const identityInfo = await grantAccount({
+				ownerAddr: walletAddr,
+				mail: email,
+				couponCode: coupon
 			})
+
+			if (identityInfo) {
+				return dispatch({
+					type: types.UPDATE_IDENTITY,
+					prop: 'identityAddr',
+					value: identityInfo.deployData.idContractAddr
+				})
+			}
+		} catch (err) {
+			addToast({
+				type: 'cancel',
+				label: 'ERR_REGISTER_GRANT_IDENTITY',
+				args: [err],
+				timeout: 20000
+			})(dispatch)
 		}
+
 		updateSpinner('getting-grant-identity', false)(dispatch)
+	}
+}
+
+export function deployFullIdentity({ wallet, email, identityTxData, identityAddr }) {
+	return async function (dispatch) {
+		updateSpinner('getting-grant-identity', true)(dispatch)
+		try {
+			const tx = await deployIdentityContract({
+				...identityTxData,
+				wallet
+			})
+
+			const regInfo = await registerFullIdentity({
+				txHash: tx.hash,
+				identity: identityAddr,
+				privileges: [wallet.address, 3],
+				mail: email
+			})
+
+			if (regInfo) {
+				updateIdentity('isRegistered', true)(dispatch)
+			} else {
+				addToast({
+					type: 'cancel',
+					label: 'ERR_REGISTER_IDENTITY',
+					args: [tx.hash],
+					timeout: 20000
+				})(dispatch)
+			}
+		}
+		catch (err) {
+			addToast({
+				type: 'cancel',
+				label: 'ERR_REGISTER_IDENTITY',
+				args: [err],
+				timeout: 20000
+			})(dispatch)
+		}
+		updateSpinner('getting-grant-identity', true)(dispatch)
+
+	}
+}
+
+export function getFullIdentityTxData({ owner, privLevel }) {
+	return async function (dispatch) {
+		try {
+			const txData = await getIdentityDeployData({ owner, privLevel })
+			updateIdentity('identityAddr', txData.expectedAddr)(dispatch)
+			updateIdentity('identityTxData', txData)(dispatch)
+		} catch (err) {
+			addToast({
+				type: 'cancel',
+				label: 'ERR_GET_IDENTITY_TX_DATA',
+				args: [err],
+				timeout: 20000
+			})(dispatch)
+		}
 	}
 }
