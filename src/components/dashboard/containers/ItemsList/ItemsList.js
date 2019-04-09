f21f984669ca5f07341bb997144fe7667a38c55f
Ivo <ivo.paunov@gmail.com>
2019-04-09 19:36:32 +0300

    update items list

--- src/components/dashboard/containers/ItemsList/ItemsList.js
+++ src/components/dashboard/containers/ItemsList/ItemsList.js
@@ -45,7 +45,7 @@ const List = ({ list, itemRenderer }) => {
 	return (<div style={{ display: 'flex', flexGrow: 1, flexWrap: 'wrap' }}>
 		{list.map((item, index) =>
 			<div
-				key={item._id || item.id}
+				key={item.ipfs || index}
 				style={{ maxWidth: '100%' }}
 			>
 				{itemRenderer(item, index)}
@@ -56,235 +56,241 @@ const List = ({ list, itemRenderer }) => {
 
 class ItemsList extends Component {
 
-    renderCard = (item, index) => {
-    	// const t = this.props.t
-    	return (
-    		<Card
-    			key={item._id}
-    			item={item}
-    			name={item._meta.fullName}
-    			logo={item._meta.img}
-    			side={this.props.side}
-    			remove={null}
-    			renderActions={() => this.renderActions(item)}
-    		/>
-    	)
-    }
+	renderCard = (item, index) => {
+		// const t = this.props.t
+		return (
+			<Card
+				key={item.ipfs}
+				item={item}
+				name={item.type}
+				logo={item.mediaUrl || item.fallbackMediaUrl}
+				side={this.props.side}
+				remove={null}
+				itemType={this.props.itemType}
+				renderActions={() => this.renderActions(item)}
+			/>
+		)
+	}
 
-    renderTableHead = ({ selected }) => {
-    	const t = this.props.t
-    	return (
-    		<TableHead>
-    			<TableRow>
-    				<TableCell>
-    					{selected.length &&
-                            <Tooltip
-                            	title={t('DELETE_ALL')}
-                            	// placement='top'
-                            	enterDelay={1000}
-                            >
-                            	<Button
-                            		// icon='delete'
-                            		onClick={null}
-                            	>
-                            		{t('DELETE_ALL')}
-                            	</Button >
-                            </Tooltip>
+	renderTableHead = ({ selected }) => {
+		const t = this.props.t
+		return (
+			<TableHead>
+				<TableRow>
+					<TableCell>
+						{selected.length &&
+							<Tooltip
+								title={t('DELETE_ALL')}
+								// placement='top'
+								enterDelay={1000}
+							>
+								<Button
+									// icon='delete'
+									onClick={null}
+								>
+									{t('DELETE_ALL')}
+								</Button >
+							</Tooltip>
 
-    					}
-    				</TableCell>
-    				<TableCell> {t('PROP_NAME')} </TableCell>
-    				<TableCell> {t('PROP_ADTYPE')} </TableCell>
-    				<TableCell> {t('PROP_SIZE')}</TableCell>
-    				<TableCell> {t('PROP_CREATEDON')} </TableCell>
-    				<TableCell> {t('ACTIONS')} </TableCell>
-    			</TableRow>
-    		</TableHead>
-    	)
-    }
+						}
+					</TableCell>
+					<TableCell> {t('PROP_NAME')} </TableCell>
+					<TableCell> {t('PROP_ADTYPE')} </TableCell>
+					<TableCell> {t('PROP_CREATEDON')} </TableCell>
+					<TableCell> {t('ACTIONS')} </TableCell>
+				</TableRow>
+			</TableHead>
+		)
+	}
 
-    renderTableRow = (item, index, { to, selected }) => {
-    	const { t, classes } = this.props
-    	return (
-    		<TableRow
-    			key={item._id || index}
-    			selected={selected}
-    		>
-    			<RRTableCell
-    				// className={tableTheme.link}
-    				to={to}
-    				// theme={tableTheme}
-    			>
-    				<Img
-    					className={classnames(classes.cellImg)}
-    					src={Item.getImgUrl(item._meta.img, process.env.IPFS_GATEWAY) || ''}
-    					alt={item._meta.fullName}
-    				/>
-    			</RRTableCell>
-    			<RRTableCell
-    				// className={tableTheme.link}
-    				to={to}
-    			>
-    				{item._meta.fullName}
-    			</RRTableCell>
-    			<TableCell> {itemAdTypeLabel({ adType: item._meta.adType })} </TableCell>
-    			<TableCell> {itemAdSizeLabel({ size: item._meta.size, t: t })} </TableCell>
-    			<TableCell> {moment(item._meta.createdOn).format('DD-MM-YYYY')} </TableCell>
-    			<TableCell>
-    				<Tooltip
-    					title={t('LABEL_VIEW')}
-    					// placement='top'
-    					enterDelay={1000}
-    				>
-    					<RRButton
-    						to={to}
-    						variant='raised'
-    						color='primary'
-    					>
-    						{t('LABEL_VIEW')}
-    					</RRButton>
-    				</Tooltip>
-    				{this.renderActions(item)}
+	renderTableRow = (item, index, { to, selected }) => {
+		const { t, classes } = this.props
+		return (
+			<TableRow
+				key={item.ipfs || index}
+				selected={selected}
+			>
+				<RRTableCell
+					// className={tableTheme.link}
+					to={to}
+				// theme={tableTheme}
+				>
+					<Img
+						className={classnames(classes.cellImg)}
+						src={item.mediaUrl || item.fallbackMediaiUrl  || ''}
+						alt={item.title}
+					/>
+				</RRTableCell>
+				<RRTableCell
+					// className={tableTheme.link}
+					to={to}
+				>
+					{item.title}
+				</RRTableCell>
+				<TableCell> {item.type} </TableCell>
+				<TableCell> {moment(item.created).format('DD-MM-YYYY')} </TableCell>
+				<TableCell>
+					<Tooltip
+						title={t('LABEL_VIEW')}
+						// placement='top'
+						enterDelay={1000}
+					>
+						<RRButton
+							to={to}
+							variant='raised'
+							color='primary'
+						>
+							{t('LABEL_VIEW')}
+						</RRButton>
+					</Tooltip>
+					{this.renderActions(item)}
 
-    			</TableCell>
-    		</TableRow>
-    	)
-    }
+				</TableCell>
+			</TableRow>
+		)
+	}
 
-    renderActions = (item) => {
-    	const { parentItem, t, account } = this.props
-    	const parentName = parentItem ? parentItem._meta.fullName : ''
-    	const itemName = item._meta.fullName
-    	const itemTypeName = t(ItemTypesNames[item._type], { isProp: true })
-    	const isDemo = account._authType === 'demo'
+	renderActions = (item) => {
+		const {
+			parentItem,
+			t,
+			account,
+			actions,
+		} = this.props
+		const parentName = parentItem ? parentItem.title : ''
+		const itemName = item.title
+		const itemTypeName = t(ItemTypesNames[item._type], { isProp: true })
+		const isDemo = account.wallet.address === 'demo'
 
-    	return (
-    		<span>
-    			{!item._archived &&
-                    <Tooltip
-                    	title={t('TOOLTIP_ARCHIVE')}
-                    	// placement='top'
-                    	enterDelay={1000}
-                    >
-                    	<IconButton
-                    		disabled={isDemo}
-                    		// label={t('ARCHIVE')}
-                    		// className={RTButtonTheme.danger}
-                    		onClick={this.props.actions.confirmAction.bind(this,
-                    			this.props.actions.archiveItem.bind(this, { item: item, authSig: this.props.account._authSig }),
-                    			null,
-                    			{
-                    				confirmLabel: t('CONFIRM_YES'),
-                    				cancelLabel: t('CONFIRM_NO'),
-                    				text: t('ARCHIVE_ITEM', { args: [itemTypeName, itemName] }),
-                    				title: t('CONFIRM_SURE')
-                    			})}
-                    	>
-                    		<ArchiveIcon />
-                    	</IconButton>
-                    </Tooltip>
-    			}
-    			{item._archived &&
-                    <Tooltip
-                    	title={t('TOOLTIP_UNARCHIVE')}
-                    	enterDelay={1000}
-                    // placement='top'
-                    >
-                    	<IconButton
-                    		disabled={isDemo}
-                    		// label={t('UNARCHIVE')}
-                    		color='secondary'
-                    		onClick={this.props.actions.confirmAction.bind(this,
-                    			this.props.actions.unarchiveItem.bind(this, { item: item, authSig: this.props.account._authSig }),
-                    			null,
-                    			{
-                    				confirmLabel: t('CONFIRM_YES'),
-                    				cancelLabel: t('CONFIRM_NO'),
-                    				text: t('UNARCHIVE_ITEM', { args: [itemTypeName, itemName] }),
-                    				title: t('CONFIRM_SURE')
-                    			})}
-                    	>
-                    		<UnarchiveIcon />
-                    	</IconButton>
-                    </Tooltip>
-    			}
-    			{(this.props.removeFromItem && parentItem) &&
-                    <Tooltip
-                    	title={t('REMOVE_FROM', { args: [parentName] })}
-                    	// placement='top'
-                    	enterDelay={1000}
-                    >
-                    	<IconButton
-                    		disabled={isDemo}
-                    		icon='remove_circle_outline'
-                    		label={t('REMOVE_FROM', { args: [parentName] })}
-                    		// className={RTButtonTheme.danger}
-                    		onClick={this.props.actions.confirmAction.bind(this,
-                    			this.props.actions.removeItemFromItem.bind(this, { item: item, toRemove: this.props.parentItem, authSig: this.props.account._authSig }),
-                    			null,
-                    			{
-                    				confirmLabel: t('CONFIRM_YES'),
-                    				cancelLabel: t('CONFIRM_NO'),
-                    				text: t('REMOVE_ITEM', { args: [itemTypeName, itemName, t(ItemTypesNames[parentItem._type], { isProp: true }), parentName] }),
-                    				title: t('CONFIRM_SURE')
-                    			})}
-                    	>
-                    		<RemoveCircleOutlineIcon />
-                    	</IconButton>
-                    </Tooltip>
-    			}
+		return (
+			<span>
+				{!item._archived &&
+					<Tooltip
+						title={t('TOOLTIP_ARCHIVE')}
+						// placement='top'
+						enterDelay={1000}
+					>
+						<IconButton
+							disabled={isDemo}
+							// label={t('ARCHIVE')}
+							// className={RTButtonTheme.danger}
+							onClick={actions.confirmAction.bind(this,
+								actions.archiveItem.bind(this, { item: item, authSig: account.wallet.authSig }),
+								null,
+								{
+									confirmLabel: t('CONFIRM_YES'),
+									cancelLabel: t('CONFIRM_NO'),
+									text: t('ARCHIVE_ITEM', { args: [itemTypeName, itemName] }),
+									title: t('CONFIRM_SURE')
+								})}
+						>
+							<ArchiveIcon />
+						</IconButton>
+					</Tooltip>
+				}
+				{item._archived &&
+					<Tooltip
+						title={t('TOOLTIP_UNARCHIVE')}
+						enterDelay={1000}
+					// placement='top'
+					>
+						<IconButton
+							disabled={isDemo}
+							// label={t('UNARCHIVE')}
+							color='secondary'
+							onClick={actions.confirmAction.bind(this,
+								actions.unarchiveItem.bind(this, { item: item, authSig: account.wallet.authSig }),
+								null,
+								{
+									confirmLabel: t('CONFIRM_YES'),
+									cancelLabel: t('CONFIRM_NO'),
+									text: t('UNARCHIVE_ITEM', { args: [itemTypeName, itemName] }),
+									title: t('CONFIRM_SURE')
+								})}
+						>
+							<UnarchiveIcon />
+						</IconButton>
+					</Tooltip>
+				}
+				{(this.props.removeFromItem && parentItem) &&
+					<Tooltip
+						title={t('REMOVE_FROM', { args: [parentName] })}
+						// placement='top'
+						enterDelay={1000}
+					>
+						<IconButton
+							disabled={isDemo}
+							icon='remove_circle_outline'
+							label={t('REMOVE_FROM', { args: [parentName] })}
+							// className={RTButtonTheme.danger}
+							onClick={actions.confirmAction.bind(this,
+								actions.removeItemFromItem.bind(this, { item: item, toRemove: parentItem, authSig: account.wallet.authSig }),
+								null,
+								{
+									confirmLabel: t('CONFIRM_YES'),
+									cancelLabel: t('CONFIRM_NO'),
+									text: t('REMOVE_ITEM', { args: [itemTypeName, itemName, t(ItemTypesNames[parentItem._type], { isProp: true }), parentName] }),
+									title: t('CONFIRM_SURE')
+								})}
+						>
+							<RemoveCircleOutlineIcon />
+						</IconButton>
+					</Tooltip>
+				}
 
-    			{(this.props.addToItem && parentItem) &&
-                    <Tooltip
-                    	tile={t('ADD_TO', { args: [parentName] })}
-                    	// placement='top'
-                    	enterDelay={1000}
-                    >
-                    	<IconButton
-                    		disabled={isDemo}
-                    		label={t('ADD_TO', { args: [parentName] })}
-                    		color='secondary'
-                    		onClick={this.props.actions.addItemToItem.bind(this, { item: item, toAdd: this.props.parentItem, authSig: this.props.account._authSig })}
-                    	>
-                    		<AddCircleOutlineIcon />
-                    	</IconButton>
-                    </Tooltip>
-    			}
-    		</span>
-    	)
-    }
+				{(this.props.addToItem && parentItem) &&
+					<Tooltip
+						tile={t('ADD_TO', { args: [parentName] })}
+						// placement='top'
+						enterDelay={1000}
+					>
+						<IconButton
+							disabled={isDemo}
+							label={t('ADD_TO', { args: [parentName] })}
+							color='secondary'
+							onClick={actions.addItemToItem.bind(this, { item: item, toAdd: this.props.parentItem, authSig: account.wallet.authSig })}
+						>
+							<AddCircleOutlineIcon />
+						</IconButton>
+					</Tooltip>
+				}
+			</span>
+		)
+	}
 
-    renderRows = (items) =>
-    	<Rows
-    		side={this.props.side}
-    		item={items}
-    		rows={items}
-    		// multiSelectable={false}
-    		// selectable={false}
-    		rowRenderer={this.renderTableRow}
-    		tableHeadRenderer={this.renderTableHead}
-    	/>
+	renderRows = (items) =>
+		<Rows
+			side={this.props.side}
+			item={items}
+			rows={items}
+			itemType={this.props.itemType}
+			// multiSelectable={false}
+			// selectable={false}
+			rowRenderer={this.renderTableRow}
+			tableHeadRenderer={this.renderTableHead}
+		/>
 
-    renderCards = (items) => {
-    	return (<List
-    		itemRenderer={this.renderCard}
-    		list={items}
-    		side={this.props.side}
-    	/>)
-    }
+	renderCards = (items) => {
+		return (<List
+			itemRenderer={this.renderCard}
+			list={items}
+			side={this.props.side}
+			itemType={this.props.itemType}
+		/>)
+	}
 
-    render() {
-    	return (
-    		<ListWithControls
-    			{...this.props}
-    			items={this.props.items}
-    			viewModeId={this.props.viewModeId}
-    			archive
-    			renderRows={this.renderRows}
-    			renderCards={this.renderCards}
-    		/>
-    	)
-    }
+	render() {
+		return (
+			<ListWithControls
+				{...this.props}
+				items={this.props.items}
+				viewModeId={this.props.viewModeId}
+				archive
+				renderRows={this.renderRows}
+				renderCards={this.renderCards}
+			/>
+		)
+	}
 }
 
 ItemsList.propTypes = {
@@ -294,7 +300,7 @@ ItemsList.propTypes = {
 	viewModeId: PropTypes.string.isRequired,
 	header: PropTypes.string.isRequired,
 	objModel: PropTypes.func.isRequired,
-	itemsType: PropTypes.number.isRequired,
+	itemType: PropTypes.string.isRequired,
 	sortProperties: PropTypes.array.isRequired
 }
 
