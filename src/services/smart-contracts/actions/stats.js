d9ee5ad5e005ee542520a5e36481a400f563d66b
Ivo <ivo.paunov@gmail.com>
2019-04-12 10:57:27 +0300

    getting  and formatting acount stats

--- src/services/smart-contracts/actions/stats.js
+++ src/services/smart-contracts/actions/stats.js
@@ -1,4 +1,8 @@
 import { getEthers } from 'services/smart-contracts/ethers'
+import { constants } from 'adex-models'
+import { utils, Contract } from 'ethers'
+const { formatEther, formatUnits } = utils
+const privilegesNames = constants.valueToKey(constants.IdentityPrivilegeLevel)
 
 export async function getAddressBalances({ address, authType }) {
 	const {
@@ -20,4 +24,53 @@ export async function getAddressBalances({ address, authType }) {
 	}
 
 	return formated
+}
+
+export async function getAccountStats({ account }) {
+	const { wallet, identity } = account
+	const {
+		provider,
+		Dai,
+		Identity
+	} = await getEthers(wallet.authType)
+
+	const identityContract = new Contract(identity.address, Identity.abi, provider)
+
+	const calls = [
+		provider.getBalance(wallet.address),
+		Dai.balanceOf(wallet.address),
+		Dai.balanceOf(identity.address),
+		identityContract.privileges(wallet.address)
+	]
+
+	const [
+		walletBalanceEth,
+		walletBalanceDai,
+		identityBalanceDai,
+		walletPrivileges
+	]
+		= await Promise.all(calls)
+
+	// BigNumber values for balances
+	const raw = {
+		walletBalanceEth,
+		walletBalanceDai,
+		identityBalanceDai,
+		walletPrivileges
+	}
+
+	const formated = {
+		walletAddress: wallet.address,
+		walletAuthType: wallet.authType,
+		walletPrivilege: privilegesNames[walletPrivileges],
+		walletBalanceEth: formatEther(walletBalanceEth),
+		walletBalanceDai: formatUnits(walletBalanceDai, 18),
+		identityAddress: identity.address,
+		identityBalanceDai: formatUnits(identityBalanceDai, 18)
+	}
+
+	return {
+		raw,
+		formated
+	}
 }
