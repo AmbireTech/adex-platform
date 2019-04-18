38775e063728ed06e2e09839a7e8c91c2d5b819f
Ivo <ivo.paunov@gmail.com>
2019-04-19 01:56:25 +0300

    add formaters

---
+++ src/helpers/formatters.js
@@ -0,0 +1,22 @@
+import { formatUnits, commify } from 'ethers/utils'
+import MomentUtils from '@date-io/moment'
+const moment = new MomentUtils()
+
+const DEFAULT_DATETIME_FORMAT = 'YYYY-MM-DD HH:mm'
+
+export const formatDateTime = (timestamp, format = DEFAULT_DATETIME_FORMAT) => {
+	if(!timestamp) {
+		return 'no date'
+	}
+	const date = moment.date(timestamp)
+	return date.format(format)
+}
+
+export const formatTokenAmount = (amountString, decimals = 18, pretty) => {
+	const formatted = formatUnits(amountString, decimals)
+	if (pretty) {
+		return commify(formatted)
+	} else {
+		return formatted
+	}
+}
