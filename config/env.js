c93014d997ceb1710820000e455d36a6bb0a9187
Ivo <ivo.paunov@gmail.com>
2019-04-19 23:39:11 +0300

    update env.js

--- config/env.js
+++ config/env.js
@@ -93,7 +93,8 @@ function getClientEnvironment(publicUrl) {
 				VALIDATOR_LEADER_ID: process.env.VALIDATOR_LEADER_ID,
 				VALIDATOR_FOLLOWER_URL: process.env.VALIDATOR_FOLLOWER_URL,
 				VALIDATOR_FOLLOWER_ID: process.env.VALIDATOR_FOLLOWER_ID,
-				DAI_TOKEN_ADDR: process.env.DAI_TOKEN_ADDR
+				DAI_TOKEN_ADDR: process.env.DAI_TOKEN_ADDR,
+				ACCESS_CODE_CHECK: process.env.ACCESS_CODE_CHECK
 			}
 		);
 	// Stringify all values so we can feed into Webpack DefinePlugin
