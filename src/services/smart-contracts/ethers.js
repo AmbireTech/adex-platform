4a0013d3e68d5988d0b1e519ab70a52b037ceed7
Ivo <ivo.paunov@gmail.com>
2019-04-21 19:49:16 +0300

    Enable metamask on user action

--- src/services/smart-contracts/ethers.js
+++ src/services/smart-contracts/ethers.js
@@ -3,8 +3,7 @@ import { contracts } from './contractsCfg'
 import { AUTH_TYPES } from 'constants/misc'
 
 const { AdExCore, Identity, DAI, IdentityFactory } = contracts
-
-const localWeb3 = () => {
+const localWeb3 = async () => {
 	const provider = new ethers.providers.JsonRpcProvider(process.env.WEB3_NODE_ADDR)
 	const adexCore = new ethers.Contract(AdExCore.address, AdExCore.abi, provider)
 	const dai = new ethers.Contract(DAI.address, DAI.abi, provider)
@@ -24,19 +23,30 @@ const localWeb3 = () => {
 	return results
 }
 
-// TODO: GET get injected web3 on user action
-const getInjectedWeb3 = new Promise(function (resolve, reject) {
-	// Wait for loading completion to avoid race conditions with web3 injection timing.
-	window.addEventListener('load', function () {
-		console.log('getInjectedWeb3')
+const loadInjectedWeb3 = new Promise((resolve, reject) => {
+	window.addEventListener('load', () => {
 		const ethereum = window.ethereum
-		let provider = null
-		// let mode = null // metamask, and as some point trezor, ledger, ...
-		let adexCore = null
-		let dai = null
-		let identityFactory = null
+		const web3 = window.web3
+		return resolve({
+			ethereum,
+			web3
+		})
+	})
+})
 
-		if (ethereum) {
+const getInjectedWeb3 = async () => {
+	const { web3, ethereum } = await loadInjectedWeb3.then()
+	let provider = null
+	// let mode = null // metamask, and as some point trezor, ledger, ...
+	let adexCore = null
+	let dai = null
+	let identityFactory = null
+
+	if (ethereum) {
+		try {
+			const enabled = await ethereum.enable()
+			console.log('Injected ethereum detected.', enabled)
+
 			provider = new ethers.providers.Web3Provider(ethereum)
 			adexCore = new ethers.Contract(AdExCore.address, AdExCore.abi, provider)
 			dai = new ethers.Contract(DAI.address, DAI.abi, provider)
@@ -44,35 +54,6 @@ const getInjectedWeb3 = new Promise(function (resolve, reject) {
 				IdentityFactory.address,
 				IdentityFactory.abi,
 				provider)
-
-			ethereum.enable()
-				.then(() => {
-					console.log('Injected web3 detected.')
-					const results = {
-						provider: provider,
-						AdExCore: adexCore,
-						Identity: Identity,
-						Dai: dai,
-						IdentityFactory: identityFactory
-					}
-
-					resolve(results)
-				})
-				.catch(err => {
-					reject('User denied account access...')
-				})
-		}
-		// Legacy dapp browsers...
-		else if (window.web3) {
-			provider = new ethers.providers.Web3Provider(window.web3.currentProvider)
-			adexCore = new ethers.Contract(AdExCore.address, AdExCore.abi, provider)
-			dai = new ethers.Contract(DAI.address, DAI.abi, provider)
-			identityFactory = new ethers.Contract(
-				IdentityFactory.address,
-				IdentityFactory.abi,
-				provider)
-
-			console.log('Injected legacy web3 detected.')
 			const results = {
 				provider: provider,
 				AdExCore: adexCore,
@@ -81,30 +62,54 @@ const getInjectedWeb3 = new Promise(function (resolve, reject) {
 				IdentityFactory: identityFactory
 			}
 
-			resolve(results)
-		} else {
-			console.log('Non-Ethereum browser detected.');
-			reject('Non-Ethereum browser detected.');
+			return results
+
+		} catch (err) {
+			console.error(err.message)
+			throw new Error(err.message)
 		}
-	})
-})
+	}
+	// Legacy dapp browsers...
+	else if (web3) {
+		provider = new ethers.providers.Web3Provider(window.web3.currentProvider)
+		adexCore = new ethers.Contract(AdExCore.address, AdExCore.abi, provider)
+		dai = new ethers.Contract(DAI.address, DAI.abi, provider)
+		identityFactory = new ethers.Contract(
+			IdentityFactory.address,
+			IdentityFactory.abi,
+			provider)
+
+		console.log('Injected legacy web3 detected.')
+		const results = {
+			provider: provider,
+			AdExCore: adexCore,
+			Identity: Identity,
+			Dai: dai,
+			IdentityFactory: identityFactory
+		}
+
+		return results
+	} else {
+		console.error('Non-Ethereum browser detected.')
+		throw new Error('Non-Ethereum browser detected.')
+	}
+}
 
 const getLocalWeb3 = new Promise(function (resolve, reject) {
 	console.log('getLocalWeb3')
 	resolve(localWeb3())
 })
 
-const getEthers = (mode) => {
+const getEthers = async (mode) => {
 	/* NOTE: use Promise wrapper because despite getWeb3 is Promise itself it is not called by user action
 	*   and this results in Trezor popup block by the browser
 	*/
-	return new Promise((resolve, reject) => {
-		if (mode === AUTH_TYPES.METAMASK.name) {
-			return resolve(getInjectedWeb3)
-		} else {
-			return resolve(getLocalWeb3)
-		}
-	})
+	if (mode === AUTH_TYPES.METAMASK.name) {
+		const injected = await getInjectedWeb3()
+		return injected
+	} else {
+		return getLocalWeb3
+	}
 }
 
 export {
