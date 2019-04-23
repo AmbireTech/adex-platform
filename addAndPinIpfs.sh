2c96921153abe84651259b0b7f6a41ae9386c636
IvoPaunov <ivo.paunov@gmail.com>
2019-04-23 17:32:04 +0300

    fix add to ipfs sh

--- addAndPinIpfs.sh
+++ addAndPinIpfs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/data/adex-dapp
-/snap/bin/ipfs pin add -r $(/snap/bin/ipfs add -r build 2> /dev/null | tail -n1 | cut -f2 -d " ")
+export IPFS_PATH=/var/ipfs
+ipfs pin add -r $(ipfs add -r build 2> /dev/null | tail -n1 | cut -f2 -d " ")
