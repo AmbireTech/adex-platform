10a1b6a0f0965e242342a8af86230349c4c4bc29
Ivo paunov <ivo.paunov@gmail.com>
2019-05-20 20:43:40 +0300

    validator actions get auth and senf msgs

--- src/services/adex-validator/actions.js
+++ src/services/adex-validator/actions.js
@@ -1,12 +1,21 @@
 import Requester from 'services/requester'
 import { translate } from 'services/translations/translations'
+import { getEthers } from 'services/smart-contracts/ethers'
+import { getSigner } from 'services/smart-contracts/actions/ethers'
+import ewt from './ewt'
 
+const BEARER_PREFIX = 'Bearer '
 
-const getRequester = ({campaign}) => {
+const getRequesters = ({ campaign }) => {
 	const leader = (campaign.validators || campaign.spec.validators)[0]
-	const requester = new Requester({baseUrl: leader.url})
+	const follower = (campaign.validators || campaign.spec.validators)[1]
+	const leaderRequester = new Requester({ baseUrl: leader.url })
+	const followerRequester = new Requester({ baseUrl: follower.url })
 
-	return requester
+	return {
+		leader: { requester: leaderRequester, validator: leader },
+		follower: { requester: followerRequester, validator: follower }
+	}
 }
 
 const processResponse = (res) => {
@@ -30,11 +39,73 @@ const processResponse = (res) => {
 		})
 }
 
-export const lastApprovedState = ({ campaign }) => {
-	const requester = getRequester({campaign})
+const getAuthToken = async ({ account, validator }) => {
+	const { identity, wallet } = account
+	const existingAuth = (identity.validatorAuthTokens || {})[validator.id]
+	if (existingAuth) {
+		return existingAuth
+	}
 
-	return requester.fetch({
+	const { provider } = await getEthers(wallet.authType)
+	const signer = await getSigner({ wallet, provider })
+
+	const payload = {
+		id: validator.id,
+		identity: identity.address,
+		era: Math.floor(Date.now() / 60000)
+	}
+
+	const token = ewt.sign(signer, payload)
+
+	return token
+}
+
+const sendMessage = async ({ account, campaign, options }) => {
+	const { follower, leader } = getRequesters({ campaign })
+
+	const followerAuthToken = await getAuthToken({ account, validator: follower.validator })
+	const leaderAuthToken = await getAuthToken({ account, validator: follower.validator })
+	const followerResult = await follower.requester.fetch({
+		...options,
+		headers: { ...options.headers, authorization: BEARER_PREFIX + followerAuthToken }
+	}).then(processResponse)
+
+	const leaderResult = await leader.requester.fetch({
+		...options,
+		headers: { ...options.headers, authorization: BEARER_PREFIX + leaderAuthToken }
+	}).then(processResponse)
+
+
+	return {
+		authTokens: {
+			[follower.validator.id]: followerAuthToken,
+			[leader.validator.id]: leaderAuthToken,
+		},
+		results: {
+			followerResult,
+			leaderResult
+		}
+	}
+}
+
+export const lastApprovedState = ({ campaign }) => {
+	const { leader } = getRequesters({ campaign })
+
+	return leader.requester.fetch({
 		route: `channel/${campaign.id}/last-approved`,
 		method: 'GET'
 	}).then(processResponse)
 }
+
+export const closeCampaign = ({ account, campaign }) => {
+	const options = {
+		route: `channel/${campaign.id}/events/close`,
+		method: 'POST',
+		body: JSON.stringify([{ type: 'CLOSE' }]),
+		headers: { 'Content-Type': 'application/json' }
+	}
+
+	return sendMessage({ account, campaign, options })
+}
+
+
