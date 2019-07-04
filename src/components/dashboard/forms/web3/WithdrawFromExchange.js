e5bf04e94b0302aa3e6b1cfe96bbd39070a1a17d
Ivo paunov <ivo.paunov@gmail.com>
2019-07-04 19:39:32 +0300

    withdrow step update

--- src/components/dashboard/forms/web3/WithdrawFromExchange.js
+++ src/components/dashboard/forms/web3/WithdrawFromExchange.js
@@ -6,7 +6,7 @@ import actions from 'actions'
 // import Translate from 'components/translate/Translate'
 import NewTransactionHoc from './TransactionHoc'
 import TextField from '@material-ui/core/TextField'
-import { validateNumber } from 'helpers/validators'
+import { validateNumber, isEthAddress } from 'helpers/validators'
 
 class WithdrawFromExchange extends Component {
 
@@ -20,44 +20,65 @@ class WithdrawFromExchange extends Component {
 		}
 	}
 
-    validateAmount = (numStr, dirty) => {
-    	let isValid = validateNumber(numStr)
-    	let msg = 'ERR_INVALID_AMOUNT_VALUE'
-    	let errMsgArgs = []
-    	if (isValid && (parseFloat(numStr) > parseFloat(this.props.exchangeAvailable))) {
-    		isValid = false
-    		msg = 'ERR_MAX_AMOUNT_TO_WITHDRAW'
-    		errMsgArgs = [this.props.exchangeAvailable, 'ADX']
-    	}
+	validateAmount = (numStr, dirty) => {
+		let isValid = validateNumber(numStr)
+		let msg = 'ERR_INVALID_AMOUNT_VALUE'
+		let errMsgArgs = []
+		if (isValid && (parseFloat(numStr) > parseFloat(this.props.exchangeAvailable))) {
+			isValid = false
+			msg = 'ERR_MAX_AMOUNT_TO_WITHDRAW'
+			errMsgArgs = [this.props.exchangeAvailable, 'ADX']
+		}
 
-    	this.props.validate('withdrawAmount', { isValid: isValid, err: { msg: msg, args: errMsgArgs }, dirty: dirty })
-    }
+		this.props.validate('withdrawAmount', { isValid: isValid, err: { msg: msg, args: errMsgArgs }, dirty: dirty })
+	}
 
-    render() {
-    	const { transaction, t, invalidFields, exchangeAvailable, handleChange } = this.props
-    	const errAmount = invalidFields['withdrawAmount']
+	validateAddress = (addr, dirty) => {
+		const isValid = isEthAddress(addr)
+		const msg = 'ERR_INVALID_ETH_ADDRESS'
+		this.props.validate('withdrawTo', { isValid: isValid, err: { msg: msg }, dirty: dirty })
+	}
 
-    	return (
-    		<div>
-    			<div> {t('EXCHANGE_CURRENT_ADX_BALANCE_AVAILABLE')} {exchangeAvailable} </div>
-    			<TextField
-    				type='text'
-    				fullWidth
-    				required
-    				label={t('TOKENS_TO_WITHDRAW')}
-    				name='withdrawAmount'
-    				value={transaction.withdrawAmount || ''}
-    				onChange={(ev) => handleChange('withdrawAmount', ev.target.value)}
-    				onBlur={() => this.validateAmount(transaction.withdrawAmount, true)}
-    				onFocus={() => this.validateAmount(transaction.withdrawAmount, false)}
-    				error={errAmount && !!errAmount.dirty}
-    				helperText={errAmount && !!errAmount.dirty ?
-    					errAmount.errMsg : t('MAX_AMOUNT_TO_WITHDRAW', { args: [exchangeAvailable, 'ADX'] })
-    				}
-    			/>
-    		</div>
-    	)
-    }
+	render() {
+		const { transaction, t, invalidFields, exchangeAvailable, handleChange } = this.props
+		const { withdrawTo, withdrawAmount } = transaction || {}
+		const errAmount = invalidFields['withdrawAmount']
+		const errAddr = invalidFields['withdrawTo']
+
+		return (
+			<div>
+				<div> {t('EXCHANGE_CURRENT_DAI_BALANCE_AVAILABLE_ON_IDENTITY')} {exchangeAvailable} </div>
+				<TextField
+					type='text'
+					required
+					fullWidth
+					label={t('WITHDRAW_TO')}
+					name='withdrawTo'
+					value={withdrawTo || ''}
+					onChange={(ev) => handleChange('withdrawTo', ev.target.value)}
+					onBlur={() => this.validateAddress(withdrawTo, true)}
+					onFocus={() => this.validateAddress(withdrawTo, false)}
+					error={errAddr && !!errAddr.dirty}
+					helperText={errAddr && !!errAddr.dirty ? errAddr.errMsg : ''}
+				/>
+				<TextField
+					type='text'
+					fullWidth
+					required
+					label={t('TOKENS_TO_WITHDRAW')}
+					name='withdrawAmount'
+					value={withdrawAmount || ''}
+					onChange={(ev) => handleChange('withdrawAmount', ev.target.value)}
+					onBlur={() => this.validateAmount(withdrawAmount, true)}
+					onFocus={() => this.validateAmount(withdrawAmount, false)}
+					error={errAmount && !!errAmount.dirty}
+					helperText={errAmount && !!errAmount.dirty ?
+						errAmount.errMsg : t('MAX_AMOUNT_TO_WITHDRAW', { args: [exchangeAvailable, 'ADX'] })
+					}
+				/>
+			</div>
+		)
+	}
 }
 
 WithdrawFromExchange.propTypes = {
