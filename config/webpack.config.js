ca75b841a88492b595b0a6ce473ccb18c913730d
Ivo paunov <ivo.paunov@gmail.com>
2019-08-06 16:44:49 +0300

    add external solc for browser

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -622,5 +622,8 @@ module.exports = function (webpackEnv) {
 		// Turn off performance processing because we utilize
 		// our own hints via the FileSizeReporter
 		performance: false,
+		externals: {
+			solcBrowser: 'Module'
+		}
 	};
 };
