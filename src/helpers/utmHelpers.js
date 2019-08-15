da76708ae4e4013a0dafc9685cc421cff143e9a3
Rangel Stoilov <rangelstoilov@gmail.com>
2019-08-15 18:27:44 +0300

    Fix that will not remove custom source & medium

--- src/helpers/utmHelpers.js
+++ src/helpers/utmHelpers.js
@@ -1,25 +1,28 @@
 import url from 'url';
 import { UTM_ATTRIBUTES, UTM_DEFAULT_VALUE } from 'constants/misc'
 
-export const addUrlUtmTracking = ({ targetUrl, source, medium, campaign, content, removeFromUrl }) => {
+export const addUrlUtmTracking = ({ targetUrl, campaign, content, removeFromUrl }) => {
 	if (!!targetUrl) {
 		const URL = url.parse(targetUrl, true)
 		delete URL.search
 		if (URL.protocol && URL.host) {
 			if (removeFromUrl) {
-				URL.query[UTM_ATTRIBUTES.SOURCE] === UTM_DEFAULT_VALUE.SOURCE 
-				&& delete URL.query[UTM_ATTRIBUTES.SOURCE]
-				URL.query[UTM_ATTRIBUTES.MEDIUM] === UTM_DEFAULT_VALUE.MEDIUM 
-				&& delete URL.query[UTM_ATTRIBUTES.MEDIUM]
-				URL.query[UTM_ATTRIBUTES.CAMPAIGN] === UTM_DEFAULT_VALUE.CAMPAIGN 
-				&& delete URL.query[UTM_ATTRIBUTES.CAMPAIGN]
-				URL.query[UTM_ATTRIBUTES.CONTENT] === UTM_DEFAULT_VALUE.CONTENT 
-				&& delete URL.query[UTM_ATTRIBUTES.CONTENT]
+				URL.query[UTM_ATTRIBUTES.SOURCE] === UTM_DEFAULT_VALUE.SOURCE
+					&& delete URL.query[UTM_ATTRIBUTES.SOURCE]
+				URL.query[UTM_ATTRIBUTES.MEDIUM] === UTM_DEFAULT_VALUE.MEDIUM
+					&& delete URL.query[UTM_ATTRIBUTES.MEDIUM]
+				URL.query[UTM_ATTRIBUTES.CAMPAIGN] === UTM_DEFAULT_VALUE.CAMPAIGN
+					&& delete URL.query[UTM_ATTRIBUTES.CAMPAIGN]
+				URL.query[UTM_ATTRIBUTES.CONTENT] === UTM_DEFAULT_VALUE.CONTENT
+					&& delete URL.query[UTM_ATTRIBUTES.CONTENT]
 				return url.format(URL)
 			}
 			const utmParams = {
-				[UTM_ATTRIBUTES.SOURCE]: (source ? source : UTM_DEFAULT_VALUE.SOURCE),
-				[UTM_ATTRIBUTES.MEDIUM]: (medium ? medium : UTM_DEFAULT_VALUE.MEDIUM),
+				//This will not remove custom added source and medium
+				[UTM_ATTRIBUTES.SOURCE]: (URL.query[UTM_ATTRIBUTES.SOURCE] ? 
+					URL.query[UTM_ATTRIBUTES.SOURCE] : UTM_DEFAULT_VALUE.SOURCE),
+				[UTM_ATTRIBUTES.MEDIUM]: (URL.query[UTM_ATTRIBUTES.MEDIUM] ? 
+					URL.query[UTM_ATTRIBUTES.MEDIUM] : UTM_DEFAULT_VALUE.MEDIUM),
 				[UTM_ATTRIBUTES.CAMPAIGN]: (campaign ? campaign : UTM_DEFAULT_VALUE.CAMPAIGN),
 				[UTM_ATTRIBUTES.CONTENT]: (content ? content : UTM_DEFAULT_VALUE.CONTENT),
 			}
