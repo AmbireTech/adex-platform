81eedb770df13a85e2a8117beb35cd6eaafd625b
Rangel Stoilov <rangelstoilov@gmail.com>
2019-08-15 18:20:31 +0300

    delete added if the value is not default

--- src/helpers/utmHelpers.js
+++ src/helpers/utmHelpers.js
@@ -1,18 +1,29 @@
 import url from 'url';
 import { UTM_ATTRIBUTES, UTM_DEFAULT_VALUE } from 'constants/misc'
 
-export const addUrlUtmTracking = (targetUrl, source, medium, campaign, content) => {
+export const addUrlUtmTracking = ({ targetUrl, source, medium, campaign, content, removeFromUrl }) => {
 	if (!!targetUrl) {
 		const URL = url.parse(targetUrl, true)
+		delete URL.search
 		if (URL.protocol && URL.host) {
+			if (removeFromUrl) {
+				URL.query[UTM_ATTRIBUTES.SOURCE] === UTM_DEFAULT_VALUE.SOURCE 
+				&& delete URL.query[UTM_ATTRIBUTES.SOURCE]
+				URL.query[UTM_ATTRIBUTES.MEDIUM] === UTM_DEFAULT_VALUE.MEDIUM 
+				&& delete URL.query[UTM_ATTRIBUTES.MEDIUM]
+				URL.query[UTM_ATTRIBUTES.CAMPAIGN] === UTM_DEFAULT_VALUE.CAMPAIGN 
+				&& delete URL.query[UTM_ATTRIBUTES.CAMPAIGN]
+				URL.query[UTM_ATTRIBUTES.CONTENT] === UTM_DEFAULT_VALUE.CONTENT 
+				&& delete URL.query[UTM_ATTRIBUTES.CONTENT]
+				return url.format(URL)
+			}
 			const utmParams = {
 				[UTM_ATTRIBUTES.SOURCE]: (source ? source : UTM_DEFAULT_VALUE.SOURCE),
 				[UTM_ATTRIBUTES.MEDIUM]: (medium ? medium : UTM_DEFAULT_VALUE.MEDIUM),
 				[UTM_ATTRIBUTES.CAMPAIGN]: (campaign ? campaign : UTM_DEFAULT_VALUE.CAMPAIGN),
 				[UTM_ATTRIBUTES.CONTENT]: (content ? content : UTM_DEFAULT_VALUE.CONTENT),
 			}
-			URL.query = { ...URL.query, ...utmParams };
-			delete URL.search
+			URL.query = { ...URL.query, ...utmParams }
 			return url.format(URL)
 		}
 	}
