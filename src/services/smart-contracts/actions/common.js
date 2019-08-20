b239d21c907e1a5c5a15503779be3c1891d21063
Rangel Stoilov <rangelstoilov@gmail.com>
2019-08-20 14:36:45 +0300

    moved inputLoading & fixed getEthers

--- src/services/smart-contracts/actions/common.js
+++ src/services/smart-contracts/actions/common.js
@@ -1,8 +1,9 @@
 import { getEthers } from 'services/smart-contracts/ethers';
+import { AUTH_TYPES } from 'constants/misc'
 
 export const isConnectionLost = async () => {
 	try {
-		const eth = await getEthers()
+		const eth = await getEthers(AUTH_TYPES.METAMASK.name)
 		await eth.provider.getBlockNumber()
 		return false
 	} catch (error) {
