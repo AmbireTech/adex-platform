fa933a0ec5337fcbe06c1f108793e1040d9866fd
Rangel Stoilov <rangelstoilov@gmail.com>
2019-08-21 11:44:45 +0300

    removed txId in validateAddress & clear errors on close

--- src/actions/validationActions.js
+++ src/actions/validationActions.js
@@ -1,15 +1,15 @@
 import { updateSpinner } from './uiActions'
 import { validEthAddress } from '../helpers/validators'
 
-export function validateAddress({ addr, dirty, txId, validate, name }) {
+export function validateAddress({ addr, dirty, validate, name }) {
 	return async function (dispatch) {
 		try {
 			// TODO: make the updateSpinner use some string and not txId
 			if(validate) validate(name, { isValid: false})
-			updateSpinner(txId, dirty)(dispatch)
+			updateSpinner(name, dirty)(dispatch)
 			const { msg } = await validEthAddress({ addr, nonZeroAddr: true, nonERC20: true })
 			const isValid = !msg
-			updateSpinner(txId, false)(dispatch)
+			updateSpinner(name, false)(dispatch)
 			if(validate) validate(name, { isValid: isValid, err: { msg: msg }, dirty: dirty})
 		} catch (error) {
 			console.log(error);
