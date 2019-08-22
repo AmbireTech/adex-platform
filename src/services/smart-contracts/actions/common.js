72f961cb8aa49b5e6e9fc5c747ebb682072c6943
Rangel Stoilov <rangelstoilov@gmail.com>
2019-08-22 17:53:34 +0300

    commit fix format

--- src/services/smart-contracts/actions/common.js
+++ src/services/smart-contracts/actions/common.js
@@ -1,6 +1,6 @@
-import { getEthers } from 'services/smart-contracts/ethers';
+import { getEthers } from 'services/smart-contracts/ethers'
 
-export const isConnectionLost = async (authType) => {
+export const isConnectionLost = async authType => {
 	try {
 		// When we remove Metamask
 		// if in 5 sec doesn't connect will
@@ -8,8 +8,11 @@ export const isConnectionLost = async (authType) => {
 		const eth = await Promise.race([
 			getEthers(authType),
 			new Promise.reject(() => {
-				setTimeout(()=> { new Error('Timed out')}, 5000)
-			})])
+				setTimeout(() => {
+					new Error('Timed out')
+				}, 5000)
+			}),
+		])
 		await eth.provider.getBlockNumber()
 		return false
 	} catch (error) {
