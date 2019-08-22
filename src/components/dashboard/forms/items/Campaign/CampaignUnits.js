6e3d2d1663c506b0961e7d3da2d873776e1dcf6b
Rangel Stoilov <rangelstoilov@gmail.com>
2019-08-22 12:01:15 +0300

    rollback of changes made in campaignUnits.js

--- src/components/dashboard/forms/items/Campaign/CampaignUnits.js
+++ src/components/dashboard/forms/items/Campaign/CampaignUnits.js
@@ -60,7 +60,6 @@ class CampaignUnits extends Component {
 
 	render() {
 		const { adUnitsArray } = this.props
-		// const adUnitsArray = []
 		return (
 			<div>
 				<Grid
@@ -70,25 +69,21 @@ class CampaignUnits extends Component {
 					<Grid item sm={12}>
 
 						<ContentBody>
-							{(adUnitsArray && adUnitsArray.length) ? 
-								(<ItemsList
-									objModel={AdUnit}
-									selectMode
-									selectedItems={this.state.selected}
-									onSelect={(unit, checked) => {
-										this.handleSelect(unit, checked)
-									}}
-									items={adUnitsArray}
-									listMode='rows'
-									itemType={'AdUnit'}
-									viewModeId={'newCampaignAdUnits'}
-									sortProperties={SORT_PROPERTIES_ITEMS}
-									filterProperties={FILTER_PROPERTIES_ITEMS}
-									noActions
-								/>)
-								: 
-								(<div>Should have at least one ad unit!</div>)
-							}
+							<ItemsList
+								objModel={AdUnit}
+								selectMode
+								selectedItems={this.state.selected}
+								onSelect={(unit, checked) => {
+									this.handleSelect(unit, checked)
+								}}
+								items={adUnitsArray}
+								listMode='rows'
+								itemType={'AdUnit'}
+								viewModeId={'newCampaignAdUnits'}
+								sortProperties={SORT_PROPERTIES_ITEMS}
+								filterProperties={FILTER_PROPERTIES_ITEMS}
+								noActions
+							/>
 						</ContentBody>
 					</Grid>
 				</Grid>
