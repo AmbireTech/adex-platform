adb68c6ddde67e38ad4a9beecba1dbe4c7c50d44
Ivo paunov <ivo.paunov@gmail.com>
2019-08-27 16:19:08 +0300

    use onBeforeOpen for new NewCloneUnitDialog

--- src/components/dashboard/containers/ItemsList/ItemsList.js
+++ src/components/dashboard/containers/ItemsList/ItemsList.js
@@ -218,7 +218,7 @@ class ItemsList extends Component {
 					>
 						<span>
 							<NewCloneUnitDialog
-								onClick={() =>
+								onBeforeOpen={() =>
 									actions.cloneItem({ item, itemType, objModel: AdUnit })
 								}
 								variant='extended'
