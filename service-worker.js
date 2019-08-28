24d3881b36dc262d2b50c65806e349902917c9cc
Ivo paunov <ivo.paunov@gmail.com>
2019-08-28 15:12:52 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae3bee3321a4614b6b39c24e1248f135.js"
+  "/precache-manifest.4f1dd0ba466fefaaed93caaf25e00b6f.js"
 );
 
 self.addEventListener('message', (event) => {
