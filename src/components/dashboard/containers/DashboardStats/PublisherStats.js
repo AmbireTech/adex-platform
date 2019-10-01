bf5fff80be934ad4d9fc4f298af63aefddefc5b7
Ivo paunov <ivo.paunov@gmail.com>
2019-10-01 15:55:08 +0300

    PublisherStats by timeframe

--- src/components/dashboard/containers/DashboardStats/PublisherStats.js
+++ src/components/dashboard/containers/DashboardStats/PublisherStats.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PublisherSimpleStatistics } from 'components/dashboard/charts/revenue'
+import Dropdown from 'components/common/dropdown'
 
-export const PublisherStats = ({ aggregates, t }) => {
-	// console.log('data', data)
+const timeFrames = [
+	{ label: 'hour', value: 'hour' },
+	{ label: 'day', value: 'day' },
+	{ label: 'week', value: 'week' },
+	{ label: 'month', value: 'month' },
+	{ label: 'year', value: 'year' },
+]
+
+export function PublisherStats({ aggregates, t }) {
+	const [timeframe, setTimeframe] = useState('hour')
+	const aggr = aggregates.publisher || {}
+
 	return (
 		<div>
+			<div>
+				<Dropdown
+					label={t('SELECT_TIMEFRAME')}
+					onChange={val => setTimeframe(val)}
+					source={timeFrames}
+					value={timeframe}
+					htmlId='page-size-select'
+				/>
+			</div>
 			<PublisherSimpleStatistics
-				data={aggregates[0].aggr}
-				options={{ title: t(aggregates[0].timeframe) }}
+				data={(aggr[timeframe] || {}).aggr}
+				options={{ title: t(timeframe) }}
 				t={t}
 			/>
 		</div>
