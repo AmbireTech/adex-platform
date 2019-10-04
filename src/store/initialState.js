9d15e39b01e16112a904a1e9cabf065f9ac9cbda
Ivo <ivo.paunov@gmail.com>
2019-10-04 23:46:00 +0300

    analytics initial state

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,4 +1,31 @@
 import { AdUnit, AdSlot, Campaign, Account } from 'adex-models'
+import {
+	VALIDATOR_ANALYTICS_SIDES,
+	VALIDATOR_ANALYTICS_EVENT_TYPES,
+	VALIDATOR_ANALYTICS_METRICS,
+	VALIDATOR_ANALYTICS_TIMEFRAMES,
+} from 'constants/misc'
+
+export const getValidatorAnalyticsInitialState = () => {
+	const initialState = {}
+
+	VALIDATOR_ANALYTICS_SIDES.forEach(side =>
+		VALIDATOR_ANALYTICS_EVENT_TYPES.forEach(eventType =>
+			VALIDATOR_ANALYTICS_METRICS.forEach(metric =>
+				VALIDATOR_ANALYTICS_TIMEFRAMES.forEach(timeframe => {
+					initialState[side] = initialState[side] || {}
+					initialState[side][eventType] = initialState[side][eventType] || {}
+					initialState[side][eventType][metric] =
+						initialState[side][eventType][metric] || {}
+					initialState[side][eventType][metric][timeframe] =
+						initialState[side][eventType][metric][timeframe] || {}
+				})
+			)
+		)
+	)
+
+	return initialState
+}
 
 let initialState = {
 	account: new Account(),
@@ -39,7 +66,12 @@ let initialState = {
 	},
 	web3Transactions: {},
 	tags: {},
-	identity: {},
+	identity: {
+		email: 'klatikureca@gmail.com',
+		emailCheck: 'klatikureca@gmail.com',
+		password: 'PassWord123',
+		passwordCheck: 'PassWord123',
+	},
 	ethNetwork: {
 		networkId: null,
 		gasData: {},
@@ -50,6 +82,9 @@ let initialState = {
 		market: {},
 		validators: {},
 	},
+	analytics: getValidatorAnalyticsInitialState(),
 }
+
+console.log('initialState', initialState)
 
 export default initialState
