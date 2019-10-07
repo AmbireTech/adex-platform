1f1e8d6b35a3c8fd891ac69fd63c2028dd11cd67
Rangel Stoilov <rangelstoilov@gmail.com>
2019-10-07 14:19:00 +0300

    removed unused plugin config

--- src/components/dashboard/charts/simplified.js
+++ src/components/dashboard/charts/simplified.js
@@ -84,35 +84,6 @@ export const SimpleStatistics = ({
 			display: true,
 			text: options.title,
 		},
-		plugins: {
-			crosshair: {
-				line: {
-					color: '#F66', // crosshair line color
-					width: 1, // crosshair line width
-				},
-				sync: {
-					enabled: true, // enable trace line syncing with other charts
-					group: 1, // chart group
-					suppressTooltips: false, // suppress tooltips when showing a synced tracer
-				},
-				zoom: {
-					enabled: true, // enable zooming
-					zoomboxBackgroundColor: 'rgba(66,133,244,0.2)', // background color of zoom box
-					zoomboxBorderColor: '#48F', // border color of zoom box
-					zoomButtonText: 'Reset Zoom', // reset zoom button text
-					zoomButtonClass: 'reset-zoom', // reset zoom button class
-				},
-				callbacks: {
-					beforeZoom: function(start, end) {
-						// called before zoom, return false to prevent zoom
-						return true
-					},
-					afterZoom: function(start, end) {
-						// called after zoom
-					},
-				},
-			},
-		},
 		tooltips: {
 			mode: 'nearest',
 			intersect: false,
