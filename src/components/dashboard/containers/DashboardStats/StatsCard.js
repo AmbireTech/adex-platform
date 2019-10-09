df4e80ac412511c9913a96f0fb962ed6e6316f63
Rangel Stoilov <rangelstoilov@gmail.com>
2019-10-09 15:20:13 +0300

    fix Before data is loaded dashboard amount is set to undefined

--- src/components/dashboard/containers/DashboardStats/StatsCard.js
+++ src/components/dashboard/containers/DashboardStats/StatsCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Box from '@material-ui/core/Box'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
@@ -8,7 +10,15 @@ import { styles } from './styles'
 import classnames from 'classnames'
 
 const StatsCard = props => {
-	const { classes, title, subtitle, linkCard, children, onClick } = props
+	const {
+		classes,
+		title,
+		subtitle,
+		linkCard,
+		loading,
+		children,
+		onClick,
+	} = props
 
 	return (
 		<Card
@@ -18,20 +28,25 @@ const StatsCard = props => {
 			})}
 			onClick={onClick}
 		>
-			<CardContent>
-				{title && (
-					<Typography variant='h5' noWrap>
-						{title}
-					</Typography>
-				)}
+			<Box display='flex' alignItems='center'>
+				<Box>
+					<CardContent>
+						{title && (
+							<Typography variant='h5' noWrap>
+								{title}
+							</Typography>
+						)}
 
-				{subtitle && (
-					<Typography component='p' noWrap>
-						{subtitle}
-					</Typography>
-				)}
-				{children}
-			</CardContent>
+						{subtitle && (
+							<Typography component='p' noWrap>
+								{subtitle}
+							</Typography>
+						)}
+						{children}
+					</CardContent>
+				</Box>
+				<Box>{loading && <CircularProgress />}</Box>
+			</Box>
 		</Card>
 	)
 }
