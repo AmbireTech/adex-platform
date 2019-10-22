efde0af18bdd35f53ae66bb3872ea5879b4361e2
Ivo paunov <ivo.paunov@gmail.com>
2019-10-22 14:27:24 +0300

    remove unused useEffect

--- src/components/signin/identity/Identity.js
+++ src/components/signin/identity/Identity.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Button from '@material-ui/core/Button'
 import IdentityHoc from './IdentityHoc'
 import IdentityContractAddressEthDeploy from './IdentityContractAddressEthDeploy'
