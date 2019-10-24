fbcb52a03e759fb0527479aac1df3d744b13dde0
Ivo paunov <ivo.paunov@gmail.com>
2019-10-24 20:13:48 +0300

    Root with hooks

--- src/components/App/Root.js
+++ src/components/App/Root.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import actions from 'actions'
-import { Route, Switch, Redirect, withRouter } from 'react-router'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import {
+	onMetamaskAccountChange,
+	metamaskNetworkCheck,
+	metamaskAccountCheck,
+	execute,
+} from 'actions'
+import { Route, Switch, Redirect } from 'react-router'
 import Dashboard from 'components/dashboard/dashboard/Dashboard'
 import ConnectHoc from 'components/signin/ConnectHoc'
 import {
@@ -17,11 +20,8 @@ import SideSelect from 'components/signin/side-select/SideSelect'
 import PageNotFound from 'components/page_not_found/PageNotFound'
 import Home from 'components/signin/Home'
 import Translate from 'components/translate/Translate'
-// import { getSig } from 'services/auth/auth'
-import { AUTH_TYPES } from 'constants/misc'
-import { logOut } from 'services/store-data/auth'
 import JustDialog from 'components/common/dialog/JustDialog'
-import { getEthers } from 'services/smart-contracts/ethers'
+import { selectAuth, selectLocation } from 'selectors'
 
 const ConnectedCreateGrantIdentity = ConnectHoc(JustDialog(CreateGrantIdentity))
 const ConnectedGrantLogin = ConnectHoc(JustDialog(LoginGrantIdentity))
@@ -31,7 +31,7 @@ const ConnectedLoginStandardIdentity = ConnectHoc(
 )
 const ConnectedRoot = ConnectHoc(Home)
 
-function PrivateRoute({ component: Component, auth, ...other }) {
+const PrivateRoute = ({ component: Component, auth, ...other }) => {
 	return (
 		<Route
 			{...other}
@@ -46,130 +46,67 @@ function PrivateRoute({ component: Component, auth, ...other }) {
 	)
 }
 
-class Root extends Component {
-	onMetamaskAccountChange = async accountAddress => {
-		const { account, memoryIdentity } = this.props
-		const { identityContractOwner } = memoryIdentity
-		const { authType } = account.wallet
-		if (authType === AUTH_TYPES.METAMASK.name || !authType || !accountAddress) {
-			logOut(!identityContractOwner || identityContractOwner === accountAddress)
-		}
-	}
+const Root = () => {
+	const auth = useSelector(selectAuth)
+	const location = useSelector(selectLocation)
 
-	componentDidCatch(error, info) {
-		// TODO: catch errors
-	}
-
-	componentWillUnmount() {}
-
-	componentDidMount() {
-		const { actions } = this.props
-		const { metamaskNetworkCheck } = actions
-
-		metamaskNetworkCheck()
+	const metamaskChecks = async () => {
+		execute(metamaskNetworkCheck())
+		execute(metamaskAccountCheck())
 		if (window.ethereum) {
 			window.ethereum.on('accountsChanged', accounts => {
 				console.log('acc changed', accounts[0])
-				this.onMetamaskAccountChange(accounts[0])
+				execute(onMetamaskAccountChange(accounts[0]))
 			})
 			window.ethereum.on('networkChanged', network => {
 				console.log('networkChanged', network)
-				metamaskNetworkCheck({ id: network })
+				execute(metamaskNetworkCheck({ id: network }))
 			})
 		}
 	}
 
-	shouldComponentUpdate(nextProps, nextState) {
-		// TODO: check if computedMatch or language change need to update
-		const authChanged = this.props.auth !== nextProps.auth
-		const locationChanged =
-			JSON.stringify(this.props.location) !== JSON.stringify(nextProps.location)
-
-		return authChanged || locationChanged
-	}
-
-	render() {
-		return (
-			<Switch>
-				<PrivateRoute
-					auth={this.props.auth}
-					path='/dashboard/:side'
-					component={Dashboard}
-				/>
-				<PrivateRoute
-					auth={this.props.auth}
-					path='/side-select'
-					component={SideSelect}
-				/>
-				<Route
-					exact
-					path='/'
-					component={props => <ConnectedRoot {...props} noBackground />}
-				/>
-				<Route
-					exact
-					path='/identity/grant'
-					component={props => (
-						<ConnectedCreateGrantIdentity {...props} noBackground />
-					)}
-				/>
-				<Route
-					exact
-					path='/login/grant'
-					component={props => <ConnectedGrantLogin {...props} noBackground />}
-				/>
-				<Route
-					exact
-					path='/login/full'
-					component={props => (
-						<ConnectedLoginStandardIdentity {...props} noBackground />
-					)}
-				/>
-				<Route
-					exact
-					path='/identity/full'
-					component={props => (
-						<ConnectedCreateFullIdentity {...props} noBackground />
-					)}
-				/>
-				{/* <Route exact path="/identity/demo" component={DemoIdentity} /> */}
-				<Route component={PageNotFound} />
-			</Switch>
-		)
-	}
+	useEffect(() => {
+		metamaskChecks()
+	}, [])
+	return (
+		<Switch>
+			<PrivateRoute auth={auth} path='/dashboard/:side' component={Dashboard} />
+			<PrivateRoute auth={auth} path='/side-select' component={SideSelect} />
+			<Route
+				exact
+				path='/'
+				component={props => <ConnectedRoot {...props} noBackground />}
+			/>
+			<Route
+				exact
+				path='/identity/grant'
+				component={props => (
+					<ConnectedCreateGrantIdentity {...props} noBackground />
+				)}
+			/>
+			<Route
+				exact
+				path='/login/grant'
+				component={props => <ConnectedGrantLogin {...props} noBackground />}
+			/>
+			<Route
+				exact
+				path='/login/full'
+				component={props => (
+					<ConnectedLoginStandardIdentity {...props} noBackground />
+				)}
+			/>
+			<Route
+				exact
+				path='/identity/full'
+				component={props => (
+					<ConnectedCreateFullIdentity {...props} noBackground />
+				)}
+			/>
+			{/* <Route exact path="/identity/demo" component={DemoIdentity} /> */}
+			<Route component={PageNotFound} />
+		</Switch>
+	)
 }
 
-Root.propTypes = {
-	actions: PropTypes.object.isRequired,
-	account: PropTypes.object.isRequired,
-}
-
-function mapStateToProps(state) {
-	const { persist, memory } = state
-	const { account } = persist
-	const { wallet, identity } = account
-
-	const hasAuth =
-		!!wallet &&
-		!!wallet.address &&
-		!!wallet.authSig &&
-		!!wallet.authType &&
-		!!identity.address
-
-	return {
-		account: account,
-		auth: hasAuth,
-		memoryIdentity: memory.identity,
-	}
-}
-
-function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators(actions, dispatch),
-	}
-}
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Translate(withRouter(Root)))
+export default Translate(Root)
