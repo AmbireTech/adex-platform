a454b354d0f2d390df5215f884746cc9a0bfa971
Ivo paunov <ivo.paunov@gmail.com>
2019-10-25 15:13:34 +0300

    auth select with hooks

--- src/components/signin/auth-select/AuthSelect.js
+++ src/components/signin/auth-select/AuthSelect.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import { withReactRouterLink } from 'components/common/rr_hoc/RRHoc.js'
@@ -8,81 +8,79 @@ import { styles } from './styles'
 import { getAuthLogo } from 'helpers/logosHelpers'
 import Img from 'components/common/img/Img'
 import classnames from 'classnames'
+import { execute, resetIdentity } from 'actions'
 
 const RRButton = withReactRouterLink(Button)
 
-class AuthSelect extends Component {
-	componentDidMount() {
+const AuthSelect = ({ t, classes }) => {
+	useEffect(() => {
 		// NOTE: reset identity if someone press backspace
 		// to go to this page
-		this.props.actions.resetIdentity()
-	}
+		execute(resetIdentity())
+	}, [])
 
-	render() {
-		const { t, classes } = this.props
-		return (
-			<div>
-				<Grid container spacing={2} direction='column' alignItems='stretch'>
-					<Grid item xs={12}>
-						<RRButton
-							variant='contained'
-							to='/identity/grant'
-							size='large'
-							color='primary'
-							fullWidth
-						>
-							{t('CREATE_GRANT_ACCOUNT')}
-						</RRButton>
-					</Grid>
-					<Grid item xs={12}>
-						<RRButton
-							variant='contained'
-							to='/login/grant'
-							size='large'
-							color='secondary'
-							fullWidth
-						>
-							{t('LOGIN_GRANT_ACCOUNT')}
-						</RRButton>
-					</Grid>
-					<Grid item xs={12}>
-						<RRButton
-							variant='contained'
-							to='/login/full?metamask'
-							size='large'
-							color='default'
-							fullWidth
-							className={classes.metamaskBtn}
-						>
-							<Img
-								src={getAuthLogo('metamask')}
-								alt={t('AUTH_WITH_METAMASK')}
-								className={classes.btnLogo}
-							/>
-							{t('METAMASK')}
-						</RRButton>
-					</Grid>
-					<Grid item xs={12}>
-						<RRButton
-							variant='contained'
-							to='/login/full?trezor'
-							size='large'
-							color='default'
-							fullWidth
-							className={classes.trezorBtn}
-						>
-							<Img
-								src={getAuthLogo('trezor')}
-								alt={t('AUTH_WITH_TREZOR')}
-								className={classnames(classes.btnLogo, classes.btnLogoNoTxt)}
-							/>
-							{/* {t('TREZOR')} */}
-						</RRButton>
-					</Grid>
+	return (
+		<div>
+			<Grid container spacing={2} direction='column' alignItems='stretch'>
+				<Grid item xs={12}>
+					<RRButton
+						variant='contained'
+						to='/identity/grant'
+						size='large'
+						color='primary'
+						fullWidth
+					>
+						{t('CREATE_GRANT_ACCOUNT')}
+					</RRButton>
 				</Grid>
-			</div>
-		)
-	}
+				<Grid item xs={12}>
+					<RRButton
+						variant='contained'
+						to='/login/grant'
+						size='large'
+						color='secondary'
+						fullWidth
+					>
+						{t('LOGIN_GRANT_ACCOUNT')}
+					</RRButton>
+				</Grid>
+				<Grid item xs={12}>
+					<RRButton
+						variant='contained'
+						to='/login/full?metamask'
+						size='large'
+						color='default'
+						fullWidth
+						className={classes.metamaskBtn}
+					>
+						<Img
+							src={getAuthLogo('metamask')}
+							alt={t('AUTH_WITH_METAMASK')}
+							className={classes.btnLogo}
+						/>
+						{t('METAMASK')}
+					</RRButton>
+				</Grid>
+				<Grid item xs={12}>
+					<RRButton
+						variant='contained'
+						to='/login/full?trezor'
+						size='large'
+						color='default'
+						fullWidth
+						className={classes.trezorBtn}
+					>
+						<Img
+							src={getAuthLogo('trezor')}
+							alt={t('AUTH_WITH_TREZOR')}
+							className={classnames(classes.btnLogo, classes.btnLogoNoTxt)}
+						/>
+						{/* {t('TREZOR')} */}
+					</RRButton>
+				</Grid>
+			</Grid>
+		</div>
+	)
 }
 
 export default Translate(withStyles(styles)(AuthSelect))
