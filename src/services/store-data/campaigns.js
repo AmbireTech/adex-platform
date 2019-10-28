1dea25a9d70011fa8b32f4cd280462d54c94ff96
Ivo paunov <ivo.paunov@gmail.com>
2019-10-28 19:24:23 +0200

    add campaigs update loop

---
+++ src/services/store-data/campaigns.js
@@ -0,0 +1,73 @@
+import { getState, updateItems, execute, addToast } from 'actions'
+import { getCampaigns } from 'services/adex-market/actions'
+import { selectAccount, selectAuth } from 'selectors'
+import { MOON_GRAVITY_ACCELERATION, MOON_TO_EARTH_WEIGHT } from 'constants/misc'
+
+const LOOP_TIMEOUT =
+	(69 - Math.floor(MOON_GRAVITY_ACCELERATION / MOON_TO_EARTH_WEIGHT)) * 500
+
+let campaignsCheckTimeout = null
+
+const clearCampaignsTimeout = () => {
+	if (campaignsCheckTimeout) {
+		clearTimeout(campaignsCheckTimeout)
+		campaignsCheckTimeout = null
+	}
+}
+
+const syncCampaigns = async () => {
+	const hasAuth = selectAuth(getState())
+	const { wallet } = selectAccount(getState())
+	const { authSig } = wallet
+
+	if (hasAuth && authSig) {
+		try {
+			const campaigns = await getCampaigns({ authSig: wallet.authSig })
+
+			const campaignsMapped = campaigns.map(c => {
+				return { ...c, ...c.spec }
+			})
+
+			execute(updateItems({ items: campaignsMapped, itemType: 'Campaign' }))
+		} catch (err) {
+			console.error('ERR_GETTING_CAMPAIGNS', err)
+			execute(
+				addToast({
+					type: 'cancel',
+					toastStr: 'ERR_GETTING_ITEMS',
+					args: [err],
+				})
+			)
+		}
+	}
+}
+
+const checkCampaigns = () => {
+	syncCampaigns()
+		.then(() => {
+			checkCampaignsLoop()
+		})
+		.catch(() => {
+			checkCampaignsLoop()
+		})
+}
+
+const checkCampaignsLoop = () => {
+	clearCampaignsTimeout()
+
+	campaignsCheckTimeout = setTimeout(checkCampaigns, LOOP_TIMEOUT)
+}
+
+const start = () => {
+	clearCampaignsTimeout()
+	checkCampaigns()
+}
+
+const stop = () => {
+	clearCampaignsTimeout()
+}
+
+export default {
+	start,
+	stop,
+}
