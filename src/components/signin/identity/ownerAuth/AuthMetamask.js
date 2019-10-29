70a0d9d0f0edcee37b4d6bf13b711285838c2896
Rangel Stoilov <rangelstoilov@gmail.com>
2019-10-29 17:26:33 +0200

    refactoring to functional component with

--- src/components/signin/identity/ownerAuth/AuthMetamask.js
+++ src/components/signin/identity/ownerAuth/AuthMetamask.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
@@ -10,6 +10,7 @@ import Img from 'components/common/img/Img'
 import AuthHoc from './AuthHoc'
 import { AUTH_TYPES } from 'constants/misc'
 import { AddrItem } from './AuthCommon'
+import { addToast, execute } from 'actions'
 import {
 	ContentBox,
 	ContentBody,
@@ -24,161 +25,147 @@ import { getSigner } from 'services/smart-contracts/actions/ethers'
 import { getAddressBalances } from 'services/smart-contracts/actions/stats'
 import Box from '@material-ui/core/Box'
 
-class AuthMetamask extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			method: '',
-			sideSelect: false,
-			address: null,
-			stats: null,
-			waitingMetamaskAction: false,
-			waitingAddrsData: false,
-		}
-	}
+function AuthMetamask(props) {
+	const [method, setMethod] = useState('')
+	const [sideSelect, setSideSelect] = useState(false)
+	const [address, setAddress] = useState(null)
+	const [stats, setStats] = useState(null)
+	const [waitingMetamaskAction, setWaitingMetamaskAction] = useState(false)
+	const [waitingAddrsData, setWaitingAddrsData] = useState(false)
+	const { t, classes } = props
 
-	checkMetamask = async () => {
-		this.setState({ waitingAddrsData: true }, async () => {
-			try {
-				const authType = AUTH_TYPES.METAMASK.name
-				const { provider } = await getEthers(authType)
-				const wallet = {
-					authType: authType,
-				}
+	const checkMetamask = async () => {
+		setWaitingAddrsData(true)
+		try {
+			const authType = AUTH_TYPES.METAMASK.name
+			const { provider } = await getEthers(authType)
+			const wallet = {
+				authType: authType,
+			}
 
-				const metamaskSigner = await getSigner({ wallet, provider })
-				const address = await metamaskSigner.getAddress()
-				const stats = await getAddressBalances({
-					address: { address },
-					authType,
-				})
+			const metamaskSigner = await getSigner({ wallet, provider })
+			const address = await metamaskSigner.getAddress()
+			const stats = await getAddressBalances({
+				address: { address },
+				authType,
+			})
+			setAddress(address)
+			setStats(stats)
+			setWaitingAddrsData(false)
 
-				this.setState({
-					address,
-					stats,
-					waitingAddrsData: false,
-				})
-
-				this.props.updateWallet({
-					address,
-					authType: AUTH_TYPES.METAMASK.name,
-					balanceEth: stats.balanceEth,
-					balanceDai: stats.balanceDai,
-					signType: AUTH_TYPES.METAMASK.signType,
-				})
-			} catch (err) {
-				console.error('Error: catch', err)
-				this.setState({ waitingMetamaskAction: false, waitingAddrsData: false })
-				this.props.actions.addToast({
+			props.updateWallet({
+				address,
+				authType: AUTH_TYPES.METAMASK.name,
+				balanceEth: stats.balanceEth,
+				balanceDai: stats.balanceDai,
+				signType: AUTH_TYPES.METAMASK.signType,
+			})
+		} catch (err) {
+			console.error('Error: catch', err)
+			setWaitingMetamaskAction(false)
+			setWaitingAddrsData(false)
+			execute(
+				addToast({
 					type: 'cancel',
 					action: 'X',
-					label: this.props.t('ERR_AUTH_METAMASK', {
+					label: t('ERR_AUTH_METAMASK', {
 						args: [Helper.getErrMsg(err)],
 					}),
 					timeout: 5000,
 				})
-			}
-		})
+			)
+		}
 	}
 
-	render() {
-		const { t, classes } = this.props
-		const { address, stats } = this.state
-
-		return (
-			<ContentBox className={classes.tabBox}>
-				{this.state.waitingMetamaskAction ? (
-					<ContentStickyTop>
-						<TopLoading msg={t('METAMASK_WAITING_ACTION')} />
-					</ContentStickyTop>
-				) : this.state.waitingAddrsData ? (
-					<TopLoading msg={t('METAMASK_WAITING_ADDR_INFO')} />
-				) : null}
-				<ContentBody>
-					<Typography paragraph variant='subheading'>
-						{t('METAMASK_INFO')}
-					</Typography>
-					<Box
-						display='flex'
-						flexDirection='column'
-						alignItems='center'
-						justifyContent='center'
-						width={1}
-					>
-						{!window.ethereum || !window.ethereum.isMetaMask ? (
-							<React.Fragment>
-								<Typography paragraph>
-									<span
-										dangerouslySetInnerHTML={{
-											__html: t('METAMASK_BASIC_USAGE_INFO', {
-												args: [
-													{
-														component: (
-															<Anchor
-																href='https://metamask.io/'
-																target='_blank'
-															>
-																https://metamask.io/
-															</Anchor>
-														),
-													},
-												],
-											}),
-										}}
-									/>
-								</Typography>
-								<Typography paragraph>
-									<Anchor href='https://metamask.io/' target='_blank'>
-										<Img
-											src={METAMASK_DL_IMG}
-											alt={'Downlad metamask'}
-											className={classes.dlBtnImg}
-										/>
-									</Anchor>
-								</Typography>
-							</React.Fragment>
-						) : (
+	return (
+		<ContentBox className={classes.tabBox}>
+			{waitingMetamaskAction ? (
+				<ContentStickyTop>
+					<TopLoading msg={t('METAMASK_WAITING_ACTION')} />
+				</ContentStickyTop>
+			) : waitingAddrsData ? (
+				<TopLoading msg={t('METAMASK_WAITING_ADDR_INFO')} />
+			) : null}
+			<ContentBody>
+				<Typography paragraph variant='subheading'>
+					{t('METAMASK_INFO')}
+				</Typography>
+				<Box
+					display='flex'
+					flexDirection='column'
+					alignItems='center'
+					justifyContent='center'
+					width={1}
+				>
+					{!window.ethereum || !window.ethereum.isMetaMask ? (
+						<React.Fragment>
 							<Typography paragraph>
-								<Img
-									src={METAMASK_IMG}
-									alt={'Downlad metamask'}
-									className={classes.dlBtnImg}
+								<span
+									dangerouslySetInnerHTML={{
+										__html: t('METAMASK_BASIC_USAGE_INFO', {
+											args: [
+												{
+													component: (
+														<Anchor href='https://metamask.io/' target='_blank'>
+															https://metamask.io/
+														</Anchor>
+													),
+												},
+											],
+										}),
+									}}
 								/>
 							</Typography>
-						)}
+							<Typography paragraph>
+								<Anchor href='https://metamask.io/' target='_blank'>
+									<Img
+										src={METAMASK_DL_IMG}
+										alt={'Downlad metamask'}
+										className={classes.dlBtnImg}
+									/>
+								</Anchor>
+							</Typography>
+						</React.Fragment>
+					) : (
+						<Typography paragraph>
+							<Img
+								src={METAMASK_IMG}
+								alt={'Downlad metamask'}
+								className={classes.dlBtnImg}
+							/>
+						</Typography>
+					)}
 
-						{address ? (
-							<div className={classes.metamaskLAbel}>
-								{stats ? (
-									<div>
-										<Typography paragraph variant='subheading' color='primary'>
-											{t('METAMASK_CONTINUE_TO_NEXT_STEP')}
-										</Typography>
-										<AddrItem stats={stats} t={t} addr={address} />
-									</div>
-								) : (
-									t('AUTH_WITH_METAMASK_LABEL', { args: [address] })
-								)}
-							</div>
-						) : window.ethereum && window.ethereum.isMetaMask ? (
-							<Button
-								onClick={this.checkMetamask}
-								variant='contained'
-								color='primary'
-								disabled={this.state.waitingAddrsData}
-							>
-								{t('AUTH_CONNECT_WITH_METAMASK')}
-							</Button>
-						) : null}
-					</Box>
-				</ContentBody>
-			</ContentBox>
-		)
-	}
+					{address ? (
+						<div className={classes.metamaskLAbel}>
+							{stats ? (
+								<div>
+									<Typography paragraph variant='subheading' color='primary'>
+										{t('METAMASK_CONTINUE_TO_NEXT_STEP')}
+									</Typography>
+									<AddrItem stats={stats} t={t} addr={address} />
+								</div>
+							) : (
+								t('AUTH_WITH_METAMASK_LABEL', { args: [address] })
+							)}
+						</div>
+					) : window.ethereum && window.ethereum.isMetaMask ? (
+						<Button
+							onClick={checkMetamask}
+							variant='contained'
+							color='primary'
+							disabled={waitingAddrsData}
+						>
+							{t('AUTH_CONNECT_WITH_METAMASK')}
+						</Button>
+					) : null}
+				</Box>
+			</ContentBody>
+		</ContentBox>
+	)
 }
 
 AuthMetamask.propTypes = {
-	actions: PropTypes.object.isRequired,
 	updateWallet: PropTypes.func.isRequired,
 	t: PropTypes.func.isRequired,
 	classes: PropTypes.object.isRequired,
