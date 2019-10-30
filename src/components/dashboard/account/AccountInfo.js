b0a0c3376cbd63bcd4b74c5a0c67bbe0d49ee5f5
Ivo paunov <ivo.paunov@gmail.com>
2019-10-30 16:44:22 +0200

    update eut top up label

--- src/components/dashboard/account/AccountInfo.js
+++ src/components/dashboard/account/AccountInfo.js
@@ -190,7 +190,7 @@ function AccountInfo({ t }) {
 								onClick={() => displayRampWidget()}
 							>
 								<CreditCardIcon className={classes.extendedIcon} />
-								{t('TOP_UP_IDENTITY')}
+								{t('TOP_UP_IDENTITY_EUR')}
 							</Button>
 						</Box>
 						<Box className={classes.itemActions}>
