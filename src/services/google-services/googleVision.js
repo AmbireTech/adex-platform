0338fad20c492c346a1d5057955272d8a5050af6
Rangel Stoilov <rangelstoilov@gmail.com>
2019-11-08 18:57:52 +0200

    saving progress google vision #40

--- src/services/google-services/googleVision.js
+++ src/services/google-services/googleVision.js
@@ -1,11 +1,13 @@
-import vision from '@google-cloud/vision'
-
-export const quickstart = async image => {
+export const labelDetection = async image => {
 	try {
+		// Imports the Google Cloud client library
+		const vision = require('@google-cloud/vision')
+
 		// Creates a client
 		const client = new vision.ImageAnnotatorClient()
+
 		// Performs label detection on the image file
-		const [result] = await client.labelDetection()
+		const [result] = await client.labelDetection('./resources/wakeupcat.jpg')
 		console.log(result)
 	} catch (error) {
 		console.log(error)
