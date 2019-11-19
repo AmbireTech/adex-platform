29b24ef2a95ba2105373277a19fdf4090bf7c48a
rori4 <rangelstoilov@gmail.com>
2019-11-19 16:17:20 +0200

    initial construction

--- src/components/dashboard/containers/DashboardStats/BasicStats.js
+++ src/components/dashboard/containers/DashboardStats/BasicStats.js
@@ -5,6 +5,17 @@ import Grid from '@material-ui/core/Grid'
 import { translate } from 'services/translations/translations'
 import { CHARTS_COLORS } from 'components/dashboard/charts/options'
 import { VALIDATOR_ANALYTICS_TIMEFRAMES } from 'constants/misc'
+import StatsCard from './StatsCard'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+	red,
+	green,
+	blue,
+	blueGrey,
+	amber,
+	brown,
+} from '@material-ui/core/colors'
+import { styles } from './styles'
 
 const timeFrames = VALIDATOR_ANALYTICS_TIMEFRAMES.map(tf => {
 	const translated = { ...tf }
@@ -42,9 +53,56 @@ const metrics = {
 export function BasicStats({ analytics, side, t }) {
 	const [timeframe, setTimeframe] = useState(timeFrames[0].value)
 	const data = analytics[side] || {}
+	const useStyles = makeStyles(styles)
+	const classes = useStyles()
 
 	return (
 		<Grid container spacing={2}>
+			<Grid item xs={6}>
+				<div className={classes.infoStatsContainer}>
+					<StatsCard
+						linkCard
+						bgColor={{
+							backgroundColor: blue[500],
+						}}
+						textColor={{ color: 'white' }}
+						subtitle={t('LABEL_TOTAL_IMPRESSIONS')}
+						title={`${0}`}
+					></StatsCard>
+					{side === 'advertiser' && (
+						<StatsCard
+							linkCard
+							bgColor={{
+								backgroundColor: red[500],
+							}}
+							textColor={{ color: 'white' }}
+							subtitle={t('LABEL_TOTAL_SPENT')}
+							title={`${0} DAI`}
+						></StatsCard>
+					)}
+
+					{side === 'publisher' && (
+						<StatsCard
+							linkCard
+							bgColor={{
+								backgroundColor: green[200],
+							}}
+							textColor={{ color: 'white' }}
+							subtitle={t('LABEL_TOTAL_REVENUE')}
+							title={`${0} DAI`}
+						></StatsCard>
+					)}
+					<StatsCard
+						linkCard
+						bgColor={{
+							backgroundColor: blueGrey[500],
+						}}
+						textColor={{ color: 'white' }}
+						subtitle={t('LABEL_AVG_CPM')}
+						title={`${0} DAI`}
+					></StatsCard>
+				</div>
+			</Grid>
 			<Grid item xs={12} sm={6} md={3}>
 				<Dropdown
 					fullWidth
