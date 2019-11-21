7d3223c8f9f0d98afd828a82e801a5ae658cf224
Ivo <ivo.paunov@gmail.com>
2019-11-21 23:15:57 +0200

    eddify side switch

--- src/components/dashboard/dashboard/SideSwitch.js
+++ src/components/dashboard/dashboard/SideSwitch.js
@@ -1,53 +1,62 @@
 import React from 'react'
-import Anchor from 'components/common/anchor/anchor'
+import { withStyles } from '@material-ui/core/styles'
 import Switch from '@material-ui/core/Switch'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
 import { withReactRouterLink } from 'components/common/rr_hoc/RRHoc.js'
-import { withStyles } from '@material-ui/core/styles'
-import { styles } from './styles'
+
+const EddieSwitch = withStyles(theme => {
+	const advColor = theme.palette.eddiePink.main
+	const pubColor = theme.palette.eddieGreen.main
+	return {
+		switchBase: {
+			color: advColor,
+
+			'&$checked': {
+				color: pubColor,
+			},
+			'&$checked + $track': {
+				backgroundColor: pubColor,
+			},
+		},
+		checked: {},
+		track: { backgroundColor: advColor },
+	}
+})(Switch)
 
 const RRSwitch = withReactRouterLink(props => (
 	<ListItem>
 		<ListItemText primary={props.label} />
 		<ListItemSecondaryAction>
-			<Anchor {...props}>
-				<Switch {...props} />
-			</Anchor>
+			<EddieSwitch {...props} />
 		</ListItemSecondaryAction>
 	</ListItem>
 ))
 
-const sideSwitch = ({ side, t, classes }) => {
+const SideSwitch = ({ side, t }) => {
 	return (
 		<div>
 			{/* Keep both if there is no valid side and force react to rerender at the same time */}
 			{side !== 'advertiser' && (
 				<RRSwitch
-					color='default'
+					color='primary'
 					checked={true}
 					value='account'
 					to={{ pathname: '/dashboard/advertiser' }}
 					label={t('PUBLISHER')}
-					classes={{
-						bar: classes.bar,
-					}}
 				/>
 			)}
 			{side !== 'publisher' && (
 				<RRSwitch
-					color='default'
+					color='primary'
 					checked={false}
 					to={{ pathname: '/dashboard/publisher' }}
 					label={t('ADVERTISER')}
-					classes={{
-						bar: classes.bar,
-					}}
 				/>
 			)}
 		</div>
 	)
 }
 
-export const SideSwitch = withStyles(styles)(sideSwitch)
+export default SideSwitch
