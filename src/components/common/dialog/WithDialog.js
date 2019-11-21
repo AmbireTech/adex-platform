0428744530eb957cefcbef4083959a5b13d23a28
Ivo <ivo.paunov@gmail.com>
2019-11-22 01:14:53 +0200

    hookify, cleanup WithDialog and remove demo mode

--- src/components/common/dialog/WithDialog.js
+++ src/components/common/dialog/WithDialog.js
@@ -1,5 +1,6 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Fab from '@material-ui/core/Fab'
 import IconButton from '@material-ui/core/IconButton'
@@ -7,22 +8,13 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import classnames from 'classnames'
-import Translate from 'components/translate/Translate'
 import Slide from '@material-ui/core/Slide'
 import { withStyles } from '@material-ui/core/styles'
 import { styles } from './styles'
-import Icon from '@material-ui/core/Icon'
 import CancelIcon from '@material-ui/icons/Cancel'
 import DialogActions from '@material-ui/core/DialogActions'
 import Typography from '@material-ui/core/Typography'
-import {
-	ContentBox,
-	ContentBody,
-	ContentStickyTop,
-} from 'components/common/dialog/content'
-import { logOut, isDemoMode } from 'services/store-data/auth'
-import DEMO_IMAGE from 'resources/rekt-eddie.png'
-import Img from 'components/common/img/Img'
+import { t } from 'selectors'
 
 const textBtn = ({ label, className, classes, style, onClick, ...rest }) => {
 	return (
@@ -43,181 +35,120 @@ const Transition = props => {
 	return <Slide direction='up' {...props} />
 }
 
+const useStyles = makeStyles(styles)
+
 export default function ItemHoc(Decorated) {
-	class WithDialog extends Component {
-		constructor(props) {
-			super(props)
-			this.state = {
-				open: false,
-			}
+	function WithDialog(props) {
+		const {
+			iconButton,
+			textButton,
+			fabButton,
+			variant,
+			fabVariant,
+			color,
+			size,
+			mini,
+			btnLabelArgs,
+			btnLabel,
+			disabled,
+			className,
+			icon,
+			title,
+			dialogActions,
+			onClick,
+			fullWidth,
+			...rest
+		} = props
+
+		const btnProps = {
+			color,
+			size,
+			mini,
+			variant,
+			fullWidth,
 		}
 
-		shouldComponentUpdate(nextProps, nextState) {
-			const shouldUpdate =
-				this.state.open !== nextState.open ||
-				JSON.stringify(this.props) !== JSON.stringify(nextProps)
+		const classes = useStyles()
+		const [open, setOpen] = useState(false)
 
-			return shouldUpdate
-		}
-
-		handleToggle = async () => {
-			const { onBeforeOpen } = this.props
-			if (typeof onBeforeOpen === 'function' && !this.state.open) {
+		const handleToggle = async () => {
+			const { onBeforeOpen } = props
+			if (typeof onBeforeOpen === 'function' && !open) {
 				await onBeforeOpen()
 			}
 
-			this.setState({ open: !this.state.open })
+			setOpen(!open)
 		}
 
-		closeDialog = () => {
-			this.setState({ open: false })
+		const closeDialog = () => {
+			setOpen(false)
 		}
 
-		renderDemoModeAction = () => {
-			const { classes, t } = this.props
-			return (
-				<ContentBox>
-					<ContentStickyTop>
-						<Typography paragraph variant='subheading'>
-							{t('DEMO_MODE_ACTION_DESCRIPTION')}
+		let ButtonComponent = Button
+
+		if (iconButton) {
+			ButtonComponent = IconButton
+		} else if (textButton) {
+			ButtonComponent = TextBtn
+		} else if (fabButton) {
+			ButtonComponent = Fab
+		}
+
+		const btnLabelTranslated = t(btnLabel, { args: btnLabelArgs || [''] })
+
+		return (
+			<Fragment>
+				<ButtonComponent
+					disabled={disabled}
+					aria-label={btnLabelTranslated}
+					label={btnLabelTranslated}
+					onClick={() => {
+						handleToggle()
+						if (onClick) onClick()
+					}}
+					{...btnProps}
+					className={classnames(
+						className,
+						{ [classes.floating]: !!fabButton },
+						{ [classes.first]: color === 'first' },
+						{ [classes.second]: color === 'second' }
+					)}
+				>
+					{icon}
+					{!iconButton && btnLabelTranslated}
+				</ButtonComponent>
+				<Dialog
+					// disableBackdropClick
+					// disableEscapeKeyDown
+					// maxWidth="xs"
+					// fullScreen
+					open={open}
+					onClose={handleToggle}
+					TransitionComponent={Transition}
+					classes={{ paper: classes.dialog }}
+					// onEscKeyDown={handleToggle}
+					// onOverlayClick={handleToggle}
+				>
+					<DialogTitle disableTypography>
+						<Typography
+							variant='subtitle1'
+							classes={{
+								root: classnames(classes.dialogTitle, classes.breakLong),
+							}}
+						>
+							{t(title)}
+							<IconButton onClick={handleToggle}>
+								<CancelIcon />
+							</IconButton>
 						</Typography>
-					</ContentStickyTop>
-					<ContentBody>
-						<div>
-							<Button onClick={logOut} variant='contained' color='primary'>
-								{t('DEMO_GO_AUTH_BTN')}
-							</Button>
-						</div>
-						<div>
-							<Img
-								className={classes.demoImg}
-								allowFullscreen={false}
-								src={DEMO_IMAGE}
-								alt={'Demo image'}
-							/>
-						</div>
-					</ContentBody>
-				</ContentBox>
-			)
-		}
-
-		render() {
-			const {
-				iconButton,
-				textButton,
-				fabButton,
-				variant,
-				fabVariant,
-				color,
-				size,
-				mini,
-				classes,
-				t,
-				btnLabelArgs,
-				disabled,
-				className,
-				icon,
-				title,
-				dialogActions,
-				onClick,
-				fullWidth,
-				...rest
-			} = this.props
-
-			let ButtonComponent = Button
-
-			const btnProps = {
-				color,
-				size,
-				mini,
-				variant,
-				fullWidth,
-			}
-
-			if (iconButton) {
-				ButtonComponent = IconButton
-			} else if (textButton) {
-				ButtonComponent = TextBtn
-			} else if (fabButton) {
-				ButtonComponent = Fab
-			}
-
-			const btnLabel = t(this.props.btnLabel, { args: btnLabelArgs || [''] })
-			const isDemo = isDemoMode()
-
-			const { open } = this.state
-
-			return (
-				<Fragment>
-					<ButtonComponent
-						disabled={disabled}
-						aria-label={btnLabel}
-						label={btnLabel}
-						onClick={() => {
-							this.handleToggle()
-							if (onClick) onClick()
-						}}
-						{...btnProps}
-						// style={this.props.style}
-						className={classnames(
-							className,
-							{ [classes.floating]: !!fabButton },
-							{ [classes.first]: color === 'first' },
-							{ [classes.second]: color === 'second' }
-						)}
-					>
-						{icon && (
-							<Icon
-								className={classnames({ [classes.btnIconLeft]: !iconButton })}
-							>
-								{' '}
-								{icon}
-							</Icon>
-						)}
-						{!iconButton && btnLabel}
-					</ButtonComponent>
-					<Dialog
-						// disableBackdropClick
-						// disableEscapeKeyDown
-						// maxWidth="xs"
-						// fullScreen
-						open={open}
-						onClose={this.handleToggle}
-						TransitionComponent={Transition}
-						classes={{ paper: classes.dialog }}
-						// onEscKeyDown={this.handleToggle}
-						// onOverlayClick={this.handleToggle}
-					>
-						{/* <AppBar className={classes.appBar}>
-                            <Toolbar> */}
-						<DialogTitle disableTypography>
-							<Typography
-								variant='subtitle1'
-								classes={{
-									root: classnames(classes.dialogTitle, classes.breakLong),
-								}}
-							>
-								{t(title)}
-								<IconButton onClick={this.handleToggle}>
-									<CancelIcon />
-								</IconButton>
-							</Typography>
-						</DialogTitle>
-						{/* </Toolbar>
-                        </AppBar> */}
-						<DialogContent classes={{ root: classes.content }}>
-							{isDemo ? (
-								// NOTE: ugly but it's temp and saves a lot of work!
-								this.renderDemoModeAction()
-							) : (
-								<Decorated {...rest} closeDialog={this.closeDialog} />
-							)}
-						</DialogContent>
-						{dialogActions && <DialogActions>{dialogActions}</DialogActions>}
-					</Dialog>
-				</Fragment>
-			)
-		}
+					</DialogTitle>
+					<DialogContent classes={{ root: classes.content }}>
+						<Decorated {...rest} closeDialog={closeDialog} />
+					</DialogContent>
+					{dialogActions && <DialogActions>{dialogActions}</DialogActions>}
+				</Dialog>
+			</Fragment>
+		)
 	}
 
 	WithDialog.propTypes = {
@@ -227,5 +158,5 @@ export default function ItemHoc(Decorated) {
 		onBeforeOpen: PropTypes.func,
 	}
 
-	return Translate(withStyles(styles)(WithDialog))
+	return WithDialog
 }
