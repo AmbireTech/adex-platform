d256200b19cf235865031363083d3691e311383d
IvoPaunov <ivo.paunov@gmail.com>
2019-11-26 19:56:27 +0200

    account loop with looper

--- src/services/store-data/account.js
+++ src/services/store-data/account.js
@@ -1,46 +1,12 @@
+import Loop from './loop'
 import { execute, updateAccountStats } from 'actions'
 
 const LOOP_TIMEOUT = 30 * 1000
 
-let statsCheckTimeout = null
+const accountStatsLoop = new Loop({
+	timeout: LOOP_TIMEOUT,
+	syncAction: () => execute(updateAccountStats()),
+	loopName: '_ACCOUNTS_STATS',
+})
 
-const clearAccountStatsTimeout = () => {
-	if (statsCheckTimeout) {
-		clearTimeout(statsCheckTimeout)
-		statsCheckTimeout = null
-	}
-}
-
-const syncStats = async () => {
-	execute(updateAccountStats())
-}
-
-const checkStats = () => {
-	syncStats()
-		.then(() => {
-			checkStatsLoop()
-		})
-		.catch(() => {
-			checkStatsLoop()
-		})
-}
-
-const checkStatsLoop = () => {
-	clearAccountStatsTimeout()
-
-	statsCheckTimeout = setTimeout(checkStats, LOOP_TIMEOUT)
-}
-
-const start = () => {
-	clearAccountStatsTimeout()
-	checkStats()
-}
-
-const stop = () => {
-	clearAccountStatsTimeout()
-}
-
-export default {
-	start,
-	stop,
-}
+export default accountStatsLoop
