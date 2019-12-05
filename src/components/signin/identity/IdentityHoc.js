ecf68f374465e1866c1fa22ed4068217c1c86701
IvoPaunov <ivo.paunov@gmail.com>
2019-12-06 00:28:44 +0200

    identity hoc using login func

--- src/components/signin/identity/IdentityHoc.js
+++ src/components/signin/identity/IdentityHoc.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import actions, { execute } from 'actions'
+import actions, { execute, login } from 'actions'
 import { push } from 'connected-react-router'
 
 export default function IdentityHoc(Decorated) {
@@ -29,33 +29,7 @@ export default function IdentityHoc(Decorated) {
 		}
 
 		save = async () => {
-			const { identity, actions } = this.props
-			const {
-				wallet,
-				email,
-				identityData,
-				identityTxData,
-				deleteLegacyKey,
-				registerAccount,
-			} = identity
-
-			const newWallet = { ...wallet }
-
-			if (registerAccount) {
-				await actions.registerAccount({
-					owner: newWallet.address,
-					identityTxData,
-					email,
-				})
-			}
-
-			actions.createSession({
-				identity: identityData,
-				wallet: newWallet,
-				email,
-				registerExpected: !identityData,
-				deleteLegacyKey,
-			})
+			execute(login())
 		}
 
 		cancel = () => {
