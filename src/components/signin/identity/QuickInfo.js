1be658defad6548ccc7c4c354459bb30d68e9a0f
IvoPaunov <ivo.paunov@gmail.com>
2019-12-11 01:06:05 +0200

    GrantInfo - hookyfy, cleanup and use vld actions

--- src/components/signin/identity/QuickInfo.js
+++ src/components/signin/identity/QuickInfo.js
@@ -1,225 +1,192 @@
-import React, { Component } from 'react'
+import React from 'react'
 import IdentityHoc from './IdentityHoc'
+import { useSelector } from 'react-redux'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
-import Translate from 'components/translate/Translate'
 import Checkbox from '@material-ui/core/Checkbox'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormHelperText from '@material-ui/core/FormHelperText'
 import FormControl from '@material-ui/core/FormControl'
 import { withStyles } from '@material-ui/core/styles'
 import { styles } from './styles'
-import { validEmail, validPassword } from 'helpers/validators'
+import {
+	t,
+	// selectIdentity,
+	selectValidationsById,
+} from 'selectors'
+import {
+	execute,
+	validateEmail,
+	validateEmailCheck,
+	validatePassword,
+	validatePasswordCheck,
+	validateTOS,
+} from 'actions'
 
-class GrantInfo extends Component {
-	componentDidMount() {
-		const {
-			email,
-			emailCheck,
-			password,
-			passwordCheck,
-			tosCheck,
-		} = this.props.identity
-
-		this.validateEmail(email, false)
-		this.validateEmailCheck(emailCheck, false)
-		this.validatePassword(password, false)
-		this.validatePasswordCheck(passwordCheck, false)
-		this.validateTOS(tosCheck, false)
+const GrantInfo = props => {
+	const checkTos = checked => {
+		handleChange('tosCheck', checked)
+		execute(validateTOS(validateId, checked, true))
 	}
 
-	validateEmail(email, dirty) {
-		const isValid = validEmail(email)
-		this.props.validate('email', {
-			isValid: isValid,
-			err: { msg: 'ERR_EMAIL' },
-			dirty: dirty,
-		})
-	}
+	const { validateId, identity, handleChange } = props
+	// const identity = useSelector(selectIdentity)
+	const validations = useSelector(
+		state => selectValidationsById(state, validateId) || {}
+	)
 
-	validateEmailCheck(emailCheck, email, dirty) {
-		const isValid = !!emailCheck && !!email && emailCheck === email
-		this.props.validate('emailCheck', {
-			isValid: isValid,
-			err: { msg: 'ERR_EMAIL_CHECK' },
-			dirty: dirty,
-		})
-	}
-
-	validatePassword(password, dirty) {
-		const isValid = validPassword(password)
-		this.props.validate('password', {
-			isValid: isValid,
-			err: { msg: 'ERR_PASSWORD' },
-			dirty: dirty,
-		})
-	}
-
-	validatePasswordCheck(passwordCheck, password, dirty) {
-		const isValid = !!passwordCheck && !!password && passwordCheck === password
-		this.props.validate('passwordCheck', {
-			isValid: isValid,
-			err: { msg: 'ERR_PASSWORD_CHECK' },
-			dirty: dirty,
-		})
-	}
-
-	validateTOS(accepted, dirty) {
-		this.props.validate('tosCheck', {
-			isValid: !!accepted,
-			err: { msg: 'ERR_TOS_CHECK' },
-			dirty: dirty,
-		})
-	}
-
-	tosCheck(checked) {
-		this.props.handleChange('tosCheck', checked)
-		this.validateTOS(checked, true)
-	}
-
-	render() {
-		const { t, identity, handleChange, invalidFields } = this.props
-		// Errors
-		const {
-			email,
-			emailCheck,
-			password,
-			passwordCheck,
-			tosCheck,
-		} = invalidFields
-		return (
-			<div>
-				<Grid container spacing={2}>
-					<Grid item xs={12}>
-						<TextField
-							fullWidth
-							type='text'
-							required
-							label={t('email', { isProp: true })}
-							name='email'
-							value={identity.email || ''}
-							onChange={ev => handleChange('email', ev.target.value)}
-							onBlur={() => this.validateEmail(identity.email, true)}
-							onFocus={() => this.validateEmail(identity.email, false)}
-							error={email && !!email.dirty}
-							maxLength={128}
-							helperText={
-								email && !!email.dirty ? email.errMsg : t('ENTER_VALID_EMAIL')
-							}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							fullWidth
-							type='text'
-							required
-							label={t('emailCheck', { isProp: true })}
-							name='emailCheck'
-							value={identity.emailCheck || ''}
-							onChange={ev => handleChange('emailCheck', ev.target.value)}
-							onBlur={() =>
-								this.validateEmailCheck(
-									identity.emailCheck,
-									identity.email,
-									true
-								)
-							}
-							onFocus={() =>
-								this.validateEmailCheck(
-									identity.emailCheck,
-									identity.email,
-									false
-								)
-							}
-							error={emailCheck && !!emailCheck.dirty}
-							maxLength={128}
-							helperText={
-								emailCheck && !!emailCheck.dirty
-									? emailCheck.errMsg
-									: t('ENTER_SAME_EMAIL')
-							}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							fullWidth
-							type='password'
-							required
-							label={t('password', { isProp: true })}
-							name='password'
-							value={identity.password || ''}
-							onChange={ev => handleChange('password', ev.target.value)}
-							onBlur={() => this.validatePassword(identity.password, true)}
-							onFocus={() => this.validatePassword(identity.password, false)}
-							error={password && !!password.dirty}
-							maxLength={128}
-							helperText={
-								password && !!password.dirty
-									? password.errMsg
-									: t('PASSWORD_RULES')
-							}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							fullWidth
-							type='password'
-							required
-							label={t('passwordCheck', { isProp: true })}
-							name='passwordCheck'
-							value={identity.passwordCheck || ''}
-							onChange={ev => handleChange('passwordCheck', ev.target.value)}
-							onBlur={() =>
-								this.validatePasswordCheck(
-									identity.passwordCheck,
-									identity.password,
-									true
-								)
-							}
-							onFocus={() =>
-								this.validatePasswordCheck(
-									identity.passwordCheck,
-									identity.password,
-									false
-								)
-							}
-							error={passwordCheck && !!passwordCheck.dirty}
-							maxLength={128}
-							helperText={
-								passwordCheck && !!passwordCheck.dirty
-									? passwordCheck.errMsg
-									: t('PASSWORD_CHECK_RULES')
-							}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<FormControl
-							required
-							error={tosCheck && tosCheck.dirty}
-							component='fieldset'
-							// className={classes.formControl}
-						>
-							<FormControlLabel
-								control={
-									<Checkbox
-										checked={!!identity.tosCheck}
-										onChange={ev => this.tosCheck(ev.target.checked)}
-										value='tosCheck'
-										color='primary'
-									/>
-								}
-								label={t('TOS_CHECK')}
-							/>
-							{tosCheck && !!tosCheck.dirty && (
-								<FormHelperText>{tosCheck.errMsg}</FormHelperText>
-							)}
-						</FormControl>
-					</Grid>
+	// Errors
+	const { email, emailCheck, password, passwordCheck, tosCheck } = validations
+	return (
+		<div>
+			<Grid container spacing={2}>
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						type='text'
+						required
+						label={t('email', { isProp: true })}
+						name='email'
+						value={identity.email || ''}
+						onChange={ev => handleChange('email', ev.target.value)}
+						onBlur={() =>
+							execute(validateEmail(validateId, identity.email, true))
+						}
+						onFocus={() =>
+							execute(validateEmail(validateId, identity.email, false))
+						}
+						error={email && !!email.dirty}
+						maxLength={128}
+						helperText={
+							email && !!email.dirty ? email.errMsg : t('ENTER_VALID_EMAIL')
+						}
+					/>
 				</Grid>
-			</div>
-		)
-	}
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						type='text'
+						required
+						label={t('emailCheck', { isProp: true })}
+						name='emailCheck'
+						value={identity.emailCheck || ''}
+						onChange={ev => handleChange('emailCheck', ev.target.value)}
+						onBlur={() =>
+							execute(
+								validateEmailCheck(
+									validateId,
+									identity.emailCheck,
+									identity.email,
+									true
+								)
+							)
+						}
+						onFocus={() =>
+							validateEmailCheck(
+								validateId,
+								identity.emailCheck,
+								identity.email,
+								false
+							)
+						}
+						error={emailCheck && !!emailCheck.dirty}
+						maxLength={128}
+						helperText={
+							emailCheck && !!emailCheck.dirty
+								? emailCheck.errMsg
+								: t('ENTER_SAME_EMAIL')
+						}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						type='password'
+						required
+						label={t('password', { isProp: true })}
+						name='password'
+						value={identity.password || ''}
+						onChange={ev => handleChange('password', ev.target.value)}
+						onBlur={() =>
+							execute(validatePassword(validateId, identity.password, true))
+						}
+						onFocus={() =>
+							execute(validatePassword(validateId, identity.password, false))
+						}
+						error={password && !!password.dirty}
+						maxLength={128}
+						helperText={
+							password && !!password.dirty
+								? password.errMsg
+								: t('PASSWORD_RULES')
+						}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						type='password'
+						required
+						label={t('passwordCheck', { isProp: true })}
+						name='passwordCheck'
+						value={identity.passwordCheck || ''}
+						onChange={ev => handleChange('passwordCheck', ev.target.value)}
+						onBlur={() =>
+							execute(
+								validatePasswordCheck(
+									validateId,
+									identity.passwordCheck,
+									identity.password,
+									true
+								)
+							)
+						}
+						onFocus={() =>
+							execute(
+								validatePasswordCheck(
+									validateId,
+									identity.passwordCheck,
+									identity.password,
+									false
+								)
+							)
+						}
+						error={passwordCheck && !!passwordCheck.dirty}
+						maxLength={128}
+						helperText={
+							passwordCheck && !!passwordCheck.dirty
+								? passwordCheck.errMsg
+								: t('PASSWORD_CHECK_RULES')
+						}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<FormControl
+						required
+						error={tosCheck && tosCheck.dirty}
+						component='fieldset'
+						// className={classes.formControl}
+					>
+						<FormControlLabel
+							control={
+								<Checkbox
+									checked={!!identity.tosCheck}
+									onChange={ev => checkTos(ev.target.checked)}
+									value='tosCheck'
+									color='primary'
+								/>
+							}
+							label={t('TOS_CHECK')}
+						/>
+						{tosCheck && !!tosCheck.dirty && (
+							<FormHelperText>{tosCheck.errMsg}</FormHelperText>
+						)}
+					</FormControl>
+				</Grid>
+			</Grid>
+		</div>
+	)
 }
 
 const IdentityGrantInfoStep = IdentityHoc(GrantInfo)
-export default Translate(withStyles(styles)(IdentityGrantInfoStep))
+export default withStyles(styles)(IdentityGrantInfoStep)
