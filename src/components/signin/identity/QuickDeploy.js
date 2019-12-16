db1d2d1fade18d8b86a18fc9eb603f7550c76dbe
IvoPaunov <ivo.paunov@gmail.com>
2019-12-16 19:26:37 +0200

    update QuickDeploy step

--- src/components/signin/identity/QuickDeploy.js
+++ src/components/signin/identity/QuickDeploy.js
@@ -3,35 +3,37 @@ import { useSelector } from 'react-redux'
 // import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import { LoadingSection } from 'components/common/spinners'
 // import { styles } from './styles'
-import { t, selectIdentity } from 'selectors'
+import { t, selectIdentity, selectSpinnerById } from 'selectors'
 
 // const useStyles = makeStyles(styles)
 
-const QuickDeploy = props => {
+const QuickDeploy = ({ validateId }) => {
 	// const classes = useStyles()
-	const { identity } = useSelector(selectIdentity)
+	const spinner = useSelector(state => selectSpinnerById(state, validateId))
+	const identity = useSelector(selectIdentity)
 	const { walletAddr, identityAddr } = identity
 
-	return (
-		<div>
-			<Grid container spacing={2}>
-				<Grid item sm={12}>
-					<Typography paragraph variant='subheading'>
-						{t('QUICK_WALLET_ADDRESS')}
-						{' ' + walletAddr}
-					</Typography>
-					<Typography paragraph variant='body2'>
-						{t('QUICK_WALLET_ADDRESS_INFO')}
-					</Typography>
-					<Typography paragraph variant='subheading'>
-						{t('IDENTITY_ADDRESS_INFO', {
-							args: [identityAddr],
-						})}
-					</Typography>
-				</Grid>
+	return !!spinner ? (
+		<LoadingSection>{t('LOADING')}</LoadingSection>
+	) : (
+		<Grid container spacing={2}>
+			<Grid item sm={12}>
+				<Typography paragraph variant='subtitle1'>
+					{t('QUICK_WALLET_ADDRESS')}
+					{' ' + walletAddr}
+				</Typography>
+				<Typography paragraph variant='body2'>
+					{t('QUICK_WALLET_ADDRESS_INFO')}
+				</Typography>
+				<Typography paragraph variant='subtitle1'>
+					{t('IDENTITY_ADDRESS_INFO', {
+						args: [identityAddr],
+					})}
+				</Typography>
 			</Grid>
-		</div>
+		</Grid>
 	)
 }
 
