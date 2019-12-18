69a7dd2c86c99b106cbc3ff5d7ab63a6a3269b0a
IvoPaunov <ivo.paunov@gmail.com>
2019-12-19 00:24:31 +0200

    update config selector

--- src/selectors/configSelectors.js
+++ src/selectors/configSelectors.js
@@ -1,11 +1,32 @@
+import { getState } from 'store'
 import { createSelector } from 'reselect'
-export const selectRelayerConfig = state => state.persist.config.relayer
+
+// can be used outside  components
+export const selectRelayerConfig = state => {
+	return (state || getState()).persist.config.relayer
+}
+
+// - relayer cfg
+// network,
+// feeTokenWhitelist, { address, symbol, decimals, min, minDeploy },
+// routineWithdrawTokens, { address, symbol, decimals, minWeekly, minFinal },
+// coreAddr,
+// identityBaseAddr,
+// identityFactoryAddr,
+// registryAddr,
+// daiAddr,
+// marketUrl,
+// identityRecoveryAddr,
+// weeklyFeeAmount,
+// mainTokenAddr,
+// mainTokenSymbol,
+// mainTokenDecimals,
+// advertiserMinGrantAmount
 
 export const selectConfig = createSelector(
 	[selectRelayerConfig],
 	relayerConfig => {
-		return {
-			...relayerConfig,
-		}
+		const cfg = { ...relayerConfig }
+		return cfg
 	}
 )
