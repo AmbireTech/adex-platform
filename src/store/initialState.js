5e217d941de0c9c20abb29a74614eeb5ff94c6d2
rori4 <rangelstoilov@gmail.com>
2020-01-09 13:48:17 +0200

    revert initial state analytics

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -3,20 +3,23 @@ import {
 	VALIDATOR_ANALYTICS_SIDES,
 	VALIDATOR_ANALYTICS_EVENT_TYPES,
 	VALIDATOR_ANALYTICS_METRICS,
+	VALIDATOR_ANALYTICS_TIMEFRAMES,
 } from 'constants/misc'
 
 export const getValidatorAnalyticsInitialState = () => {
 	const initialState = { timeframe: 'day' }
 	VALIDATOR_ANALYTICS_SIDES.forEach(side =>
 		VALIDATOR_ANALYTICS_EVENT_TYPES.forEach(eventType =>
-			VALIDATOR_ANALYTICS_METRICS.forEach(metric => {
-				initialState[side] = initialState[side] || {}
-				initialState[side][eventType] = initialState[side][eventType] || {}
-				initialState[side][eventType][metric] =
-					initialState[side][eventType][metric] || {}
-				initialState[side][eventType][metric][initialState.timeframe] =
-					initialState[side][eventType][metric][initialState.timeframe] || {}
-			})
+			VALIDATOR_ANALYTICS_METRICS.forEach(metric =>
+				VALIDATOR_ANALYTICS_TIMEFRAMES.forEach(timeframe => {
+					initialState[side] = initialState[side] || {}
+					initialState[side][eventType] = initialState[side][eventType] || {}
+					initialState[side][eventType][metric] =
+						initialState[side][eventType][metric] || {}
+					initialState[side][eventType][metric][timeframe.value] =
+						initialState[side][eventType][metric][timeframe.value] || {}
+				})
+			)
 		)
 	)
 
