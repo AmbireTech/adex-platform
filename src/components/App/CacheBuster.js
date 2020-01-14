9fe323ad97326bfbb7543ed1bc82be90a80971ac
rori4 <rangelstoilov@gmail.com>
2020-01-14 17:01:47 +0200

    cache buster setup

--- src/components/App/CacheBuster.js
+++ src/components/App/CacheBuster.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import packageJson from '../package.json'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import packageJson from '../../../package.json'
 global.appVersion = packageJson.version
 
-const semverGreaterThan = (versionA, versionB) => {
-	const versionsA = versionA.split(/\./g)
-
-	const versionsB = versionB.split(/\./g)
-	while (versionsA.length || versionsB.length) {
-		const a = Number(versionsA.shift())
-
-		const b = Number(versionsB.shift())
-		// eslint-disable-next-line no-continue
-		if (a === b) continue
-		// eslint-disable-next-line no-restricted-globals
-		return a > b || isNaN(b)
-	}
-	return false
-}
-
-export default function CacheBuster() {
-	const [loading, setLoading] = useState(true)
-	const [isLatestVersion, setIsLatestVersion] = useState(false)
+export default function CacheBuster(props) {
+	let location = useLocation()
 	const refreshCacheAndReload = () => {
 		console.log('Clearing cache and hard reloading...')
 		if (caches) {
@@ -31,6 +15,22 @@ export default function CacheBuster() {
 		}
 		// delete browser cache and hard reload
 		window.location.reload(true)
+	}
+
+	const semverGreaterThan = (versionA, versionB) => {
+		const versionsA = versionA.split(/\./g)
+
+		const versionsB = versionB.split(/\./g)
+		while (versionsA.length || versionsB.length) {
+			const a = Number(versionsA.shift())
+
+			const b = Number(versionsB.shift())
+			// eslint-disable-next-line no-continue
+			if (a === b) continue
+			// eslint-disable-next-line no-restricted-globals
+			return a > b || isNaN(b)
+		}
+		return false
 	}
 
 	useEffect(() => {
@@ -48,21 +48,15 @@ export default function CacheBuster() {
 					console.log(
 						`We have a new version - ${latestVersion}. Should force refresh`
 					)
-					setLoading(false)
-					setIsLatestVersion(false)
+					global.appVersion = meta.version
+					refreshCacheAndReload()
 				} else {
 					console.log(
 						`You already have the latest version - ${latestVersion}. No cache refresh needed.`
 					)
-					setLoading(false)
-					setIsLatestVersion(true)
 				}
 			})
-	}, [])
+	}, [location])
 
-	return this.props.children({
-		loading,
-		isLatestVersion,
-		refreshCacheAndReload,
-	})
+	return props.children
 }
