610faf2e005a006e885a2d090fc293eac988cf0d
rori4 <rangelstoilov@gmail.com>
2020-01-16 20:17:27 +0200

    save memo ens resolver

--- src/components/common/ens/EnsAddressResolver.js
+++ src/components/common/ens/EnsAddressResolver.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, memo } from 'react'
 import PropTypes from 'prop-types'
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
 import Box from '@material-ui/core/Box'
@@ -13,70 +13,74 @@ import { addToast, execute } from 'actions'
 import { makeStyles } from '@material-ui/core/styles'
 import { styles } from './styles.js'
 
-const EnsAddress = props => {
+export const fetchName = async lookup => {
+	const provider = ethers.getDefaultProvider()
+	try {
+		const name = await provider.lookupAddress(lookup)
+		return name
+	} catch (err) {
+		console.error(err)
+		return false
+	}
+}
+
+const EnsAddress = memo(props => {
 	const { address } = props
-	const [name, setName] = useState()
-	const [searching, setSearching] = useState()
+	const [name, setName] = useState('')
+	const [searching, setSearching] = useState(true)
 	const useStyles = makeStyles(styles)
 	const classes = useStyles()
 
 	useEffect(() => {
+		setSearching(true)
+		async function resolveName() {
+			const resolved = await fetchName(address)
+			setName(resolved)
+			setSearching(false)
+		}
 		if (!address) return false
-		if (isEthAddress(address)) {
-			if (!name) fetchName(address)
+		if (!name && isEthAddress(address)) {
+			resolveName()
 		}
-	}, [address, name])
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
-	const fetchName = lookup => {
-		const provider = ethers.getDefaultProvider()
-		try {
-			setSearching(true)
-			provider.lookupAddress(lookup).then(name => {
-				setName(name)
-				setSearching(false)
-			})
-		} catch (err) {
-			console.error(err)
-		}
-	}
-
-	return useMemo(
-		() => (
-			<Box display='flex' flexWrap={'nowrap'} alignItems='center'>
+	return (
+		<Box display='flex' flexWrap={'nowrap'} alignItems='center'>
+			{address && (
 				<Box pr={1}>
 					<Jazzicon diameter={30} seed={jsNumberForAddress(address)} />
 				</Box>
-				<Box>
-					<ListItemText
-						className={classes.address}
-						primary={name ? name : address}
-						secondary={
-							searching ? t('SEARCHING_ENS') : name ? address : t('ENS_NOT_SET')
-						}
-					/>
-				</Box>
-				<Box pl={2}>
-					<IconButton
-						color='primary'
-						onClick={() => {
-							copy(address)
-							execute(
-								addToast({
-									type: 'accept',
-									label: t('COPIED_TO_CLIPBOARD'),
-									timeout: 5000,
-								})
-							)
-						}}
-					>
-						<CopyIcon />
-					</IconButton>
-				</Box>
+			)}
+			<Box>
+				<ListItemText
+					className={classes.address}
+					primary={name ? name : address}
+					secondary={
+						searching ? t('SEARCHING_ENS') : name ? address : t('ENS_NOT_SET')
+					}
+				/>
 			</Box>
-		),
-		[address, classes.address, name, searching]
+			<Box pl={2}>
+				<IconButton
+					color='primary'
+					onClick={() => {
+						copy(address)
+						execute(
+							addToast({
+								type: 'accept',
+								label: t('COPIED_TO_CLIPBOARD'),
+								timeout: 5000,
+							})
+						)
+					}}
+				>
+					<CopyIcon />
+				</IconButton>
+			</Box>
+		</Box>
 	)
-}
+})
 
 EnsAddress.propTypes = {
 	address: PropTypes.string.isRequired,
