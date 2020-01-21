2fb7e230fcef96e4db9c1a7e6e0a8c866dff8e2a
IvoPaunov <ivo.paunov@gmail.com>
2020-01-21 23:37:25 +0200

    async crypto

--- src/services/crypto/crypto.test.js
+++ src/services/crypto/crypto.test.js
@@ -1,12 +1,12 @@
 const { encrypt, decrypt } = require('./crypto')
 
 describe('encrypt/decrypt', () => {
-	it('should encrypt and decrypt data', () => {
+	it('should encrypt and decrypt data', async () => {
 		const key = '51023e1e62e331dd1db5c08e9ecaec3515ffe68e6f2b942b3cf3eb8ca6'
 		const data = JSON.stringify({ test: [1, 2, 3, 4] })
 
-		const encrypted = encrypt(data, key)
-		const decrypted = decrypt(encrypted, key)
+		const encrypted = await encrypt(data, key)
+		const decrypted = await decrypt(encrypted, key)
 
 		expect(decrypted).toEqual(data)
 	})
