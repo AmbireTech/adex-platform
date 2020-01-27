7487f8a3b1a0538d27a49f5b179f71408da8c9d7
Rangel Stoilov <rangelstoilov@gmail.com>
2020-01-27 19:33:45 +0200

    changes to filtering slider

--- src/components/dashboard/containers/Tables/commonFilters.js
+++ src/components/dashboard/containers/Tables/commonFilters.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import { Typography, Slider, Box } from '@material-ui/core'
+import { formatTokenAmount } from 'helpers/formatters'
 
-export const sliderFilterOptions = (initial, filterTitle) => {
+export const sliderFilterOptions = ({
+	initial,
+	filterTitle,
+	isToken = false,
+	decimals = 18,
+}) => {
 	return {
 		filter: true,
 		display: 'true',
 		filterType: 'custom',
-		filterList: initial,
 		customFilterListOptions: {
-			render: v => {
-				console.log(v)
-				if (v === initial) return null
-				else return `${filterTitle}: ${v[0]} - ${v[1]}`
-			},
+			render: v => `${filterTitle}: ${v[0]} - ${v[1]}`,
 		},
+
 		filterOptions: {
 			names: [],
-			logic(deposit, filters) {
-				// if (filters[0] && filters[1]) {
-				// 	return age < filters[0] || age > filters[1]
-				// } else if (filters[0]) {
-				// 	return age < filters[0]
-				// } else if (filters[1]) {
-				// 	return age > filters[1]
-				// }
-				// return false
+			logic: (rowNumber, filters) => {
+				const formatedDeposit = isToken
+					? formatTokenAmount(rowNumber, decimals)
+					: rowNumber
+				if (filters[0] && filters[1]) {
+					return formatedDeposit < filters[0] || formatedDeposit > filters[1]
+				}
+				return false
 			},
 			display: (filterList, onChange, index, column) => {
 				return (
@@ -34,9 +35,13 @@ export const sliderFilterOptions = (initial, filterTitle) => {
 						</Typography>
 						<Box m={3}>
 							<Slider
-								min={0}
-								max={2000}
-								value={filterList[index]}
+								min={initial[0]}
+								max={initial[1]}
+								value={
+									filterList[index][0] && filterList[index][1]
+										? filterList[index]
+										: initial
+								}
 								onChange={(_, newValue) => {
 									onChange(newValue, index, column)
 								}}
