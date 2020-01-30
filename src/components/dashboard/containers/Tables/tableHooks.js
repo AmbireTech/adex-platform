febdc2c73028201a32d72810500ccef25fe65934
IvoPaunov <ivo.paunov@gmail.com>
2020-01-30 23:09:14 +0200

    add getColumns to useTableData

--- src/components/dashboard/containers/Tables/tableHooks.js
+++ src/components/dashboard/containers/Tables/tableHooks.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-export function useTableData(selector, selectorArgs) {
+export function useTableData({ selector, selectorArgs, getColumns }) {
 	const [data, setData] = useState([])
+	const [columns, setColumns] = useState([])
 
 	const selectedData = useSelector(state => selector(state, selectorArgs))
 
 	useEffect(() => {
 		setData(selectedData)
+		setColumns(getColumns())
+
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [selectedData.length])
 
-	return data
+	return { data, columns }
 }
