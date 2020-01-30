41e41f17c9f0f13abc47b9bf390b4592ed7623cf
IvoPaunov <ivo.paunov@gmail.com>
2020-01-30 13:04:03 +0200

    update selectCampaignsTableData

--- src/selectors/campaignSelectors.js
+++ src/selectors/campaignSelectors.js
@@ -1,33 +1,39 @@
 import { createSelector } from 'reselect'
 import { selectCampaignsArray, selectRoutineWithdrawTokens } from 'selectors'
 import { formatTokenAmount } from 'helpers/formatters'
+import { formatUnits } from 'ethers/utils'
 
 export const selectItems = state => state.persist.items
 
 export const selectCampaignsTableData = createSelector(
-	[selectCampaignsArray, (_, side) => side],
-	(campaigns, side) =>
-		campaigns.map(item => ({
-			media: {
-				id: item.id,
-				adUnits: item.adUnits,
-			},
-			status: {
-				humanFriendlyName: item.status.humanFriendlyName,
-				originalName: item.status.name,
-			},
-			depositAmount: item.depositAmount || 0,
-			fundsDistributedRatio: item.status.fundsDistributedRatio || 0,
-			impressions: item.impressions || 0,
-			clicks: item.clicks || 0,
-			minPerImpression: item.spec.minPerImpression || 0,
-			created: item.created,
-			activeFrom: item.spec.withdrawPeriodStart,
-			withdrawPeriodStart: item.spec.withdrawPeriodStart,
-			actions: {
-				to: `/dashboard/${side}/Campaign/${item.id}`,
-			},
-		}))
+	[selectCampaignsArray, selectRoutineWithdrawTokens, (_, side) => side],
+	(campaigns, tokens, side) =>
+		campaigns.map(item => {
+			const { decimals } = tokens[item.depositAsset] || 18
+			return {
+				media: {
+					id: item.id,
+					adUnits: item.adUnits,
+				},
+				status: {
+					humanFriendlyName: item.status.humanFriendlyName,
+					originalName: item.status.name,
+				},
+				depositAmount: Number(formatUnits(item.depositAmount || '0', decimals)),
+				fundsDistributedRatio: item.status.fundsDistributedRatio || 0,
+				impressions: Number(item.impressions || '0'),
+				clicks: Number(item.clicks || '0'),
+				minPerImpression:
+					Number(formatUnits(item.spec.minPerImpression || '0', decimals)) *
+					1000,
+				created: item.created,
+				activeFrom: item.spec.activeFrom,
+				withdrawPeriodStart: item.spec.withdrawPeriodStart,
+				actions: {
+					to: `/dashboard/${side}/Campaign/${item.id}`,
+				},
+			}
+		})
 )
 
 export const selectCampaignsMaxImpressions = createSelector(
