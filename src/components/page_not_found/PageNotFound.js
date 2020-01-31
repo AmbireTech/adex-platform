cd3801d762a6ba8328030c51898cf7705fa6a230
IvoPaunov <ivo.paunov@gmail.com>
2020-02-01 00:47:52 +0200

    drakefy 404

--- src/components/page_not_found/PageNotFound.js
+++ src/components/page_not_found/PageNotFound.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button'
-import IMG_404 from 'resources/404.png'
+import IMG_404_1 from 'resources/404-no.png'
+import IMG_404_2 from 'resources/404-yes.png'
 import Img from 'components/common/img/Img'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
@@ -9,13 +10,25 @@ import { withReactRouterLink } from 'components/common/rr_hoc/RRHoc.js'
 import { withStyles } from '@material-ui/core/styles'
 import Translate from 'components/translate/Translate'
 import { styles } from './styles'
+import { t } from 'selectors'
 
 const RRButton = withReactRouterLink(Button)
+
+const G404 = ({ size }) => (
+	<Grid item xs={size}>
+		<Typography
+			inline
+			variant={`h${size}`}
+			style={{ textTransform: 'uppercase' }}
+		>
+			{t('404')}
+		</Typography>
+	</Grid>
+)
 
 class PageNotFound extends React.Component {
 	render() {
 		const {
-			t,
 			classes,
 			title,
 			subtitle,
@@ -26,9 +39,9 @@ class PageNotFound extends React.Component {
 		return (
 			<div className={classes.wrapper}>
 				<Grid container spacing={2} justify='center' alignItems='center'>
-					<Grid item sm={12}>
-						<Typography className={classes.text} variant='h3'>
-							{t(title || '404_TEXT')}
+					<Grid item xs={12}>
+						<Typography className={classes.text} variant='h3' align='center'>
+							{t(title || '404')}
 						</Typography>
 
 						{subtitle && (
@@ -38,26 +51,35 @@ class PageNotFound extends React.Component {
 						)}
 					</Grid>
 
-					<Grid item sm={12}>
-						<Typography
-							inline
-							className={classes.text}
-							variant='h1'
-							style={{ textTransform: 'uppercase' }}
-						>
-							{t('404')}
+					<G404 size={3} />
+					<G404 size={4} />
+					<G404 size={2} />
+					<G404 size={1} />
+					<G404 size={5} />
+					<G404 size={3} />
+					<G404 size={2} />
+
+					<Grid item xs={5} sm={3}>
+						<Img src={IMG_404_1} alt={'no'} className={classes.img} />
+					</Grid>
+					<Grid item xs={7} sm={9}>
+						<Typography inline className={classes.text} variant='body1'>
+							{window.location.href}
+						</Typography>
+						<Typography inline className={classes.text} variant='body1'>
+							{window.location.href}
+						</Typography>
+						<Typography inline className={classes.text} variant='body1'>
+							{window.location.href}
+						</Typography>
+						<Typography inline className={classes.text} variant='body1'>
+							{window.location.href}
 						</Typography>
 					</Grid>
-
-					<Grid item sm={12}>
-						<Img
-							src={IMG_404}
-							alt={'Downlad metamask'}
-							className={classes.img}
-						/>
+					<Grid item xs={5} sm={3}>
+						<Img src={IMG_404_2} alt={'yes'} className={classes.img} />
 					</Grid>
-
-					<Grid item sm={12}>
+					<Grid item xs={7} sm={9}>
 						{!skipGoToButton && (
 							<RRButton
 								variant='contained'
