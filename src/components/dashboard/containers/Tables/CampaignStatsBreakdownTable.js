5b285a67cecb04b68d8b29a247f6738329c02ce7
IvoPaunov <ivo.paunov@gmail.com>
2020-01-31 15:35:04 +0200

    update and refactor CampaignStatsBreakdownTable

--- src/components/dashboard/containers/Tables/CampaignStatsBreakdownTable.js
+++ src/components/dashboard/containers/Tables/CampaignStatsBreakdownTable.js
@@ -1,93 +1,78 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { t, selectCampaignAnalytics, selectMainToken } from 'selectors'
+import {
+	t,
+	selectCampaignStatsTableData,
+	selectCampaignStatsMaxValues,
+	selectMainToken,
+} from 'selectors'
 import MUIDataTableEnhanced from 'components/dashboard/containers/Tables/MUIDataTableEnhanced'
-import { useSelector, shallowEqual } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { sliderFilterOptions } from './commonFilters'
 import { formatNumberWithCommas } from 'helpers/formatters'
+import { useTableData } from './tableHooks'
 
-const IMPRESSION = 'IMPRESSION'
-const CLICK = 'CLICK'
-function CampaignStatsBreakdownTable(props) {
+const getCols = ({ symbol, maxClicks, maxImpressions, maxEarnings }) => [
+	{
+		name: 'website',
+		label: t('WEBSITE'),
+		options: {
+			filter: true,
+			sort: true,
+		},
+	},
+	{
+		name: 'impressions',
+		label: t('LABEL_IMPRESSIONS'),
+		options: {
+			sortDirection: 'desc',
+			customBodyRender: impressions => formatNumberWithCommas(impressions || 0),
+			...sliderFilterOptions({
+				initial: [0, maxImpressions],
+				filterTitle: t('IMPRESSIONS_FILTER'),
+			}),
+		},
+	},
+	{
+		name: 'earnings',
+		label: t('WEBSITE_EARNINGS'),
+		options: {
+			customBodyRender: earnings =>
+				`${formatNumberWithCommas(earnings || 0)} ${symbol}`,
+			...sliderFilterOptions({
+				initial: [0, maxEarnings],
+				filterTitle: t('EARNINGS_FILTER'),
+			}),
+		},
+	},
+	{
+		name: 'clicks',
+		label: t('CHART_LABEL_CLICKS'),
+		options: {
+			sort: true,
+			customBodyRender: clicks => formatNumberWithCommas(clicks || 0),
+			...sliderFilterOptions({
+				initial: [0, maxClicks],
+				filterTitle: t('CLICKS_FILTER'),
+			}),
+		},
+	},
+]
+
+function CampaignStatsBreakdownTable({ campaignId }) {
 	const { symbol } = useSelector(selectMainToken)
-	const { campaignId } = props
-	const campaingAnalytics = useSelector(selectCampaignAnalytics)
-	const getTableData = () => {
-		// (IMPRESSION | CLICK).byChannelStats["0x80b2d99df436d53660737d51b8a130d053279b46cb2ac9ba91dd79b34dab6687"]
-		const results = []
-		const campaign = type => campaingAnalytics[type].byChannelStats[campaignId]
-		const imprStats = campaign(IMPRESSION).reportChannelToHostname
-		const clickStats = campaign(CLICK).reportChannelToHostname
-		const earnStats = campaign(IMPRESSION).reportChannelToHostnamePay
-		Object.keys(imprStats).map(key => {
-			results.push({
-				website: key,
-				impressions: imprStats[key] || 0,
-				earnings: Number((earnStats[key] || 0).toFixed(2)),
-				clicks: clickStats[key] || 0,
-			})
-		})
-		return results
-	}
-	const data = getTableData()
-	const columns = [
-		{
-			name: 'website',
-			label: t('WEBSITE'),
-			options: {
-				filter: true,
-				sort: true,
-			},
-		},
-		{
-			name: 'impressions',
-			label: t('LABEL_IMPRESSIONS'),
-			options: {
-				sortDirection: 'desc',
-				customBodyRender: impressions =>
-					formatNumberWithCommas(impressions || 0),
-				...sliderFilterOptions({
-					initial: [
-						0,
-						Math.max(
-							...Object.values(data).map(i => Number(i.impressions || 0))
-						),
-					],
-					filterTitle: t('IMPRESSIONS_FILTER'),
-				}),
-			},
-		},
-		{
-			name: 'earnings',
-			label: t('WEBSITE_EARNINGS'),
-			options: {
-				customBodyRender: earnings =>
-					`${formatNumberWithCommas(earnings || 0)} ${symbol}`,
-				...sliderFilterOptions({
-					initial: [
-						0,
-						Math.max(...Object.values(data).map(i => Number(i.earnings || 0))),
-					],
-					filterTitle: t('EARNINGS_FILTER'),
-				}),
-			},
-		},
-		{
-			name: 'clicks',
-			label: t('CHART_LABEL_CLICKS'),
-			options: {
-				sort: true,
-				customBodyRender: clicks => formatNumberWithCommas(clicks || 0),
-				...sliderFilterOptions({
-					initial: [
-						0,
-						Math.max(...Object.values(data).map(i => Number(i.clicks || 0))),
-					],
-					filterTitle: t('CLICKS_FILTER'),
-				}),
-			},
-		},
-	]
+
+	const { maxClicks, maxImpressions, maxEarnings } = useSelector(state =>
+		selectCampaignStatsMaxValues(state, campaignId)
+	)
+
+	const { data, columns } = useTableData({
+		selector: selectCampaignStatsTableData,
+		selectorArgs: campaignId,
+		getColumns: () =>
+			getCols({ symbol, maxClicks, maxImpressions, maxEarnings }),
+	})
+
 	return (
 		<MUIDataTableEnhanced
 			title={t('CAMPAIGN_STATS_BREAKDOWN')}
@@ -106,4 +91,4 @@ CampaignStatsBreakdownTable.propTypes = {
 	campaignId: PropTypes.string.isRequired,
 }
 
-export default React.memo(CampaignStatsBreakdownTable)
+export default CampaignStatsBreakdownTable
