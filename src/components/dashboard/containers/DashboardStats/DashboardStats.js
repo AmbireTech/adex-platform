9bb11cb26067879379d69cd825670bfc147ecb6a
IvoPaunov <ivo.paunov@gmail.com>
2020-01-31 22:03:47 +0200

    cleanup DashboardStats

--- src/components/dashboard/containers/DashboardStats/DashboardStats.js
+++ src/components/dashboard/containers/DashboardStats/DashboardStats.js
@@ -6,57 +6,20 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import classnames from 'classnames'
 import SideSelect from 'components/signin/side-select/SideSelect'
-import StatsCard from './StatsCard'
 import { styles } from './styles'
 import Grid from '@material-ui/core/Grid'
 import { BasicStats } from './BasicStats'
-import { push } from 'connected-react-router'
-import {
-	t,
-	selectSide,
-	selectAccountStatsFormatted,
-	selectMainToken,
-} from 'selectors'
+import { t, selectSide } from 'selectors'
 
 const useStyles = makeStyles(styles)
 
 export function DashboardStats(props) {
 	const side = useSelector(selectSide)
 	const classes = useStyles()
-	const { symbol } = useSelector(selectMainToken)
-	const {
-		availableIdentityBalanceMainToken,
-		outstandingBalanceMainToken,
-	} = useSelector(selectAccountStatsFormatted)
 
 	useEffect(() => {
 		execute(updateNav('navTitle', t('DASHBOARD')))
 	}, [])
-
-	const goToAccount = () => {
-		execute(push('/dashboard/' + side + '/account'))
-	}
-
-	const InfoStats = () => {
-		return (
-			<div className={classes.infoStatsContainer}>
-				<StatsCard
-					linkCard
-					onClick={goToAccount}
-					subtitle={t('IDENTITY_MAIN_TOKEN_BALANCE_AVAILABLE_INFO', {
-						args: [outstandingBalanceMainToken || 0, symbol],
-					})}
-					loading={
-						(!outstandingBalanceMainToken &&
-							outstandingBalanceMainToken !== 0) ||
-						(!availableIdentityBalanceMainToken &&
-							availableIdentityBalanceMainToken !== 0)
-					}
-					title={`${availableIdentityBalanceMainToken || 0} ${symbol}`}
-				></StatsCard>
-			</div>
-		)
-	}
 
 	return side !== 'advertiser' && side !== 'publisher' ? (
 		<SideSelect active={true} />
@@ -70,9 +33,6 @@ export function DashboardStats(props) {
 						</CardContent>
 					</Card>
 				</Grid>
-				{/* <Grid item md={12} lg={6}>
-					<InfoStats />
-				</Grid> */}
 			</Grid>
 		</div>
 	)
