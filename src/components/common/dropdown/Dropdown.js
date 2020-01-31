06d4fe9e5895127be9271d87a9646180b1030b43
IvoPaunov <ivo.paunov@gmail.com>
2020-01-31 23:25:30 +0200

    hookify and fix Dropdown

--- src/components/common/dropdown/Dropdown.js
+++ src/components/common/dropdown/Dropdown.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import Input from '@material-ui/core/Input'
 import InputLabel from '@material-ui/core/InputLabel'
 import MenuItem from '@material-ui/core/MenuItem'
@@ -12,70 +12,81 @@ import classnames from 'classnames'
 import { styles } from './styles'
 import { t } from 'selectors'
 
-class Dropdown extends React.Component {
-	handleChange = event => {
-		this.props.onChange(event.target.value)
+const useStyles = makeStyles(styles)
+
+function Dropdown(props) {
+	const classes = useStyles()
+
+	const {
+		onChange,
+		label = '',
+		value,
+		source = [],
+		htmlId = 'some-id',
+		disabled = false,
+		error = false,
+		helperText,
+		// margin = '',
+		fullWidth = false,
+		className,
+		required,
+		loading,
+		noSrcLabel,
+		variant,
+		IconComponent,
+	} = props
+
+	const inputLabel = React.useRef(null)
+	const [labelWidth, setLabelWidth] = React.useState(0)
+	React.useEffect(() => {
+		setLabelWidth(inputLabel.current.offsetWidth)
+	}, [])
+
+	const handleChange = event => {
+		onChange(event.target.value)
 	}
 
-	render() {
-		const {
-			classes,
-			label = '',
-			value,
-			source = [],
-			htmlId = 'some-id',
-			name = '',
-			disabled = false,
-			error = false,
-			helperText,
-			// margin = '',
-			fullWidth = false,
-			className,
-			required,
-			loading,
-			noSrcLabel,
-		} = this.props
-
-		// TODO: add native renderer for mobile devices when supported
-		return (
-			<FormControl
-				className={classnames(className, classes.formControl)}
-				disabled={disabled}
-				error={error}
-				fullWidth={fullWidth}
-			>
-				<InputLabel htmlFor={htmlId} required={required}>
-					{label}
-				</InputLabel>
-				{!!source.length && !loading ? (
-					<Select
-						value={value.id || value}
-						onChange={this.handleChange}
-						input={<Input name={name} id={htmlId} />}
-					>
-						{[...source].map(src => {
-							return (
-								<MenuItem
-									key={src.value.key || src.value.id || src.value}
-									value={src.value.id || src.value}
-								>
-									{src.label}
-								</MenuItem>
-							)
-						})}
-					</Select>
-				) : !loading ? (
-					<Input disabled value={noSrcLabel} />
-				) : (
-					<>
-						<Input disabled value={t('LOADING_DATA')} />
-						<InputLoading />
-					</>
-				)}
-				{helperText && <FormHelperText>{helperText}</FormHelperText>}
-			</FormControl>
-		)
-	}
+	// TODO: add native renderer for mobile devices when supported
+	return (
+		<FormControl
+			className={classnames(className, classes.formControl)}
+			disabled={disabled}
+			error={error}
+			fullWidth={fullWidth}
+			variant={variant}
+		>
+			<InputLabel ref={inputLabel} htmlFor={htmlId} required={required}>
+				{label}
+			</InputLabel>
+			{!!source.length && !loading ? (
+				<Select
+					value={value.id || value}
+					onChange={handleChange}
+					labelWidth={labelWidth}
+					IconComponent={IconComponent}
+				>
+					{[...source].map(src => {
+						return (
+							<MenuItem
+								key={src.value.key || src.value.id || src.value}
+								value={src.value.id || src.value}
+							>
+								{src.label}
+							</MenuItem>
+						)
+					})}
+				</Select>
+			) : !loading ? (
+				<Input disabled value={noSrcLabel} />
+			) : (
+				<>
+					<Input disabled value={t('LOADING_DATA')} />
+					<InputLoading />
+				</>
+			)}{' '}
+			{helperText && <FormHelperText>{helperText}</FormHelperText>}
+		</FormControl>
+	)
 }
 
 Dropdown.propTypes = {
@@ -91,9 +102,9 @@ Dropdown.propTypes = {
 	disabled: PropTypes.bool,
 	error: PropTypes.bool,
 	htmlId: PropTypes.string.isRequired,
-	name: PropTypes.string,
 	displayEmpty: PropTypes.bool,
 	helperText: PropTypes.string,
+	IconComponent: PropTypes.element,
 }
 
-export default withStyles(styles)(Dropdown)
+export default Dropdown
