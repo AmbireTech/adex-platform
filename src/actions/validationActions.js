06eb3c86d163d65420489d138a33bca77d20041a
IvoPaunov <ivo.paunov@gmail.com>
2020-02-02 23:34:57 +0200

    update channel validators

--- src/actions/validationActions.js
+++ src/actions/validationActions.js
@@ -254,10 +254,14 @@ export function validateCampaignValidators({ validateId, validators, dirty }) {
 			!!validators[1] &&
 			!!validators[0].id &&
 			!!validators[0].url &&
-			!!validators[0].fee &&
+			validators[0].feeNum !== undefined &&
+			validators[0].feeDen !== undefined &&
+			validators[0].feeAddr !== undefined &&
 			!!validators[1].id &&
 			!!validators[1].url &&
-			!!validators[1].fee
+			validators[1].feeNum !== undefined &&
+			validators[1].feeDen !== undefined &&
+			validators[0].feeAddr !== undefined
 
 		validate(validateId, 'validators', {
 			isValid,
