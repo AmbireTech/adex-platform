0c73eda82a294c7f2f23188ba08dbe84192ef63c
IvoPaunov <ivo.paunov@gmail.com>
2020-02-02 21:58:06 +0200

    fix fillEmptyTime for analytics

--- src/helpers/timeHelpers.js
+++ src/helpers/timeHelpers.js
@@ -1,79 +1,101 @@
 import moment from 'moment'
 
-export const intervalsMs = {
-	lastHour: {
-		start: Date.now() - 60 * 60 * 1000,
-		end: Date.now(),
-	},
-	last24Hours: {
-		start: moment(Date.now() - 24 * 60 * 60 * 1000).startOf('hour'),
-		end: moment(Date.now()).startOf('hour'),
-	},
-	last7Days: {
-		start: moment()
-			.startOf('hour')
-			.subtract(4, 'hours')
-			.subtract(7, 'days')
-			.valueOf(),
-		end: moment()
-			.startOf('hour')
-			.subtract(4, 'hours')
-			.valueOf(),
-	},
-	thisWeek: {
-		start: moment()
-			.startOf('isoWeek')
-			.valueOf(),
-		end: moment()
-			.endOf('isoWeek')
-			.valueOf(),
-	},
-	lastWeek: {
-		start: moment()
-			.subtract(1, 'week')
-			.startOf('isoWeek')
-			.valueOf(),
-		end: moment()
-			.subtract(1, 'week')
-			.endOf('isoWeek')
-			.valueOf(),
-	},
-	thisMonth: {
-		start: moment()
-			.startOf('month')
-			.valueOf(),
-		end: moment()
-			.endOf('month')
-			.valueOf(),
-	},
-	lastMonth: {
-		start: moment()
-			.subtract(1, 'month')
-			.startOf('month')
-			.valueOf(),
-		end: moment()
-			.subtract(1, 'month')
-			.endOf('month')
-			.valueOf(),
-	},
+export const intervalsMs = () => {
+	const nowUTC = moment.utc()
+
+	// TODO: Make it dynamic
+	const lastSixHoursPeriod = Math.floor(
+		(nowUTC.clone().valueOf() - nowUTC.clone().startOf('day')) /
+			(6 * 60 * 60 * 1000)
+	)
+	return {
+		lastHour: {
+			start: moment()
+				.startOf('minute')
+				.subtract(1, 'hours'),
+			end: moment().startOf('minute'),
+		},
+		last24Hours: {
+			start: moment()
+				.subtract(1, 'day')
+				.startOf('hour')
+				.valueOf(),
+			end: moment()
+				.startOf('hour')
+				.valueOf(),
+		},
+		last7Days: {
+			start: nowUTC
+				.clone()
+				.startOf('day')
+				.add(lastSixHoursPeriod * 6, 'hours')
+				.subtract(7, 'days')
+				.valueOf(),
+			end: nowUTC
+				.clone()
+				.startOf('day')
+				.add(lastSixHoursPeriod * 6, 'hours')
+				.add(2, 'hours')
+				.valueOf(),
+		},
+		thisWeek: {
+			start: moment()
+				.utc()
+				.startOf('isoWeek')
+				.valueOf(),
+			end: moment()
+				.utc()
+				.endOf('isoWeek')
+				.valueOf(),
+		},
+		lastWeek: {
+			start: moment()
+				.subtract(1, 'week')
+				.startOf('isoWeek')
+				.valueOf(),
+			end: moment()
+				.subtract(1, 'week')
+				.endOf('isoWeek')
+				.valueOf(),
+		},
+		thisMonth: {
+			start: moment()
+				.startOf('month')
+				.valueOf(),
+			end: moment()
+				.endOf('month')
+				.valueOf(),
+		},
+		lastMonth: {
+			start: moment()
+				.subtract(1, 'month')
+				.startOf('month')
+				.valueOf(),
+			end: moment()
+				.subtract(1, 'month')
+				.endOf('month')
+				.valueOf(),
+		},
+	}
 }
 
 export const fillEmptyTime = (prevAggr, timeframe) => {
+	const intervals = intervalsMs()
 	const time = {
-		interval: intervalsMs.lastHour,
+		interval: intervals.lastHour,
 		step: { ammount: 1, unit: 'minute' },
 	}
 	switch (timeframe) {
 		case 'hour':
-			time.interval = intervalsMs.lastHour
+			time.interval = intervals.lastHour
 			time.step = { ammount: 1, unit: 'minute' }
 			break
 		case 'day':
-			time.interval = intervalsMs.last24Hours
+			time.interval = intervals.last24Hours
 			time.step = { ammount: 1, unit: 'hour' }
 			break
 		case 'week':
-			time.interval = intervalsMs.last7Days
+			time.interval = intervals.last7Days
 			time.step = { ammount: 6, unit: 'hour' }
 			break
 		default:
@@ -87,7 +109,22 @@ export const fillEmptyTime = (prevAggr, timeframe) => {
 	) {
 		newAggr.push({ value: '0', time: m.unix() * 1000 })
 	}
-	const result = newAggr.map((item, i) => ({ ...item, ...prevAggr[i] }))
+
+	const data = [...prevAggr, ...newAggr].reduce((data, a) => {
+		const newData = { ...data }
+		const value = (+data[a.time] || +a.value || 0).toString()
+		newData[a.time] = value
+
+		return newData
+	}, {})
+
+	const result = Object.entries(data)
+		.map(([key, value]) => ({
+			value,
+			time: +key,
+		}))
+		.sort((a, b) => a.time - b.time)
+
 	return result
 }
 
