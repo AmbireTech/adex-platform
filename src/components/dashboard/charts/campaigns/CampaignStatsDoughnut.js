ef68db039d0f745d94f9dc0913731bfeb315c1bd
IvoPaunov <ivo.paunov@gmail.com>
2020-02-02 12:32:44 +0200

    CampaignStatsDoughnut use data selector

--- src/components/dashboard/charts/campaigns/CampaignStatsDoughnut.js
+++ src/components/dashboard/charts/campaigns/CampaignStatsDoughnut.js
@@ -1,58 +1,22 @@
 import React from 'react'
 import { Doughnut } from 'react-chartjs-2'
-import { t, selectCampaignAnalytics } from 'selectors'
+import { t, selectCampaignDoughnutStats } from 'selectors'
 import { useSelector } from 'react-redux'
 import { CHARTS_COLORS } from 'components/dashboard/charts/options'
 import PropTypes from 'prop-types'
 
-function CampaignStatsDoughnut(props) {
-	const campaingAnalytics = useSelector(selectCampaignAnalytics)
-	const { campaignId } = props
-	const maxDataSets = props.maxDataSets || CHARTS_COLORS.length
-	const getPieChartData = () => {
-		const results = {
-			labels: [],
-			datasets: [
-				{
-					backgroundColor: CHARTS_COLORS,
-					hoverBackgroundColor: CHARTS_COLORS,
-					borderWidth: 0,
-					data: [],
-					label: t('CHART_LABEL_IMPRESSIONS'),
-				},
-				{
-					backgroundColor: CHARTS_COLORS,
-					hoverBackgroundColor: CHARTS_COLORS,
-					borderWidth: 0,
-					data: [],
-					label: t('CHART_LABEL_CLICKS'),
-				},
-			],
-		}
-		const campaign = type => campaingAnalytics[type].byChannelStats[campaignId]
-		const imprStats = campaign('IMPRESSION').reportChannelToHostname
-		const clickStats = campaign('CLICK').reportChannelToHostname
-		Object.keys(imprStats)
-			.sort((a, b) => imprStats[b] - imprStats[a])
-			.map((key, i) => {
-				if (i < maxDataSets) {
-					results.labels.push(key)
-					results.datasets[0].data.push(imprStats[key] || 0)
-					results.datasets[1].data.push(clickStats[key] || 0)
-				} else {
-					results.labels[maxDataSets] = t('PIE_CHART_OTHER')
-
-					results.datasets[0].data[maxDataSets] =
-						(results.datasets[0].data[maxDataSets] || 0) + (imprStats[key] || 0)
-
-					results.datasets[1].data[maxDataSets] =
-						(results.datasets[1].data[maxDataSets] || 0) +
-						(clickStats[key] || 0)
-				}
-			})
-		return results
-	}
-	const data = getPieChartData()
+function CampaignStatsDoughnut({
+	campaignId,
+	maxDataSets = CHARTS_COLORS.length,
+	chartColors = CHARTS_COLORS,
+}) {
+	const data = useSelector(state =>
+		selectCampaignDoughnutStats(state, {
+			campaignId,
+			chartColors,
+			maxDataSets,
+		})
+	)
 
 	return (
 		<Doughnut
