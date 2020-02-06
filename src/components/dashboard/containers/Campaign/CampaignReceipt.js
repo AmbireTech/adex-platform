7b17214dc37b370e277c6c9662ef717dae34f4f7
rori4 <rangelstoilov@gmail.com>
2020-02-06 18:02:44 +0200

    Invoice render website spend breakdown

--- src/components/dashboard/containers/Campaign/CampaignReceipt.js
+++ src/components/dashboard/containers/Campaign/CampaignReceipt.js
@@ -1,25 +1,44 @@
 import React, { useRef } from 'react'
 import { useParams } from 'react-router'
-import { Box, Card, Button, Typography, Divider } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+import {
+	Box,
+	Card,
+	Button,
+	Typography,
+	Divider,
+	Table,
+	TableHead,
+	TableBody,
+	TableRow,
+	TableCell,
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import ReactToPrint from 'react-to-print'
-import { t } from 'selectors'
+import { t, selectCampaignStatsTableData, selectMainToken } from 'selectors'
 import classnames from 'classnames'
 import AdexIconTxt from 'components/common/icons/AdexIconTxt'
 
-const useStyles = makeStyles(theme => {
+const useStyles = makeStyles(TableCelleme => {
 	return {
 		a4: {
-			width: '210mm',
+			widTableCell: '210mm',
 			minHeight: '297mm',
 			padding: '8mm',
 		},
 		icon: {
 			height: '3rem',
-			width: 'auto',
+			widTableCell: 'auto',
 		},
 		dottedDivider: {
-			border: `0.5px dotted ${theme.palette.grey.main}`,
+			borderBottom: `0.5px dotted ${TableCelleme.palette.grey.main}`,
+		},
+		solidDevider: {
+			borderBottom: `0.5px solid ${TableCelleme.palette.grey.main}`,
+		},
+		breakdownTable: {
+			widTableCell: '100%',
+			textAlign: 'left',
 		},
 	}
 })
@@ -27,7 +46,11 @@ function CampaignReceipt() {
 	const classes = useStyles()
 	const { itemId } = useParams()
 	const invoice = useRef()
-
+	const { symbol } = useSelector(selectMainToken)
+	const campaignBreakdown = useSelector(state =>
+		selectCampaignStatsTableData(state, itemId)
+	)
+	console.log('CB', campaignBreakdown)
 	return (
 		<Box display='flex' justifyContent='center' alignContent='center'>
 			<Box
@@ -42,93 +65,163 @@ function CampaignReceipt() {
 						content={() => invoice.current}
 					/>
 				</Box>
-				<Card ref={invoice} className={classnames(classes.a4)}>
-					<Box mb={2} display='flex' justifyContent='space-between'>
-						<Box>
-							<Typography variant='h4'>{`Invoice for XXX`}</Typography>
-							<Typography variant='h5'>{`Account ID: XXX`}</Typography>
-							<Typography variant='body2'>{`Invoice ID: XXX`}</Typography>
-						</Box>
-						<Box>
-							<AdexIconTxt className={classnames(classes.icon)} />
-						</Box>
-					</Box>
-					<Divider />
-					<Box
-						mt={5}
-						display='flex'
-						justifyContent='space-between'
-						alignContent='center'
-					>
-						<Box display='flex' flexDirection='column'>
-							<Box mb={2}>
-								<Typography variant='subtitle2'>
-									<strong>{`Company Details`}</strong>
-								</Typography>
-								<Typography variant='body2'>{`My Cool Company Ltd.`}</Typography>
-								<Typography variant='body2'>{`First Last Name`}</Typography>
-								<Typography variant='body2'>{`Address`}</Typography>
-								<Typography variant='body2'>{`Country`}</Typography>
+				<Card>
+					<Box ref={invoice} className={classnames(classes.a4)}>
+						<Box mb={2} display='flex' justifyContent='space-between'>
+							<Box>
+								<Typography variant='h4'>{`Invoice for XXX`}</Typography>
+								<Typography variant='h5'>{`Account ID: XXX`}</Typography>
+								<Typography variant='body2'>{`Invoice ID: XXX`}</Typography>
 							</Box>
-							<Box mb={2}>
-								<Typography variant='body2'>{`Invoice/payment date`}</Typography>
-								<Typography variant='subtitle2'>
-									<strong>{`31 Aug 2017, 11:12`}</strong>
-								</Typography>
-							</Box>
-							<Box mb={2}>
-								<Typography variant='body2'>{`Campaign ID`}</Typography>
-								<Typography variant='subtitle2'>
-									<strong>{itemId}</strong>
-								</Typography>
+							<Box>
+								<AdexIconTxt className={classnames(classes.icon)} />
 							</Box>
 						</Box>
-						<Box display='flex' flexDirection='column' alignItems='flex-end'>
-							<Typography variant='h6'>{`Paid`}</Typography>
-							<Typography variant='h4'>
-								<strong>{`ETH XXX`}</strong>
-							</Typography>
-							<Typography variant='p'>{`Total cost in USD ($XXX)`}</Typography>
+						<Divider />
+						<Box
+							mt={5}
+							display='flex'
+							justifyContent='space-between'
+							alignContent='center'
+						>
+							<Box display='flex' flexDirection='column'>
+								<Box mb={2}>
+									<Typography variant='subtitle2'>
+										<strong>{`Company Details`}</strong>
+									</Typography>
+									<Typography variant='body2'>{`My Cool Company LTableCell .`}</Typography>
+									<Typography variant='body2'>{`First Last Name`}</Typography>
+									<Typography variant='body2'>{`Address`}</Typography>
+									<Typography variant='body2'>{`Country`}</Typography>
+								</Box>
+								<Box mb={2}>
+									<Typography variant='body2'>{`Invoice/payment date`}</Typography>
+									<Typography variant='subtitle2'>
+										<strong>{`31 Aug 2017, 11:12`}</strong>
+									</Typography>
+								</Box>
+								<Box mb={2}>
+									<Typography variant='body2'>{`Campaign ID`}</Typography>
+									<Typography variant='subtitle2'>
+										<strong>{itemId}</strong>
+									</Typography>
+								</Box>
+							</Box>
+							<Box display='flex' flexDirection='column' alignItems='flex-end'>
+								<Typography variant='h6'>{`Paid`}</Typography>
+								<Typography variant='h4'>
+									<strong>{`ETH XXX`}</strong>
+								</Typography>
+								<Typography variant='p'>{`Total cost in USD ($XXX)`}</Typography>
+							</Box>
 						</Box>
-					</Box>
-					<Box display='flex' justifyContent='space-between'>
-						<Box>
-							<Typography variant='h6'>{`Campaign Details`}</Typography>
+						<Box display='flex' justifyContent='space-between'>
+							<Box>
+								<Typography variant='h6'>{`Campaign Overview`}</Typography>
+							</Box>
 						</Box>
-					</Box>
-					<Divider className={classnames(classes.dottedDivider)} />
-					<Box mt={2} display='flex' justifyContent='space-between'>
-						<Box>
-							<Typography variant='body2'>
-								{'From 2020-02-02 19:28, to 2020-03-31 19:28'}
-							</Typography>
+						<Divider className={classnames(classes.dottedDivider)} />
+						<Box mt={2} display='flex' justifyContent='space-between'>
+							<Box>
+								<Typography variant='body2'>
+									{'From 2020-02-02 19:28, to 2020-03-31 19:28'}
+								</Typography>
+							</Box>
+							<Box>
+								<Typography variant='subtitle2'>
+									<strong>{'XXX ETH'}</strong>
+								</Typography>
+							</Box>
 						</Box>
-						<Box>
-							<Typography variant='subtitle2'>
-								<strong>{'XXX ETH'}</strong>
-							</Typography>
+						<Divider />
+						<Box mt={2} display='flex' justifyContent='space-between'>
+							<Box>
+								<Typography variant='body2'>
+									{t('LABEL_IMPRESSIONS')}
+								</Typography>
+							</Box>
+							<Box>
+								<Typography variant='subtitle2'>
+									<strong>{'XXX'}</strong>
+								</Typography>
+							</Box>
 						</Box>
-					</Box>
-					<Divider />
-					<Box mt={2} display='flex' justifyContent='space-between'>
-						<Box>
-							<Typography variant='body2'>{t('LABEL_IMPRESSIONS')}</Typography>
+						<Divider />
+						<Box mt={2} display='flex' justifyContent='space-between'>
+							<Box>
+								<Typography variant='body2'>{t('LABEL_CLICKS')}</Typography>
+							</Box>
+							<Box>
+								<Typography variant='subtitle2'>
+									<strong>{'XXX'}</strong>
+								</Typography>
+							</Box>
 						</Box>
-						<Box>
-							<Typography variant='subtitle2'>
-								<strong>{'XXX'}</strong>
-							</Typography>
+						<Divider />
+						<Box mt={2} display='flex' justifyContent='space-between'>
+							<Box>
+								<Typography variant='h6'>{`Campaign Breakdown`}</Typography>
+							</Box>
 						</Box>
-					</Box>
-					<Box mt={2} display='flex' justifyContent='space-between'>
-						<Box>
-							<Typography variant='body2'>{t('LABEL_CLICKS')}</Typography>
-						</Box>
-						<Box>
-							<Typography variant='subtitle2'>
-								<strong>{'XXX'}</strong>
-							</Typography>
-						</Box>
+						<Divider className={classnames(classes.dottedDivider)} />
+						<Table size='small'>
+							<TableHead>
+								<TableRow className={classnames(classes.dottedDivider)}>
+									<TableCell>
+										<Typography variant='subtitle2'>
+											<strong>{t('WEBSITE')}</strong>
+										</Typography>
+									</TableCell>
+									<TableCell>
+										<Typography variant='subtitle2'>
+											<strong>{t('LABEL_IMPRESSIONS')}</strong>
+										</Typography>
+									</TableCell>
+									<TableCell>
+										<Typography variant='subtitle2'>
+											<strong>{t('CHART_LABEL_CLICKS')}</strong>
+										</Typography>
+									</TableCell>
+									<TableCell>
+										<Typography variant='subtitle2'>
+											<strong>{t('CTR')}</strong>
+										</Typography>
+									</TableCell>
+									<TableCell>
+										<Typography variant='subtitle2'>
+											<strong>{t('WEBSITE_EARNINGS')}</strong>
+										</Typography>
+									</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{campaignBreakdown
+									.sort((a, b) => b.earnings - a.earnings)
+									.map(stats => (
+										<TableRow>
+											<TableCell>
+												<Typography variant='body2'>{stats.website}</Typography>
+											</TableCell>
+											<TableCell>
+												<Typography variant='body2'>
+													{stats.impressions}
+												</Typography>
+											</TableCell>
+											<TableCell>
+												<Typography variant='body2'>{stats.clicks}</Typography>
+											</TableCell>
+											<TableCell>
+												<Typography variant='body2'>{stats.ctr}</Typography>
+											</TableCell>
+											<TableCell>
+												<Typography variant='body2'>
+													{stats.earnings}
+												</Typography>
+											</TableCell>
+										</TableRow>
+									))}
+							</TableBody>
+						</Table>
 					</Box>
 				</Card>
 			</Box>
