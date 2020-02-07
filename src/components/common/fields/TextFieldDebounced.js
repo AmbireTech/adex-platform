6a41ec7bb58410db32488a26348d9939064c6f10
Rangel Stoilov <rangelstoilov@gmail.com>
2020-02-07 18:27:33 +0200

    Fix TextFueldDebounced to useCallback

--- src/components/common/fields/TextFieldDebounced.js
+++ src/components/common/fields/TextFieldDebounced.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { TextField } from '@material-ui/core'
 import debounce from 'lodash.debounce'
 
 function TextFieldDebounced(props) {
 	const [value, setValue] = useState(props.value)
-	const debounceChange = debounce(props.debounceChange, 1000, {
-		leading: false,
-		trailing: true,
-	})
+	const debounceChange = useCallback(
+		debounce(newValue => props.debounceChange(newValue), 1000),
+		[]
+	)
+
 	const handleChange = e => {
 		const val = e.target.value
 		setValue(val)
-
 		debounceChange(val)
 	}
 	return <TextField {...props} value={value} onChange={handleChange} />
