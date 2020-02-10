89e5f0d28b77935f8673c4292c6a889ac39d9bc4
rori4 <rangelstoilov@gmail.com>
2020-02-10 18:20:37 +0200

    Hide receipts on mobile due to fixed size

--- src/components/dashboard/containers/Receipt/CampaignReceipt.js
+++ src/components/dashboard/containers/Receipt/CampaignReceipt.js
@@ -1,10 +1,10 @@
 import React, { useRef } from 'react'
 import { useParams } from 'react-router'
-import { Box, Card, Button } from '@material-ui/core'
+import { Box, Card, Button, Hidden, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import ReactToPrint from 'react-to-print'
 import classnames from 'classnames'
-import { Print } from '@material-ui/icons'
+import { Print, Visibility } from '@material-ui/icons'
 import Receipt from './Receipt'
 import CompanyDetails from './CompanyDetails'
 import { useSelector } from 'react-redux'
@@ -16,6 +16,16 @@ const useStyles = makeStyles(theme => {
 			width: '210mm',
 			minHeight: '297mm',
 			padding: '8mm',
+		},
+		hideOnMobile: {
+			[theme.breakpoints.down('sm')]: {
+				display: 'none',
+			},
+		},
+		hideOnDesktop: {
+			[theme.breakpoints.up('md')]: {
+				display: 'none',
+			},
 		},
 	}
 })
@@ -55,14 +65,30 @@ function CampaignReceipt(props) {
 						/>
 					</Box>
 				</CompanyDetails>
-				<Card className={classnames(classes.hideOnMobile)}>
-					{receipts.length > 0 && (
-						<Box ref={invoice} className={classnames(classes.a4)}>
-							{receipts.map(campaignId => (
-								<Receipt campaignId={campaignId} />
-							))}
+				<Card>
+					<Box className={classnames(classes.hideOnDesktop)}>
+						<Box
+							p={5}
+							display='flex'
+							justifyContent='center'
+							alignItems='center'
+							flexDirection='column'
+						>
+							<Visibility />
+							<Typography variant='overline' display='block' gutterBottom>
+								Preview available only on desktop
+							</Typography>
 						</Box>
-					)}
+					</Box>
+					<Box className={classnames(classes.hideOnMobile)}>
+						{receipts.length > 0 && (
+							<Box ref={invoice} className={classnames(classes.a4)}>
+								{receipts.map(campaignId => (
+									<Receipt campaignId={campaignId} />
+								))}
+							</Box>
+						)}
+					</Box>
 				</Card>
 			</Box>
 		</Box>
