cf1a301615fdfd967bcd4052456fa8aeba729a72
IvoPaunov <ivo.paunov@gmail.com>
2020-02-11 19:39:36 +0200

    add ens selectors

--- src/selectors/identitySelectors.js
+++ src/selectors/identitySelectors.js
@@ -1,1 +1,10 @@
+import { createSelector } from 'reselect'
+
 export const selectIdentity = state => state.memory.identity
+
+export const selectEnsAddresses = state => state.persist.ensAddresses
+
+export const selectEnsAddressByAddr = createSelector(
+	[selectEnsAddresses, (_, address) => address],
+	(ensAddresses, address) => ensAddresses[address] || ''
+)
