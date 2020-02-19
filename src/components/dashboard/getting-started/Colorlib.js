9552c099bbcb2767193a7e6946c8b50a576733a8
rori4 <rangelstoilov@gmail.com>
2020-02-19 17:04:43 +0200

    Remove line change on active or completed

--- src/components/dashboard/getting-started/Colorlib.js
+++ src/components/dashboard/getting-started/Colorlib.js
@@ -43,18 +43,6 @@ export const ColorlibConnector = withStyles(theme => ({
 	alternativeLabel: {
 		top: 50,
 	},
-	active: {
-		// '& $line': {
-		// 	backgroundImage:
-		// 		'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-		// },
-	},
-	completed: {
-		// '& $line': {
-		// 	backgroundImage:
-		// 		'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-		// },
-	},
 	line: {
 		height: 3,
 		border: 0,
