a608f0239cb4ceccfdefa66ef1fb62953bb33e20
IvoPaunov <ivo.paunov@gmail.com>
2020-03-02 20:37:03 +0200

    translations cleanup - components args with 1 line

--- src/services/translations/translations.js
+++ src/services/translations/translations.js
@@ -1,59 +1,26 @@
-import React from 'react'
 import adexTranslations from 'adex-translations'
-import { renderToString } from 'react-dom/server'
+import { removeTags } from 'helpers/sanitizer'
 
 const translations = adexTranslations()
 
 let lang = 'en-US'
 
-// TODO: sanitization
 const interpolate = (tpl, args) => {
 	if (typeof tpl !== 'string') {
 		return ''
 	}
 
-	return tpl.replace(/\${(\w+)}/g, (_, v) => {
-		let arg = args[v]
-		if (typeof arg === 'object' && !!arg && arg.component) {
-			return renderToString(arg.component)
-		} else {
-			return arg || ''
-		}
-	})
-}
+	const pattern = new RegExp(/\${(\w+)}/, 'gi')
+	const parts = tpl.split(pattern)
 
-const addComponents = (tpl, components) => {
-	let result = [tpl]
-	for (let i = 0; i < components.length; i++) {
-		const replace = `<${i}>(.*?)</${i}>`
-		const regex = new RegExp(replace, 'g')
-		while (true) {
-			let newResult = []
-			for (let r = 0; r < result.length; r++) {
-				if (typeof result[r] !== 'string') {
-					newResult = [...newResult, result[r]]
-					continue
-				}
-				const parts = result[r].split(regex)
-				for (let j = 1; j < parts.length; j += 2) {
-					parts[j] = React.cloneElement(
-						components[i],
-						components[i].props,
-						parts[j]
-					)
-				}
-				newResult = [...newResult, ...parts]
-			}
-			if (result.length === newResult.length) break
-			result = newResult
-		}
-	}
-	return result
+	const interpolated = parts.map(p => args[p] || p)
+
+	return interpolated
 }
 
 export const translate = (
 	val = '',
-	{ isProp = false, args = [''], components = [] } = {},
+	{ isProp = false, args = [''] } = {},
 	language = lang
 ) => {
 	let key = val + ''
@@ -63,7 +30,7 @@ export const translate = (
 
 	key = key.toUpperCase()
 
-	let translation = translations[language][key] || val
+	let translation = removeTags(translations[language][key] || val)
 
 	if (args.length && Array.isArray(args)) {
 		const translatedArgs = args.map(a => {
@@ -75,10 +42,6 @@ export const translate = (
 		})
 
 		translation = interpolate(translation, translatedArgs)
-	}
-
-	if (components.length && Array.isArray(components)) {
-		translation = addComponents(translation, components)
 	}
 
 	return translation
