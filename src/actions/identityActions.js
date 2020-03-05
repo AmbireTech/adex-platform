63ececcd2169790b7d2b379c1367c5be7ee77a34
IvoPaunov <ivo.paunov@gmail.com>
2020-03-05 15:33:30 +0200

    cleanup identity actions

--- src/actions/identityActions.js
+++ src/actions/identityActions.js
@@ -38,7 +38,6 @@ import {
 	validateWallet,
 	validateIdentityContractOwner,
 	validateAccessWarning,
-	validateNotExistingEmail,
 } from './validationActions'
 import { getErrorMsg } from 'helpers/errors'
 import {
@@ -664,7 +663,6 @@ export function validateQuickInfo({ validateId, dirty, onValid, onInvalid }) {
 			),
 			validateTOS(validateId, tosCheck, dirty)(dispatch),
 			validateAccessWarning(validateId, accessWarningCheck, dirty)(dispatch),
-			// validateNotExistingEmail(validateId, email, dirty)(dispatch),
 		])
 
 		const isValid = validations.every(v => v === true)
@@ -707,7 +705,6 @@ export function validateFullInfo({ validateId, dirty, onValid, onInvalid }) {
 			validateEmailCheck(validateId, emailCheck, email, dirty)(dispatch),
 			validateTOS(validateId, tosCheck, dirty)(dispatch),
 			validateAccessWarning(validateId, accessWarningCheck, dirty)(dispatch),
-			// validateNotExistingEmail(validateId, email, dirty)(dispatch),
 		])
 
 		const isValid = validations.every(v => v === true)
