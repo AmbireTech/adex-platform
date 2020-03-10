b3e13b0360ef88293d1614ffa33b099e8e2796f7
IvoPaunov <ivo.paunov@gmail.com>
2020-03-10 17:02:09 +0200

    remove updateNav where used

--- src/components/dashboard/containers/DashboardStats/DashboardStats.js
+++ src/components/dashboard/containers/DashboardStats/DashboardStats.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-import { execute, updateNav } from 'actions'
 import { Box, Paper } from '@material-ui/core'
 import SideSelect from 'components/signin/side-select/SideSelect'
 import { BasicStats } from './BasicStats'
@@ -8,10 +7,6 @@ import { t, selectSide } from 'selectors'
 
 export function DashboardStats(props) {
 	const side = useSelector(selectSide)
-
-	useEffect(() => {
-		execute(updateNav('navTitle', t('DASHBOARD')))
-	}, [])
 
 	return side !== 'advertiser' && side !== 'publisher' ? (
 		<SideSelect active={true} />
