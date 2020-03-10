a875d6341d6d377b641abd95a009a05a52b38d3e
IvoPaunov <ivo.paunov@gmail.com>
2020-03-10 16:57:00 +0200

    add breadcrumbs selector

--- src/selectors/locationSelectors.js
+++ src/selectors/locationSelectors.js
@@ -16,3 +16,28 @@ export const selectSearchParams = createSelector(
 	selectLocation,
 	({ search }) => new URLSearchParams(search)
 )
+
+export const selectDashboardBreadcrumbs = createSelector(
+	selectLocation,
+	({ pathname = '' }) => {
+		const paths = pathname.split('/').filter(x => !!x)
+
+		// TODO: validate and use item titles
+		const sideDashboard = paths.slice(0, 2)
+		const sideDashboardBC = {
+			to: `/${sideDashboard.join('/')}`,
+			label: 'dashboard',
+		}
+
+		const rest = paths.slice(2)
+
+		const restBC = rest.map((x, index) => ({
+			to: `${sideDashboardBC.to}/${rest.slice(0, index + 1).join('/')}`,
+			label: x,
+		}))
+
+		const breadcrumbs = [sideDashboardBC, ...restBC]
+
+		return breadcrumbs || []
+	}
+)
