40835ddc98ff030c7b86aacacb0d15ae2cc85271
Rangel Stoilov <rangelstoilov@gmail.com>
2020-03-10 17:22:30 +0200

    revert to script

--- src/store/history.js
+++ src/store/history.js
@@ -1,14 +1,12 @@
 import { createHashHistory } from 'history'
-import ReactGA from 'react-ga'
 
 const history = createHashHistory()
 
-ReactGA.initialize(process.env.GA_CODE)
 history.listen(location => {
-	ReactGA.pageview(
-		location.pathname + location.search
-		//{ debug: true }
-	)
+	window.gtag &&
+		window.gtag('config', process.env.GA_CODE, {
+			page_path: location.pathname + location.search,
+		})
 })
 
 export default history
