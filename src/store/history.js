ee40b506e490cf3463b03e069a94e430cdec3ad0
Rangel Stoilov <rangelstoilov@gmail.com>
2020-03-10 16:15:27 +0200

    add ReactGA

--- src/store/history.js
+++ src/store/history.js
@@ -1,5 +1,14 @@
 import { createHashHistory } from 'history'
+import ReactGA from 'react-ga'
 
 const history = createHashHistory()
+
+ReactGA.initialize('UA-100388362-3')
+history.listen(location => {
+	ReactGA.pageview(
+		location.pathname + location.search
+		// { debug: true }
+	)
+})
 
 export default history
