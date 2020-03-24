3e5331f42d655be0c78cc8eed9c05a17f3084273
rori4 <rangelstoilov@gmail.com>
2020-03-24 16:46:22 +0200

    add selectors and fix templates based on side

--- src/components/dashboard/containers/Receipt/Receipt.js
+++ src/components/dashboard/containers/Receipt/Receipt.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useParams, Redirect } from 'react-router'
 import { Box, Card, Button, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
@@ -8,7 +8,13 @@ import { Print, Visibility } from '@material-ui/icons'
 import { CampaignReceiptTpl, PublisherReceiptTpl } from './ReceiptTemplates'
 import CompanyDetails from './CompanyDetails'
 import { useSelector } from 'react-redux'
-import { t, selectSelectedCampaigns, selectSide } from 'selectors'
+import {
+	t,
+	selectSelectedCampaigns,
+	selectSide,
+	selectSelectedPublisherReceipts,
+} from 'selectors'
+import { execute, resetSelectedItems } from 'actions'
 
 const useStyles = makeStyles(theme => {
 	return {
@@ -35,11 +41,22 @@ function Receipt(props) {
 	const { itemId, date } = useParams()
 	const side = useSelector(selectSide)
 	const selectedCampaigns = useSelector(state => selectSelectedCampaigns(state))
+	const selectedPublisherReceipts = useSelector(state =>
+		selectSelectedPublisherReceipts(state)
+	)
 	// TODO: render back button
 	const selectedByPropsOrParams = props.itemId || itemId || date
+	const selectedItems =
+		selectedCampaigns.length > 0 ? selectedCampaigns : selectedPublisherReceipts
 	const receipts = selectedByPropsOrParams
 		? [selectedByPropsOrParams]
-		: selectedCampaigns
+		: selectedItems
+
+	useEffect(() => {
+		return () => {
+			execute(resetSelectedItems())
+		}
+	})
 
 	if (receipts.length === 0)
 		return (
@@ -97,10 +114,10 @@ function Receipt(props) {
 						{receipts.length > 0 && (
 							<Box ref={invoice} className={classnames(classes.a4)}>
 								{receipts.map(item =>
-									itemId ? (
+									side === 'advertiser' ? (
 										<CampaignReceiptTpl campaignId={item} key={item} />
 									) : (
-										date && <PublisherReceiptTpl date={date} key={item} />
+										<PublisherReceiptTpl date={item} key={item} />
 									)
 								)}
 							</Box>
