3b692c1ad116f3179e6e4a5a0fa0770f6a341a2a
rori4 <rangelstoilov@gmail.com>
2020-03-25 13:12:15 +0200

    add getRceiptData to loadAccountData

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -7,7 +7,7 @@ import {
 	backupWallet,
 	getIdentityData,
 } from 'services/adex-relayer/actions'
-import { updateSpinner, updateGlobalUi } from './uiActions'
+import { updateSpinner, updateGlobalUi, getReceiptData } from 'actions'
 import { translate } from 'services/translations/translations'
 import { getAuthSig } from 'services/smart-contracts/actions/ethers'
 import {
@@ -536,7 +536,8 @@ export function loadAccountData() {
 		!isAccountChanged(getState, account) &&
 			(await analyticsCampaignsLoop.start())
 		!isAccountChanged(getState, account) && (await campaignsLoop.start())
-
+		!isAccountChanged(getState, account) &&
+			(await getReceiptData()(dispatch, getState))
 		updateSlotsDemandThrottled()(dispatch, getState)
 	}
 }
