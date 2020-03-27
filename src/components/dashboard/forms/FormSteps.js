bae2ab39064fe21b6e4062652ff1e5f7e0bfffa5
IvoPaunov <ivo.paunov@gmail.com>
2020-03-27 23:16:33 +0200

    FormSteps refactor - work in progress

--- src/components/dashboard/forms/FormSteps.js
+++ src/components/dashboard/forms/FormSteps.js
@@ -1,66 +1,62 @@
-import React, { Component, forwardRef } from 'react'
+import React, { forwardRef, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import MaterialStepper from 'components/common/stepper/MaterialUiStepper'
 import ValidItemHoc from 'components/dashboard/forms/ValidItemHoc'
 import { t } from 'selectors'
 
-class FormSteps extends Component {
-	shouldComponentUpdate(nextProps) {
-		return JSON.stringify(this.props) !== JSON.stringify(nextProps)
-	}
+function FormSteps(props) {
+	const {
+		SaveBtn,
+		cancelFunction,
+		onSave,
+		stepsId,
+		stepsPages,
+		stepsPreviewPage,
+		validateIdBase,
+		closeDialog,
+		...rest
+	} = props
 
-	render() {
-		const pages = []
-		const {
-			SaveBtn,
-			CancelBtn,
-			cancelFunction,
-			onSave,
-			stepsId,
-			stepsPages,
-			stepsPreviewPage,
-			validateIdBase,
-			...rest
-		} = this.props
-		const cancelButton = () => (
-			<CancelBtn {...rest} stepsId={stepsId} onSave={onSave} t={t} />
-		)
+	const [pages, setPages] = useState([])
+
+	useEffect(() => {
 		const validateId = (validateIdBase || '') + '-' + stepsId
 
+		const updatedPages = []
+
 		stepsPages.forEach((page, index) => {
-			pages.push({
+			updatedPages.push({
 				title: t(page.title),
-				cancelBtn: cancelButton,
+				cancelFunction,
 				component: !page.pageValidation
 					? ValidItemHoc(page.page || page)
 					: page.page || page,
 				pageValidation: page.pageValidation,
 				goToNextPageIfValid: page.goToNextPageIfValid,
 				stepsId,
-				props: { ...this.props, stepsId, validateId: validateId + '-' + index },
+				props: { ...props, stepsId, validateId: validateId + '-' + index },
 			})
 		})
 
-		pages.push({
+		updatedPages.push({
 			title: t(stepsPreviewPage.title || 'PREVIEW'),
 			completeBtn: () => (
 				<SaveBtn {...rest} stepsId={stepsId} onSave={onSave} t={t} />
 			),
-			cancelBtn: cancelButton,
+			cancelFunction,
 			component: ValidItemHoc(stepsPreviewPage.page || stepsPreviewPage),
 			stepsId,
 			props: {
-				...this.props,
+				...props,
 				validateId: validateId + '-' + stepsPages.length,
 			},
 		})
 
-		return (
-			// <div style={{ textAlign: 'left' }}>
-			<MaterialStepper pages={pages} />
-			// </div>
-		)
-	}
+		setPages(updatedPages)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [validateIdBase, stepsId])
+
+	return <MaterialStepper pages={pages} closeDialog={closeDialog} />
 }
 
 FormSteps.propTypes = {
