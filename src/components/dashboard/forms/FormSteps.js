b6790a49b7ee83151ff848563f13ca925f932aa4
IvoPaunov <ivo.paunov@gmail.com>
2020-03-30 17:19:39 +0300

    Cleanup FormSteps

--- src/components/dashboard/forms/FormSteps.js
+++ src/components/dashboard/forms/FormSteps.js
@@ -1,69 +1,29 @@
 import React, { forwardRef, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import MaterialStepper from 'components/common/stepper/MaterialUiStepper'
-import ValidItemHoc from 'components/dashboard/forms/ValidItemHoc'
 import { t } from 'selectors'
 
-function FormSteps(props) {
-	const {
-		SaveBtn,
-		cancelFunction,
-		onSave,
-		stepsId,
-		stepsPages,
-		stepsPreviewPage,
-		validateIdBase,
-		closeDialog,
-		...rest
-	} = props
-
-	const [pages, setPages] = useState([])
+function FormSteps({ cancelFunction, stepsId, steps, closeDialog, ...rest }) {
+	const [stepperSteps, setSteps] = useState([])
 
 	useEffect(() => {
-		const validateId = (validateIdBase || '') + '-' + stepsId
-
-		const updatedPages = []
-
-		stepsPages.forEach((page, index) => {
-			updatedPages.push({
-				title: t(page.title),
-				cancelFunction,
-				component: !page.pageValidation
-					? ValidItemHoc(page.page || page)
-					: page.page || page,
-				pageValidation: page.pageValidation,
-				goToNextPageIfValid: page.goToNextPageIfValid,
-				stepsId,
-				props: { ...props, stepsId, validateId: validateId + '-' + index },
-			})
-		})
-
-		updatedPages.push({
-			title: t(stepsPreviewPage.title || 'PREVIEW'),
-			completeBtn: () => (
-				<SaveBtn {...rest} stepsId={stepsId} onSave={onSave} t={t} />
-			),
-			cancelFunction,
-			component: ValidItemHoc(stepsPreviewPage.page || stepsPreviewPage),
+		const updatedSteps = steps.map((s, index) => ({
+			...s,
 			stepsId,
-			props: {
-				...props,
-				validateId: validateId + '-' + stepsPages.length,
-			},
-		})
+			title: t(s.title),
+			props: { ...rest, stepsId, validateId: stepsId + '-' + index },
+		}))
 
-		setPages(updatedPages)
+		setSteps(updatedSteps)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [validateIdBase, stepsId])
+	}, [stepsId])
 
-	return <MaterialStepper pages={pages} closeDialog={closeDialog} />
+	return <MaterialStepper steps={stepperSteps} closeDialog={closeDialog} />
 }
 
 FormSteps.propTypes = {
-	title: PropTypes.string,
-	itemPages: PropTypes.arrayOf(PropTypes.func),
 	stepsId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-	stepsPages: PropTypes.arrayOf(PropTypes.object),
+	steps: PropTypes.arrayOf(PropTypes.object),
 }
 
 export default forwardRef((props, ref) => (
