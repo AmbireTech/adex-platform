8c7f382da07dc5970b4345d36e1f57b985acaccd
IvoPaunov <ivo.paunov@gmail.com>
2020-04-03 08:25:45 +0300

    init unit detials with hooks

--- src/components/dashboard/containers/Unit/Unit.js
+++ src/components/dashboard/containers/Unit/Unit.js
@@ -1,71 +1,41 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import actions from 'actions'
-import ItemHoc from 'components/dashboard/containers/ItemHoc'
+import React from 'react'
+
+import { useItem, DirtyProps } from 'components/dashboard/containers/ItemHoc/'
 import { AdUnit } from 'adex-models'
 import UnitTargets from 'components/dashboard/containers/UnitTargets'
-import Translate from 'components/translate/Translate'
+
 import { BasicProps } from 'components/dashboard/containers/ItemCommon'
+import { t } from 'selectors'
 
-export class Unit extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			closeDialog: false,
-		}
-	}
-	// shouldComponentUpdate(nextProps, nextState) {
-	//     let diffProps = JSON.stringify(this.props) !== JSON.stringify(nextProps)
-	//     let diffState = JSON.stringify(this.state) !== JSON.stringify(nextState)
-	//     return diffProps || diffState
-	// }
-	render() {
-		const { item, t, ...rest } = this.props
+function Unit({ match }) {
+	const {
+		item,
+		activeFields,
+		setActiveFields,
+		returnPropToInitialState,
+	} = useItem({ itemType: 'AdUnit', match, objModel: AdUnit })
+	if (!item) return <h1>Unit '404'</h1>
 
-		if (!item) return <h1>Unit '404'</h1>
-
-		return (
-			<div>
-				<BasicProps
-					item={item}
-					t={t}
-					url={item.adUrl}
-					rightComponent={
-						<UnitTargets
-							{...rest}
-							targets={item.targeting}
-							t={t}
-							subHeader={'PROP_TARGETING'}
-						/>
-					}
-				/>
-			</div>
-		)
-	}
+	return (
+		<div>
+			<DirtyProps
+				activeFields={activeFields}
+				returnPropToInitialState={returnPropToInitialState}
+			/>
+			<BasicProps
+				item={item}
+				t={t}
+				url={item.adUrl}
+				rightComponent={
+					<UnitTargets
+						targets={item.targeting}
+						t={t}
+						subHeader={'PROP_TARGETING'}
+					/>
+				}
+			/>
+		</div>
+	)
 }
 
-Unit.propTypes = {
-	actions: PropTypes.object.isRequired,
-	item: PropTypes.object.isRequired,
-}
-
-function mapStateToProps(state, props) {
-	return {
-		objModel: AdUnit,
-		itemType: 'AdUnit',
-	}
-}
-
-function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators(actions, dispatch),
-	}
-}
-
-const UnitItem = ItemHoc(Unit)
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Translate(UnitItem))
+export default Unit
