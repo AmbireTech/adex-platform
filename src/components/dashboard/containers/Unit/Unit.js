8507f02ad4223ecc0034149f72d0155081b35697
IvoPaunov <ivo.paunov@gmail.com>
2020-04-03 21:17:47 +0300

    work on unit details

--- src/components/dashboard/containers/Unit/Unit.js
+++ src/components/dashboard/containers/Unit/Unit.js
@@ -1,15 +1,25 @@
 import React from 'react'
-
+import {
+	Grid,
+	Card,
+	CardMedia,
+	TextField,
+	List,
+	ListItem,
+	ListItemText,
+	Divider,
+} from '@material-ui/core'
 import {
 	useItem,
 	DirtyProps,
 	ItemTitle,
 	ItemDescription,
-} from 'components/dashboard/containers/ItemHoc/'
+	MediaCard,
+} from 'components/dashboard/containers/ItemCommon/'
 import { AdUnit } from 'adex-models'
 import TargetsList from 'components/dashboard/containers/TargetsList'
-
-import { BasicProps } from 'components/dashboard/containers/ItemCommon'
+import Anchor from 'components/common/anchor/anchor'
+import Img from 'components/common/img/Img'
 import { t } from 'selectors'
 
 function Unit({ match }) {
@@ -19,7 +29,7 @@ function Unit({ match }) {
 		objModel: AdUnit,
 	})
 
-	const { title, description } = item
+	const { title, description, mediaUrl, mediaMime, type, targetUrl } = item
 	const { title: errTitle, description: errDescription } = validations
 
 	if (!item) return <h1>Unit '404'</h1>
@@ -27,24 +37,49 @@ function Unit({ match }) {
 	return (
 		<div>
 			<DirtyProps {...hookProps} />
-			<ItemTitle title={title} errTitle={errTitle} {...hookProps} />
-			<ItemDescription
-				description={description}
-				errDescription={errDescription}
-				{...hookProps}
-			/>
-			<BasicProps
-				item={item}
-				t={t}
-				url={item.adUrl}
-				rightComponent={
-					<TargetsList
-						targets={item.targeting}
-						t={t}
-						subHeader={'PROP_TARGETING'}
-					/>
-				}
-			/>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={12} md={6} lg={5}>
+					<List>
+						<ListItem>
+							<ListItemText
+								secondary={type}
+								primary={t('type', { isProp: true })}
+							/>
+						</ListItem>
+						<MediaCard mediaUrl={mediaUrl} mediaMime={mediaMime} />
+
+						<Divider />
+						<ListItem>
+							<ListItemText
+								secondary={
+									<Anchor href={targetUrl} target='_blank'>
+										{targetUrl}
+									</Anchor>
+								}
+								primary={t('targetUrl', { isProp: true })}
+							/>
+						</ListItem>
+					</List>
+				</Grid>
+				<Grid item xs={12} sm={12} md={6} lg={7}>
+					<List>
+						<ListItem>
+							<ItemTitle title={title} errTitle={errTitle} {...hookProps} />
+						</ListItem>
+						<Divider />
+						<ListItem>
+							<ItemDescription
+								description={description}
+								errDescription={errDescription}
+								{...hookProps}
+							/>
+						</ListItem>
+						<Divider />
+					</List>
+					<TargetsList targets={item.targeting} subHeader={'PROP_TARGETING'} />
+				</Grid>
+				<Grid item xs={12} sm={12} md={12} lg={6}></Grid>
+			</Grid>
 		</div>
 	)
 }
