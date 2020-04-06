3bdea81b7e08ef2b9d57351be7ac62b64f14a582
IvoPaunov <ivo.paunov@gmail.com>
2020-04-06 21:56:56 +0300

    add slot basic component

---
+++ src/components/dashboard/containers/Slot/SlotBasic.js
@@ -0,0 +1,81 @@
+import React, { Fragment } from 'react'
+import { Paper, Grid, Box } from '@material-ui/core'
+import { WebsiteIssues } from 'components/dashboard/containers/Slot/WebsiteIssues'
+import TargetsList from 'components/dashboard/containers/TargetsList'
+import {
+	DirtyProps,
+	ItemTitle,
+	ItemDescription,
+	ItemAdType,
+	ItemFallbackMediaURL,
+	MediaCard,
+	ItemMinPerImpression,
+	ItemWebsite,
+} from 'components/dashboard/containers/ItemCommon/'
+
+export const SlotBasic = ({ item, ...hookProps }) => {
+	const {
+		title,
+		description,
+		fallbackMediaUrl,
+		mediaMime,
+		type,
+		fallbackTargetUrl,
+		website,
+	} = item
+	const { title: errTitle, description: errDescription } = hookProps.validations
+
+	return (
+		<Fragment>
+			<DirtyProps {...hookProps} />
+			<Paper elevation={2}>
+				<Box p={2}>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={12} md={6} lg={5}>
+							<Box py={1}>
+								<MediaCard mediaUrl={fallbackMediaUrl} mediaMime={mediaMime} />
+							</Box>
+							<Box py={1}>
+								<ItemFallbackMediaURL targetUrl={fallbackTargetUrl} />
+							</Box>
+							<Box py={1}>
+								<TargetsList targets={item.tags} subHeader={'PROP_TAGS'} />
+							</Box>
+						</Grid>
+						<Grid item xs={12} sm={12} md={6} lg={7}>
+							<Box py={1}>
+								<ItemTitle title={title} errTitle={errTitle} {...hookProps} />
+							</Box>
+							<Box py={1}>
+								<ItemDescription
+									description={description}
+									errDescription={errDescription}
+									{...hookProps}
+								/>
+							</Box>
+							<Grid container spacing={1}>
+								<Grid item xs={12} sm={12} md={6}>
+									<Box py={1}>
+										<ItemAdType type={type} />
+									</Box>
+								</Grid>
+								<Grid item xs={12} sm={12} md={6}>
+									<Box py={1}>
+										<ItemMinPerImpression item={item} {...hookProps} />
+									</Box>
+								</Grid>
+							</Grid>
+							<Box py={1}>
+								<ItemWebsite item={item} {...hookProps} />
+							</Box>
+							<Box>
+								<WebsiteIssues website={website} />
+							</Box>
+						</Grid>
+						<Grid item xs={12} sm={12} md={12} lg={6}></Grid>
+					</Grid>
+				</Box>
+			</Paper>
+		</Fragment>
+	)
+}
