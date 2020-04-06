6f92f1bb0ede601acda4cf23f0966ad57d9b8efe
IvoPaunov <ivo.paunov@gmail.com>
2020-04-06 19:24:35 +0300

    update unit component

--- src/components/dashboard/containers/Unit/Unit.js
+++ src/components/dashboard/containers/Unit/Unit.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Grid, Paper, Box } from '@material-ui/core'
 import {
 	useItem,
@@ -8,15 +8,18 @@ import {
 	ItemAdType,
 	ItemTargetURL,
 	MediaCard,
+	SaveBtn,
 } from 'components/dashboard/containers/ItemCommon/'
 import { AdUnit } from 'adex-models'
 import TargetsList from 'components/dashboard/containers/TargetsList'
+import { validateAndUpdateUnit } from 'actions'
 
 function Unit({ match }) {
-	const { item, validations, ...hookProps } = useItem({
+	const { item, validations, validateId, validate, ...hookProps } = useItem({
 		itemType: 'AdUnit',
 		match,
 		objModel: AdUnit,
+		validateAndUpdateFn: validateAndUpdateUnit,
 	})
 
 	const { title, description, mediaUrl, mediaMime, type, targetUrl } = item
@@ -25,43 +28,46 @@ function Unit({ match }) {
 	if (!item) return <h1>Unit '404'</h1>
 
 	return (
-		<Paper>
-			<Box p={2}>
-				<DirtyProps {...hookProps} />
-				<Grid container spacing={2}>
-					<Grid item xs={12} sm={12} md={6} lg={5}>
-						<Box py={1}>
-							<MediaCard mediaUrl={mediaUrl} mediaMime={mediaMime} />
-						</Box>
-						<Box py={1}>
-							<ItemAdType type={type} />
-						</Box>
-						<Box py={1}>
-							<ItemTargetURL targetUrl={targetUrl} />
-						</Box>
+		<Fragment>
+			<SaveBtn validations={validations} {...hookProps} />
+			<DirtyProps {...hookProps} />
+			<Paper elevation={2}>
+				<Box p={2}>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={12} md={6} lg={5}>
+							<Box py={1}>
+								<MediaCard mediaUrl={mediaUrl} mediaMime={mediaMime} />
+							</Box>
+							<Box py={1}>
+								<ItemAdType type={type} />
+							</Box>
+							<Box py={1}>
+								<ItemTargetURL targetUrl={targetUrl} />
+							</Box>
+						</Grid>
+						<Grid item xs={12} sm={12} md={6} lg={7}>
+							<Box py={1}>
+								<ItemTitle title={title} errTitle={errTitle} {...hookProps} />
+							</Box>
+							<Box py={1}>
+								<ItemDescription
+									description={description}
+									errDescription={errDescription}
+									{...hookProps}
+								/>
+							</Box>
+							<Box py={1}>
+								<TargetsList
+									targets={item.targeting}
+									subHeader={'PROP_TARGETING'}
+								/>
+							</Box>
+						</Grid>
+						<Grid item xs={12} sm={12} md={12} lg={6}></Grid>
 					</Grid>
-					<Grid item xs={12} sm={12} md={6} lg={7}>
-						<Box py={1}>
-							<ItemTitle title={title} errTitle={errTitle} {...hookProps} />
-						</Box>
-						<Box py={1}>
-							<ItemDescription
-								description={description}
-								errDescription={errDescription}
-								{...hookProps}
-							/>
-						</Box>
-						<Box py={1}>
-							<TargetsList
-								targets={item.targeting}
-								subHeader={'PROP_TARGETING'}
-							/>
-						</Box>
-					</Grid>
-					<Grid item xs={12} sm={12} md={12} lg={6}></Grid>
-				</Grid>
-			</Box>
-		</Paper>
+				</Box>
+			</Paper>
+		</Fragment>
 	)
 }
 
