eb563af41e170749914a4f33ae3f20f8cfdb1285
IvoPaunov <ivo.paunov@gmail.com>
2020-04-07 14:06:31 +0300

    update slot tabs colors

--- src/components/dashboard/containers/Slot/Slot.js
+++ src/components/dashboard/containers/Slot/Slot.js
@@ -1,19 +1,54 @@
 import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import { AdSlot } from 'adex-models'
-import { Tab, Tabs, AppBar } from '@material-ui/core'
-
+import { Tab, Tabs, AppBar, Box } from '@material-ui/core'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
 import { useItem, SaveBtn } from 'components/dashboard/containers/ItemCommon/'
-import { validateAndUpdateSlot } from 'actions'
-
-import { t } from 'selectors'
-
 import { SlotBasic } from './SlotBasic'
 import { IntegrationCode } from './IntegrationCode'
+import { validateAndUpdateSlot } from 'actions'
+import { t } from 'selectors'
+
+export const styles = theme => {
+	return {
+		appBar: {
+			zIndex: theme.zIndex.appBar - 1,
+			backgroundColor: theme.palette.accentTwo.main,
+		},
+	}
+}
+
+const useStyles = makeStyles(styles)
+
+const StyledTabs = withStyles(theme => ({
+	indicator: {
+		backgroundColor: theme.palette.accentTwo.contrastText,
+	},
+}))(props => <Tabs {...props} />)
+
+const StyledTab = withStyles(theme => ({
+	root: {
+		color: theme.palette.accentTwo.contrastText,
+		opacity: 0.69,
+		'&:hover': {
+			color: theme.palette.accentTwo.contrastText,
+			opacity: 1,
+		},
+		'&$selected': {
+			color: theme.palette.accentTwo.contrastText,
+			opacity: 1,
+		},
+		'&:focus': {
+			color: theme.palette.accentTwo.contrastText,
+			opacity: 1,
+		},
+	},
+	selected: {},
+}))(props => <Tab {...props} />)
 
 function Slot({ match }) {
+	const classes = useStyles()
 	const [tabIndex, setTabIndex] = useState(0)
-
 	const { item, ...hookProps } = useItem({
 		itemType: 'AdSlot',
 		match,
@@ -24,24 +59,24 @@ function Slot({ match }) {
 	return (
 		<Fragment>
 			<SaveBtn {...hookProps} />
-			<AppBar position='static' color='default'>
-				<Tabs
+			<AppBar position='static' className={classes.appBar}>
+				<StyledTabs
 					value={tabIndex}
 					onChange={(ev, index) => setTabIndex(index)}
 					scrollButtons='auto'
 					indicatorColor='primary'
 					textColor='primary'
 				>
-					<Tab label={t('SLOT_BASIC')} />
-					<Tab label={t('INTEGRATION')} />
+					<StyledTab label={t('SLOT_BASIC')} />
+					<StyledTab label={t('INTEGRATION')} />
 					{/* There are no stats displayed currently so I will just comment this out */}
 					{/* <Tab label={t('STATISTICS')} /> */}
-				</Tabs>
+				</StyledTabs>
 			</AppBar>
-			<div style={{ marginTop: 10 }}>
+			<Box my={2}>
 				{tabIndex === 0 && <SlotBasic item={item} {...hookProps} />}
 				{tabIndex === 1 && <IntegrationCode slot={item} />}
-			</div>
+			</Box>
 		</Fragment>
 	)
 }
