155e24a82dcf4b1c2c57b543e505fcf667b2a193
IvoPaunov <ivo.paunov@gmail.com>
2020-04-14 23:23:33 +0300

    add analytics ui selectors

--- src/selectors/uiSelectors.js
+++ src/selectors/uiSelectors.js
@@ -1,4 +1,6 @@
 import { createSelector } from 'reselect'
+import dateUtils from 'helpers/dateUtils'
+
 import { selectAccountIdentityAddr } from './accountSelectors'
 
 const REGISTRATION_OPEN = process.env.REGISTRATION_OPEN === 'true'
@@ -15,6 +17,11 @@ export const selectSpinners = state => state.memory.spinners
 export const selectIdentityUi = createSelector(
 	[selectAccountIdentityAddr, selectIdentitiesUi],
 	(identityAddr, identitiesUi) => identitiesUi[identityAddr] || {}
+)
+
+export const selectIdentitySideUi = createSelector(
+	[selectIdentityUi, selectSide],
+	(identityUi, side) => identityUi[side] || {}
 )
 
 export const selectCompanyData = createSelector(
@@ -77,4 +84,19 @@ export const selectLoginDirectSide = createSelector(
 export const selectInitialDataLoaded = createSelector(
 	selectIdentityUi,
 	({ initialDataLoaded }) => initialDataLoaded
+)
+
+export const selectIdentitySideAnalyticsTimeframe = createSelector(
+	[selectIdentitySideUi],
+	({ sideAnalyticsTimeframe = 'day' } = {}) => sideAnalyticsTimeframe
+)
+
+export const selectIdentitySideAnalyticsPeriod = createSelector(
+	[selectIdentitySideUi],
+	({
+		sideAnalyticsPeriod = {
+			start: +dateUtils.startOfDay(dateUtils.date()),
+			end: +dateUtils.endOfDay(dateUtils.date()),
+		},
+	} = {}) => sideAnalyticsPeriod
 )
