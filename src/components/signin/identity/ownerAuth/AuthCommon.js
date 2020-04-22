a18bd16c99678c29d7d2468e5094f5a0830f489f
IvoPaunov <ivo.paunov@gmail.com>
2020-04-22 23:50:07 +0300

    add AddressSelect to auth common components

--- src/components/signin/identity/ownerAuth/AuthCommon.js
+++ src/components/signin/identity/ownerAuth/AuthCommon.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import ListItemText from '@material-ui/core/ListItemText'
+import { List, ListItem, ListItemText } from '@material-ui/core'
+import {
+	ContentBox,
+	ContentBody,
+	ContentStickyTop,
+	TopLoading,
+} from 'components/common/dialog/content'
 import { makeStyles } from '@material-ui/core/styles'
 import { styles } from './styles'
+import { selectWalletAddress, execute } from 'actions'
 
 const useStyles = makeStyles(styles)
 
@@ -27,5 +34,49 @@ export function AddrItem({ stats }) {
 				</span>
 			}
 		/>
+	)
+}
+
+export const AddressSelect = ({
+	addresses,
+	address,
+	waitingAction,
+	actionWaitingLabel,
+	selectLabel,
+	signType,
+	authType,
+	hdWalletAddrPath,
+	classes,
+}) => {
+	return (
+		<ContentBox>
+			<ContentStickyTop>
+				{waitingAction ? <TopLoading msg={actionWaitingLabel} /> : selectLabel}
+			</ContentStickyTop>
+			<ContentBody>
+				<List>
+					{addresses.map((addrData, hdWalletAddrIdx) => (
+						<ListItem
+							classes={{ root: classes.addrListItem }}
+							key={addrData.address}
+							onClick={() =>
+								execute(
+									selectWalletAddress({
+										addrData,
+										hdWalletAddrPath,
+										hdWalletAddrIdx,
+										signType,
+										authType,
+									})
+								)
+							}
+							selected={address === addrData.address}
+						>
+							<AddrItem stats={addrData} address={addrData.address} />
+						</ListItem>
+					))}
+				</List>
+			</ContentBody>
+		</ContentBox>
 	)
 }
