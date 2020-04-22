48aecb112c0b3443e5a8c7c2a50d8d0029aec1fb
rori4 <rangelstoilov@gmail.com>
2020-04-22 16:12:59 +0300

    style cleanup

--- src/components/dashboard/deposit/styles.js
+++ src/components/dashboard/deposit/styles.js
@@ -1,32 +1,9 @@
 export const styles = theme => {
 	return {
-		root: {
-			maxWidth: 400,
-			// minHeight: 300,
-			height: '100%',
-			flexGrow: 1,
-			display: 'flex',
-			flex: 1,
-			flexDirection: 'column',
-			justifyContent: 'space-between',
-		},
-		badgeFull: {
-			width: '100%',
-		},
 		copyBtn: {
 			backgroundColor: '#E2EAED',
 			overflowWrap: 'anywhere',
 			userSelect: 'text',
-		},
-		bullet: {
-			display: 'inline-block',
-			margin: '0 2px',
-			transform: 'scale(0.8)',
-		},
-		authImg: {
-			width: '1em',
-			height: '1em',
-			display: 'flex',
 		},
 		title: {
 			fontSize: '2rem',
@@ -38,20 +15,9 @@ export const styles = theme => {
 			textAlign: 'center',
 			textTransform: 'uppercase',
 		},
-		pos: {
-			marginBottom: 12,
-		},
-		actions: {
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'space-between',
-		},
-		onRampLogo: {
-			margin: '10px',
-		},
-		onRamp: {
-			display: 'flex',
-			justifyContent: 'space-between',
+		paper: {
+			height: '100%',
+			width: '100%',
 		},
 		info: {
 			fontSize: '10px',
@@ -62,17 +28,8 @@ export const styles = theme => {
 		infoTitle: {
 			fontSize: '12px',
 		},
-		rampInfoTitle: {
-			fontSize: '0.8rem',
-		},
-		listItem: {
-			display: 'flex',
-			flexWrap: 'wrap',
-			flex: 1,
-			maxWidth: '100%',
-		},
 		img: {
-			maxWidth: '100%',
+			width: '100%',
 		},
 	}
 }
