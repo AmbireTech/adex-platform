ae5b2a3f3047e3656c5da92ae307c5e200bf2b75
IvoPaunov <ivo.paunov@gmail.com>
2020-04-23 00:17:43 +0300

    delete ledger constants and logo

--- src/helpers/logosHelpers.js
+++ src/helpers/logosHelpers.js
@@ -1,7 +1,6 @@
 import { AUTH_TYPES } from 'constants/misc'
 import metamaskLogo from 'resources/metamask-logo.png'
 import trezorLogo from 'resources/trezor-logo-h.png'
-import ledgerLogo from 'resources/ledger_logo_header.png'
 import demoLogo from 'resources/demo-logo.png'
 
 export const getAuthLogo = authType => {
@@ -10,8 +9,6 @@ export const getAuthLogo = authType => {
 			return metamaskLogo
 		case AUTH_TYPES.TREZOR.name:
 			return trezorLogo
-		case AUTH_TYPES.LEDGER.name:
-			return ledgerLogo
 		case AUTH_TYPES.DEMO.name:
 			return demoLogo
 		default:
