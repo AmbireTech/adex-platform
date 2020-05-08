a504c1b49b8708faae67a25bf392e080581fd280
IvoPaunov <ivo.paunov@gmail.com>
2020-05-08 18:55:51 +0300

    update websites page

--- src/components/dashboard/containers/Websites/Websites.js
+++ src/components/dashboard/containers/Websites/Websites.js
@@ -8,17 +8,21 @@ import {
 	ListItemText,
 	ListItemIcon,
 	ListItemSecondaryAction,
-	IconButton,
-	Typography,
+	Button,
+	ListSubheader,
 	Paper,
+	Box,
 } from '@material-ui/core'
 import { CheckSharp, WarningSharp, RefreshSharp } from '@material-ui/icons'
-import { selectWebsitesArray } from 'selectors'
+import {
+	WebsiteIssues,
+	WebsiteIssuesLegend,
+} from 'components/dashboard/containers/Slot/WebsiteIssues'
+import { t, selectWebsitesArray } from 'selectors'
 
 const useStyles = makeStyles(theme => ({
 	list: {
 		width: '100%',
-		backgroundColor: theme.palette.background.paper,
 	},
 	inline: {
 		display: 'inline',
@@ -37,43 +41,52 @@ export default function Websites() {
 	const websites = useSelector(selectWebsitesArray)
 
 	return (
-		<Paper variant='outlined'>
-			<List className={classes.list} dense>
-				{websites.map((ws, index) => (
-					<Fragment key={ws.id}>
-						<ListItem alignItems='flex-start'>
-							<ListItemIcon>
-								{ws.issues.length ? (
-									<WarningSharp className={classes.warning} />
-								) : (
-									<CheckSharp className={classes.success} />
-								)}
-							</ListItemIcon>
-							<ListItemText
-								primary={ws.id}
-								secondary={
-									// TODO:
-									<Typography
-										component='span'
-										variant='caption'
-										className={classes.inline}
-									>
-										{!!ws.issues && ws.issues.join(', ')}
-									</Typography>
-								}
-							/>
-							<ListItemSecondaryAction>
-								{!!ws.issues.length && (
-									<IconButton edge='end'>
-										<RefreshSharp />
-									</IconButton>
-								)}
-							</ListItemSecondaryAction>
-						</ListItem>
-						{index < websites.length - 1 && <Divider />}
-					</Fragment>
-				))}
-			</List>
-		</Paper>
+		<Fragment>
+			<Box>
+				<Paper variant='outlined'>
+					<List className={classes.list} dense disablePadding>
+						<ListSubheader disableSticky>
+							{t('REGISTERED_WEBSITES')}
+						</ListSubheader>
+						{websites.map((ws, index) => (
+							<Fragment key={ws.id}>
+								<ListItem>
+									<ListItemIcon>
+										{ws.issues.length ? (
+											<WarningSharp className={classes.warning} />
+										) : (
+											<CheckSharp className={classes.success} />
+										)}
+									</ListItemIcon>
+									<ListItemText
+										primary={ws.id}
+										secondary={<WebsiteIssues issues={ws.issues} asIcons />}
+									/>
+									<ListItemSecondaryAction>
+										{!!ws.issues.length && (
+											<Button
+												variant='contained'
+												color='primary'
+												startIcon={<RefreshSharp />}
+												size='small'
+												edge='end'
+											>
+												{t('TRY_VERIFY')}
+											</Button>
+										)}
+									</ListItemSecondaryAction>
+								</ListItem>
+								{index < websites.length - 1 && <Divider />}
+							</Fragment>
+						))}
+					</List>
+				</Paper>
+			</Box>
+			<Box mt={2}>
+				<Paper variant='outlined'>
+					<WebsiteIssuesLegend />
+				</Paper>
+			</Box>
+		</Fragment>
 	)
 }
