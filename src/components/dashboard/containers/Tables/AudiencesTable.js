045bffa8fb6391e45ff6293679c23c3a820a90db
IvoPaunov <ivo.paunov@gmail.com>
2020-06-10 16:50:35 +0300

    fix adudiences table

--- src/components/dashboard/containers/Tables/AudiencesTable.js
+++ src/components/dashboard/containers/Tables/AudiencesTable.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Tooltip, Button } from '@material-ui/core'
+import React, { Fragment } from 'react'
+import { Tooltip, Button, ButtonGroup } from '@material-ui/core'
 import { EditSharp } from '@material-ui/icons'
+import CampaignIcon from 'components/common/icons/CampaignIcon'
+
 import MUIDataTableEnhanced from 'components/dashboard/containers/Tables/MUIDataTableEnhanced'
 import {
 	t,
@@ -21,30 +23,55 @@ const getCols = ({ classes, symbol }) => [
 	{
 		name: 'id',
 		label: t('PROP_ID'),
+		options: {
+			filter: false,
+			sort: false,
+		},
 	},
 	{
 		name: 'inputs',
 		label: t('PROP_INPUTS'),
+		options: {
+			filter: false,
+			sort: false,
+			customBodyRender: ({ location, categories, publishers }) => '',
+		},
 	},
 	{
 		name: 'actions',
 		label: t('ACTIONS'),
 		options: {
 			filter: false,
-			sort: true,
-			download: false,
-			customBodyRender: ({ to, item }) => (
-				<Tooltip
-					key={item.id}
-					title={t('LABEL_EDIT')}
-					// placement='top'
-					enterDelay={1000}
-					aria-label='view'
-				>
-					<Button to={to} variant='contained'>
-						<EditSharp color='primary' />
-					</Button>
-				</Tooltip>
+			sort: false,
+			customBodyRender: ({ id, item }) => (
+				<Fragment key={id}>
+					<Tooltip
+						title={t('LABEL_EDIT')}
+						// placement='top'
+						enterDelay={1000}
+						aria-label='view'
+					>
+						<Button
+							// startIcon={<EditSharp />}
+							variant='contained'
+						>
+							{t('EDIT')}
+						</Button>
+					</Tooltip>
+					<Tooltip
+						title={t('LABEL_NEW_CAMPAIGN')}
+						// placement='top'
+						enterDelay={1000}
+						aria-label='view'
+					>
+						<Button
+							// startIcon={<CampaignIcon />}
+							variant='contained'
+						>
+							{t('NEW_CAMPAIGN')}
+						</Button>
+					</Tooltip>
+				</Fragment>
 			),
 		},
 	},
