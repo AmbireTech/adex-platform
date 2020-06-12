ef256574899f4ceca8914ae6e2439b2833902356
IvoPaunov <ivo.paunov@gmail.com>
2020-06-13 01:51:36 +0300

    work on audience input validation action

--- src/actions/audienceActions.js
+++ src/actions/audienceActions.js
@@ -1,9 +1,17 @@
 import { ADD_ITEM } from 'constants/actionTypes'
-import { addToast } from 'actions'
+import { addToast, updateSpinner } from 'actions'
 import { postAudience } from 'services/adex-market/actions'
 
 import { getErrorMsg } from 'helpers/errors'
-import { t } from 'selectors'
+import { t, selectNewAudience } from 'selectors'
+import {
+	// validateSchemaProp,
+	handleAfterValidation,
+} from 'actions'
+// import { schemas } from 'adex-models'
+
+// TODO:
+// const { audiencePost, audiencePu } = schemas
 
 export function saveAudience({ campaignId, audienceInput }) {
 	return async function(dispatch, getState) {
@@ -29,5 +37,48 @@ export function saveAudience({ campaignId, audienceInput }) {
 			})(dispatch)
 			throw new Error('ERR_CREATING_ITEM', err)
 		}
+	}
+}
+
+export function validateAudienceBasics({
+	validateId,
+	dirty,
+	onValid,
+	onInvalid,
+}) {
+	return async function(dispatch, getState) {
+		await updateSpinner(validateId, true)(dispatch)
+		// TODO:
+		let isValid = true
+		try {
+			const state = getState()
+			const audience = selectNewAudience(state)
+			const { title, inputs, version } = audience
+
+			// TODO:
+			// const validations = await Promise.all([
+			// 	validateSchemaProp({
+			// 		validateId,
+			// 		value: title,
+			// 		prop: 'title',
+			// 		schema: audiencePost.title,
+			// 		dirty,
+			// 	})(dispatch),
+			// 	validateSchemaProp({
+			// 		validateId,
+			// 		value: inputs,
+			// 		prop: 'inputs',
+			// 		schema: audiencePost.description,
+			// 		dirty,
+			// 	})(dispatch),
+			// ])
+
+			// isValid = validations.every(v => v === true)
+		} catch (err) {
+			console.error('ERR_VALIDATING_SLOT_BASIC', err)
+		}
+
+		await handleAfterValidation({ isValid, onValid, onInvalid })
+		await updateSpinner(validateId, false)(dispatch)
 	}
 }
