3591bbcc475dddf49d8cb1b6aa76ccffb6cc2c91
IvoPaunov <ivo.paunov@gmail.com>
2020-06-12 15:34:13 +0300

    add campaign audience update popup

--- src/components/dashboard/containers/Campaign/CampaignAudience.js
+++ src/components/dashboard/containers/Campaign/CampaignAudience.js
@@ -7,21 +7,66 @@ import {
 	ExpansionPanelSummary,
 	Typography,
 } from '@material-ui/core'
+import WithDialog from 'components/common/dialog/WithDialog'
 import { t, selectAudienceByCampaignId } from 'selectors'
 import AudiencePreview from 'components/dashboard/containers/AudiencePreview'
+import FormSteps from 'components/common/stepper/FormSteps'
 import { ExpandMoreSharp as ExpandMoreIcon } from '@material-ui/icons'
-import { NewCampaignFromAudience } from 'components/dashboard/forms/items/NewItems'
-import { execute, updateNewCampaign } from 'actions'
+import { ChangeControls } from 'components/dashboard/containers/ItemCommon/'
+import {
+	NewCampaignFromAudience,
+	CampaignTargetingRules,
+} from 'components/dashboard/forms/items/NewItems'
+import {
+	execute,
+	updateNewCampaign,
+	resetNewItem,
+	updateCampaignAudienceInput,
+} from 'actions'
+import { Campaign } from 'adex-models'
 
-export const CampaignAudience = ({ item }) => {
+export const TargetingSteps = ({ updateField, itemId, ...props }) => {
+	return (
+		<FormSteps
+			{...props}
+			cancelFunction={() => execute(resetNewItem('Campaign', itemId))}
+			itemId={itemId}
+			validateIdBase={`update-campaign-${itemId}-`}
+			itemType='Campaign'
+			stepsId={`update-campaign-${itemId}`}
+			steps={[
+				{
+					title: 'CAMPAIGN_AUDIENCE',
+					component: CampaignTargetingRules,
+					completeBtnTitle: 'OK',
+					completeFn: props =>
+						execute(
+							updateCampaignAudienceInput({ updateField, itemId, ...props })
+						),
+				},
+			]}
+			itemModel={Campaign}
+		/>
+	)
+}
+
+const TargetingRulesEdit = WithDialog(TargetingSteps)
+
+export const CampaignAudience = ({ item, ...hookProps }) => {
 	const { inputs } =
 		useSelector(state => selectAudienceByCampaignId(state, item.id)) || {}
 
+	const audienceInput = Object.keys(item.audienceInput.inputs).length
+		? item.audienceInput.inputs
+		: inputs
+
 	return (
 		<Fragment>
+			<ChangeControls {...hookProps} />
+
 			<Paper elevation={2} variant='outlined'>
 				<Box p={1}>
-					<AudiencePreview audienceInput={inputs} />
+					<AudiencePreview audienceInput={audienceInput} />
 				</Box>
 				<Box p={2}>
 					<NewCampaignFromAudience
@@ -31,10 +76,20 @@ export const CampaignAudience = ({ item }) => {
 						onBeforeOpen={() =>
 							execute(
 								updateNewCampaign('audienceInput', {
-									...inputs,
+									...audienceInput,
 								})
 							)
 						}
+					/>
+
+					<TargetingRulesEdit
+						btnLabel='UPDATE_TAGS'
+						title='UPDATE_SLOT_TAGS'
+						itemId={item.id}
+						disableBackdropClick
+						updateField={hookProps.updateField}
+						color='secondary'
+						variant='contained'
 					/>
 				</Box>
 			</Paper>
