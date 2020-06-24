ad2d801efb408fdf4e1a88128adaee4d20a4bc5d
IvoPaunov <ivo.paunov@gmail.com>
2020-06-24 15:22:44 +0300

    add new targeting reducer and update selectors

--- src/selectors/analyticsSelectors.js
+++ src/selectors/analyticsSelectors.js
@@ -14,6 +14,7 @@ import { DEFAULT_DATETIME_FORMAT } from 'helpers/formatters'
 import { t } from './translationsSelectors'
 
 export const selectAnalytics = state => state.memory.analytics
+export const selectTargeting = state => state.persist.targeting
 
 const MIN_SLOTS_FOR_AD_TYPE = 2
 
@@ -68,8 +69,8 @@ export const selectDemandAnalytics = createSelector(
 )
 
 export const selectTargetingAnalytics = createSelector(
-	[selectAnalytics],
-	({ targeting = {} }) => targeting.targetingData || []
+	[selectTargeting],
+	({ targetingData = {} }) => targetingData || []
 )
 
 export const selectTargetingAnalyticsWithMinSlotsCountByType = createSelector(
