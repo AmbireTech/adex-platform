9efd01f0ef4fa29b92c41ef6368bc6d93f0848d7
IvoPaunov <ivo.paunov@gmail.com>
2020-07-15 14:56:38 +0300

    update audience preview layout

--- src/components/dashboard/containers/AudiencePreview/AudiencePreview.js
+++ src/components/dashboard/containers/AudiencePreview/AudiencePreview.js
@@ -18,126 +18,115 @@ const AudiencePreview = ({ audienceInput = {}, title, subHeader }) => {
 
 	return (
 		<Box>
-			<Box m={1}>
-				{title && <OutlinedPropView label={t('TITLE')} value={title} />}
-				{!!location.apply ? (
-					<OutlinedPropView
-						margin='dense'
-						label={t(`LOCATION_${location.apply.toUpperCase()}`)}
-						value={
-							location.apply === 'allin' ? (
-								<Chip
-									variant='outlined'
-									size='small'
-									label={t('ALL_COUNTRIES')}
-								/>
-							) : (
-								(location[location.apply] || []).map(x => (
-									<Tooltip
-										key={x}
-										title={
-											CountryTiers[x]
-												? CountryTiers[x].countries.join(', ')
-												: t(CountryNames[x] || x || '')
-										}
-									>
-										<Chip variant='outlined' size='small' label={t(x || '')} />
-									</Tooltip>
-								))
-							)
-						}
-					/>
-				) : (
-					<OutlinedPropView
-						margin='dense'
-						label={t(`LOCATION`)}
-						value={t('NOT_SELECTED')}
-					/>
-				)}
-			</Box>
-			{!!categories.apply ? (
-				categories.apply.map(apply => (
-					<Box key={apply} m={1}>
-						<OutlinedPropView
-							margin='dense'
-							label={t(`CATEGORIES_${(apply || '').toUpperCase()}`)}
-							value={(categories[apply] || []).map(cat => (
-								<Chip
-									key={cat}
-									variant='outlined'
-									size='small'
-									label={t(
-										IabCategories.wrbshrinkerWebsiteApiV3Categories[cat] || cat
-									)}
-								/>
-							))}
-						/>
-					</Box>
-				))
+			{title && <OutlinedPropView label={t('TITLE')} value={title} />}
+
+			{!!location.apply ? (
+				<OutlinedPropView
+					margin='dense'
+					label={t(`LOCATION_${location.apply.toUpperCase()}`)}
+					value={
+						location.apply === 'allin' ? (
+							<Chip
+								variant='outlined'
+								size='small'
+								label={t('ALL_COUNTRIES')}
+							/>
+						) : (
+							(location[location.apply] || []).map(x => (
+								<Tooltip
+									key={x}
+									title={
+										CountryTiers[x]
+											? CountryTiers[x].countries.join(', ')
+											: t(CountryNames[x] || x || '')
+									}
+								>
+									<Chip variant='outlined' size='small' label={t(x || '')} />
+								</Tooltip>
+							))
+						)
+					}
+				/>
 			) : (
-				<Box m={1}>
-					<OutlinedPropView
-						margin='dense'
-						label={t(`CATEGORIES`)}
-						value={t('NOT_SELECTED')}
-					/>
-				</Box>
+				<OutlinedPropView
+					margin='dense'
+					label={t(`LOCATION`)}
+					value={t('NOT_SELECTED')}
+				/>
 			)}
 
-			<Box m={1}>
-				{publishers.apply ? (
+			{!!categories.apply ? (
+				categories.apply.map(apply => (
 					<OutlinedPropView
+						key={apply}
 						margin='dense'
-						label={t(`PUBLISHERS_${publishers.apply.toUpperCase()}`)}
-						value={
-							publishers.apply === 'allin' ? (
+						label={t(`CATEGORIES_${(apply || '').toUpperCase()}`)}
+						value={(categories[apply] || []).map(cat => (
+							<Chip
+								key={cat}
+								variant='outlined'
+								size='small'
+								label={t(
+									IabCategories.wrbshrinkerWebsiteApiV3Categories[cat] || cat
+								)}
+							/>
+						))}
+					/>
+				))
+			) : (
+				<OutlinedPropView
+					margin='dense'
+					label={t(`CATEGORIES`)}
+					value={t('NOT_SELECTED')}
+				/>
+			)}
+
+			{publishers.apply ? (
+				<OutlinedPropView
+					margin='dense'
+					label={t(`PUBLISHERS_${publishers.apply.toUpperCase()}`)}
+					value={
+						publishers.apply === 'allin' ? (
+							<Chip
+								variant='outlined'
+								size='small'
+								label={t('ALL_PUBLISHERS')}
+							/>
+						) : (
+							(publishers[publishers.apply] || []).map((x, index) => (
 								<Chip
+									key={index}
 									variant='outlined'
 									size='small'
-									label={t('ALL_PUBLISHERS')}
+									label={(JSON.parse(x) || {}).hostname}
 								/>
-							) : (
-								(publishers[publishers.apply] || []).map((x, index) => (
-									<Chip
-										key={index}
-										variant='outlined'
-										size='small'
-										label={(JSON.parse(x) || {}).hostname}
-									/>
-								))
-							)
-						}
-					/>
-				) : (
-					<OutlinedPropView
-						margin='dense'
-						label={t(`PUBLISHERS`)}
-						value={t('NOT_SELECTED')}
-					/>
-				)}
-			</Box>
+							))
+						)
+					}
+				/>
+			) : (
+				<OutlinedPropView
+					margin='dense'
+					label={t(`PUBLISHERS`)}
+					value={t('NOT_SELECTED')}
+				/>
+			)}
 
-			<Box m={1}>
-				<OutlinedPropView
-					margin='dense'
-					label={t(`INCLUDE_INCENTIVIZED_TRAFFIC`)}
-					value={t(advanced.includeIncentivized ? 'YES' : 'NO')}
-				/>
-			</Box>
-			<Box m={1}>
-				<OutlinedPropView
-					margin='dense'
-					label={t(`DISABLE_FREQUENCY_CAPPING`)}
-					value={t(advanced.disableFrequencyCapping ? 'YES' : 'NO')}
-				/>
-			</Box>
-			<Box m={1}>
-				<OutlinedPropView
-					margin='dense'
-					label={t(`LIMIT_AVERAGE_DAILY_SPENDING`)}
-					value={t(advanced.limitDailyAverageSpending ? 'YES' : 'NO')}
-				/>
-			</Box>
+			<OutlinedPropView
+				margin='dense'
+				label={t(`INCLUDE_INCENTIVIZED_TRAFFIC`)}
+				value={t(advanced.includeIncentivized ? 'YES' : 'NO')}
+			/>
+			<OutlinedPropView
+				margin='dense'
+				label={t(`DISABLE_FREQUENCY_CAPPING`)}
+				value={t(advanced.disableFrequencyCapping ? 'YES' : 'NO')}
+			/>
+			<OutlinedPropView
+				margin='dense'
+				label={t(`LIMIT_AVERAGE_DAILY_SPENDING`)}
+				value={t(advanced.limitDailyAverageSpending ? 'YES' : 'NO')}
+			/>
 		</Box>
 	)
 }
