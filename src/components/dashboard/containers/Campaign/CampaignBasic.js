c0ebb457204c3f1b598c8a7ef261eba8fbdb6e81
IvoPaunov <ivo.paunov@gmail.com>
2020-07-15 14:45:14 +0300

    cleanup and update campaign basic detials component

--- src/components/dashboard/containers/Campaign/CampaignBasic.js
+++ src/components/dashboard/containers/Campaign/CampaignBasic.js
@@ -72,232 +72,205 @@ export const CampaignBasic = ({ item, ...hookProps }) => {
 	)
 
 	return (
-		<Fragment>
-			<ChangeControls {...hookProps} />
-			<Paper variant='outlined'>
-				<Box p={2}>
-					<Grid container spacing={2}>
-						<Grid item xs={12} sm={12} md={6} lg={5}>
-							<Box my={1}>
-								<MediaCard mediaUrl={mediaUrl} mediaMime={mediaMime} />
-							</Box>
-							{['Ready', 'Active', 'Unhealthy'].includes(status.name) && (
-								<Grid container spacing={1} alignItems='center'>
-									<Grid item xs={12} sm={6} md={12} lg={6}>
-										<div className={classes.wrapper}>
-											<Button
-												variant='contained'
-												color='secondary'
-												size='large'
-												fullWidth
-												onClick={() => {
-													execute(
-														confirmAction(
-															() => execute(closeCampaign({ campaign: item })),
-															null,
-															{
-																confirmLabel: t('CLOSE_CAMPAIGN_CONFIRM_LABEL'),
-																cancelLabel: t('CANCEL'),
-																title: t('CLOSE_CAMPAIGN_CONFIRM_TITLE', {
-																	args: [title],
-																}),
-																text: t('CLOSE_CAMPAIGN_CONFIRM_INFO', {
-																	args: [],
-																}),
-															}
-														)
-													)
-												}}
-												disabled={
-													closeSpinner || humanFriendlyName === 'Closed'
+		<Grid container spacing={2}>
+			<Grid item xs={12} sm={12} md={6} lg={5}>
+				<Box my={1}>
+					<MediaCard mediaUrl={mediaUrl} mediaMime={mediaMime} />
+				</Box>
+				{['Ready', 'Active', 'Unhealthy'].includes(status.name) && (
+					<Grid container spacing={1} alignItems='center'>
+						<Grid item xs={12} sm={6} md={12} lg={6}>
+							<div className={classes.wrapper}>
+								<Button
+									variant='contained'
+									color='secondary'
+									size='large'
+									fullWidth
+									onClick={() => {
+										execute(
+											confirmAction(
+												() => execute(closeCampaign({ campaign: item })),
+												null,
+												{
+													confirmLabel: t('CLOSE_CAMPAIGN_CONFIRM_LABEL'),
+													cancelLabel: t('CANCEL'),
+													title: t('CLOSE_CAMPAIGN_CONFIRM_TITLE', {
+														args: [title],
+													}),
+													text: t('CLOSE_CAMPAIGN_CONFIRM_INFO', {
+														args: [],
+													}),
 												}
-												endIcon={<StopSharp />}
-											>
-												{t('BTN_CLOSE_CAMPAIGN')}
-											</Button>
-											{closeSpinner && (
-												<CircularProgress
-													size={24}
-													className={classes.buttonProgress}
-												/>
-											)}
-										</div>
-									</Grid>
-
-									<Grid item xs={12} sm={6} md={12} lg={6}>
-										<div className={classes.wrapper}>
-											<Button
-												variant='contained'
-												color='secondary'
-												size='large'
-												fullWidth
-												onClick={() => {
-													execute(
-														confirmAction(
-															() =>
-																execute(
-																	pauseOrResumeCampaign({ campaign: item })
-																),
-															null,
-															{
-																confirmLabel: t(
-																	`${pauseAction}_CAMPAIGN_CONFIRM_LABEL`
-																),
-																cancelLabel: t('CANCEL'),
-																title: t(
-																	`${pauseAction}_CAMPAIGN_CONFIRM_TITLE`,
-																	{
-																		args: [title],
-																	}
-																),
-																text: t(
-																	`${pauseAction}_CAMPAIGN_CONFIRM_INFO`,
-																	{
-																		args: [],
-																	}
-																),
-															}
-														)
-													)
-												}}
-												disabled={
-													pauseSpinner || humanFriendlyName === 'Closed'
-												}
-												endIcon={isPaused ? <StarSharp /> : <PauseSharp />}
-											>
-												{t(`BTN_${pauseAction}_CAMPAIGN`)}
-											</Button>
-											{pauseSpinner && (
-												<CircularProgress
-													size={24}
-													className={classes.buttonProgress}
-												/>
-											)}
-										</div>
-									</Grid>
-								</Grid>
-							)}
+											)
+										)
+									}}
+									disabled={closeSpinner || humanFriendlyName === 'Closed'}
+									endIcon={<StopSharp />}
+								>
+									{t('BTN_CLOSE_CAMPAIGN')}
+								</Button>
+								{closeSpinner && (
+									<CircularProgress
+										size={24}
+										className={classes.buttonProgress}
+									/>
+								)}
+							</div>
 						</Grid>
 
-						<Grid item xs={12} sm={12} md={6} lg={7}>
-							<Box my={1}>
-								<ItemTitle title={title} errTitle={errTitle} {...hookProps} />
-							</Box>
-							<Box my={1}>
-								<ItemSpecProp
-									prop={'id'}
-									value={item.id}
-									label={t('id', { isProp: true })}
-								/>
-							</Box>
+						<Grid item xs={12} sm={6} md={12} lg={6}>
+							<div className={classes.wrapper}>
+								<Button
+									variant='contained'
+									color='secondary'
+									size='large'
+									fullWidth
+									onClick={() => {
+										execute(
+											confirmAction(
+												() =>
+													execute(pauseOrResumeCampaign({ campaign: item })),
+												null,
+												{
+													confirmLabel: t(
+														`${pauseAction}_CAMPAIGN_CONFIRM_LABEL`
+													),
+													cancelLabel: t('CANCEL'),
+													title: t(`${pauseAction}_CAMPAIGN_CONFIRM_TITLE`, {
+														args: [title],
+													}),
+													text: t(`${pauseAction}_CAMPAIGN_CONFIRM_INFO`, {
+														args: [],
+													}),
+												}
+											)
+										)
+									}}
+									disabled={pauseSpinner || humanFriendlyName === 'Closed'}
+									endIcon={isPaused ? <StarSharp /> : <PauseSharp />}
+								>
+									{t(`BTN_${pauseAction}_CAMPAIGN`)}
+								</Button>
+								{pauseSpinner && (
+									<CircularProgress
+										size={24}
+										className={classes.buttonProgress}
+									/>
+								)}
+							</div>
+						</Grid>
+					</Grid>
+				)}
+			</Grid>
 
-							<Grid container spacing={2}>
-								<Grid item xs={12} sm={12} md={6}>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'humanFriendlyName'}
-											value={t(humanFriendlyName, { toUpperCase: true })}
-											label={t('status', { isProp: true })}
-											InputProps={{
-												endAdornment: (
-													<InputAdornment position='end'>
-														{mapStatusIcons(
-															humanFriendlyName,
-															status.name,
-															'md'
-														)}
-													</InputAdornment>
-												),
-											}}
-										/>
-									</Box>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'created'}
-											value={formatDateTime(item.created)}
-											label={t('created', { isProp: true })}
-										/>
-									</Box>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'activeFrom'}
-											value={formatDateTime(item.activeFrom)}
-											label={t('activeFrom', { isProp: true })}
-										/>
-									</Box>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'withdrawPeriodStart'}
-											value={formatDateTime(item.withdrawPeriodStart)}
-											label={t('withdrawPeriodStart', { isProp: true })}
-										/>
-									</Box>
-									{/* <Box my={1}>
+			<Grid item xs={12} sm={12} md={6} lg={7}>
+				<Box my={1}>
+					<ItemTitle title={title} errTitle={errTitle} {...hookProps} />
+				</Box>
+				<Box my={1}>
+					<ItemSpecProp
+						prop={'id'}
+						value={item.id}
+						label={t('id', { isProp: true })}
+					/>
+				</Box>
+
+				<Grid container spacing={2}>
+					<Grid item xs={12} sm={12} md={6}>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'humanFriendlyName'}
+								value={t(humanFriendlyName, { toUpperCase: true })}
+								label={t('status', { isProp: true })}
+								InputProps={{
+									endAdornment: (
+										<InputAdornment position='end'>
+											{mapStatusIcons(humanFriendlyName, status.name, 'md')}
+										</InputAdornment>
+									),
+								}}
+							/>
+						</Box>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'created'}
+								value={formatDateTime(item.created)}
+								label={t('created', { isProp: true })}
+							/>
+						</Box>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'activeFrom'}
+								value={formatDateTime(item.activeFrom)}
+								label={t('activeFrom', { isProp: true })}
+							/>
+						</Box>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'withdrawPeriodStart'}
+								value={formatDateTime(item.withdrawPeriodStart)}
+								label={t('withdrawPeriodStart', { isProp: true })}
+							/>
+						</Box>
+						{/* <Box my={1}>
 										<ItemSpecProp
 											prop={'CAMPAIGN_MIN_TARGETING'}
 											value={item.minTargetingScore > 0 ? t('YES') : t('NO')}
 											label={t('CAMPAIGN_MIN_TARGETING')}
 										/>
 									</Box> */}
-								</Grid>
-								<Grid item xs={12} sm={12} md={6}>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'fundsDistributedRatio'}
-											value={((status.fundsDistributedRatio || 0) / 10).toFixed(
-												2
-											)}
-											label={t('PROP_DISTRIBUTED', { args: ['%'] })}
-										/>
-									</Box>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'depositAmount'}
-											value={
-												formatTokenAmount(item.depositAmount, decimals) +
-												' ' +
-												symbol
-											}
-											label={t('depositAmount', { isProp: true })}
-										/>
-									</Box>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'CPM_MIN'}
-											value={
-												formatTokenAmount(
-													bigNumberify(cpmMin || 0).mul(1000),
-													decimals,
-													true
-												) +
-												' ' +
-												symbol
-											}
-											label={t('CPM_MIN')}
-										/>
-									</Box>
-									<Box my={1}>
-										<ItemSpecProp
-											prop={'CPM_MAX'}
-											value={
-												formatTokenAmount(
-													bigNumberify(cpmMax || 0).mul(1000),
-													decimals,
-													true
-												) +
-												' ' +
-												symbol
-											}
-											label={t('CPM_MAX')}
-										/>
-									</Box>
-								</Grid>
-							</Grid>
-						</Grid>
-						<Grid item xs={12} sm={12} md={12} lg={6}></Grid>
 					</Grid>
-				</Box>
-			</Paper>
-		</Fragment>
+					<Grid item xs={12} sm={12} md={6}>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'fundsDistributedRatio'}
+								value={((status.fundsDistributedRatio || 0) / 10).toFixed(2)}
+								label={t('PROP_DISTRIBUTED', { args: ['%'] })}
+							/>
+						</Box>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'depositAmount'}
+								value={
+									formatTokenAmount(item.depositAmount, decimals) + ' ' + symbol
+								}
+								label={t('depositAmount', { isProp: true })}
+							/>
+						</Box>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'CPM_MIN'}
+								value={
+									formatTokenAmount(
+										bigNumberify(cpmMin || 0).mul(1000),
+										decimals,
+										true
+									) +
+									' ' +
+									symbol
+								}
+								label={t('CPM_MIN')}
+							/>
+						</Box>
+						<Box my={1}>
+							<ItemSpecProp
+								prop={'CPM_MAX'}
+								value={
+									formatTokenAmount(
+										bigNumberify(cpmMax || 0).mul(1000),
+										decimals,
+										true
+									) +
+									' ' +
+									symbol
+								}
+								label={t('CPM_MAX')}
+							/>
+						</Box>
+					</Grid>
+				</Grid>
+			</Grid>
+			<Grid item xs={12} sm={12} md={12} lg={6}></Grid>
+		</Grid>
 	)
 }
