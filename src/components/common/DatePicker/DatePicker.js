207be86c4affc7e9c9035e07ede84d988b0e2f07
IvoPaunov <ivo.paunov@gmail.com>
2020-07-20 14:36:09 +0300

    update date picker btns order

--- src/components/common/DatePicker/DatePicker.js
+++ src/components/common/DatePicker/DatePicker.js
@@ -21,17 +21,17 @@ const CalendarIconAdor = ({
 	onLiveClick,
 }) => (
 	<InputAdornment position='end'>
+		{onNextClick && (
+			<IconButton color={iconColor} onClick={onNextClick} size='small'>
+				<NavigateNextRounded />
+			</IconButton>
+		)}
 		<IconButton color={iconColor} onClick={onIconClick} size='small'>
 			{icon}
 		</IconButton>
 		{onLiveClick && (
 			<IconButton color={iconColor} onClick={onLiveClick} size='small'>
 				<Update />
-			</IconButton>
-		)}
-		{onNextClick && (
-			<IconButton color={iconColor} onClick={onNextClick} size='small'>
-				<NavigateNextRounded />
 			</IconButton>
 		)}
 	</InputAdornment>
