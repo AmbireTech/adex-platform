55a9c91dcf351a528ad5540874f4c5dcc49b1ad6
IvoPaunov <ivo.paunov@gmail.com>
2020-08-14 16:08:26 +0300

    update cache buster check

--- src/components/App/CacheBuster.js
+++ src/components/App/CacheBuster.js
@@ -3,40 +3,26 @@ import { useSelector } from 'react-redux'
 import { selectLocation } from 'selectors'
 import packageJson from '../../../package.json'
 import { refreshCacheAndReload, execute, notifyNewTOS } from 'actions'
-global.meta = {
+
+const buildMeta = {
 	currentVersion: packageJson.version,
 	currentTOS: packageJson.tosVersion,
 }
 
 export default function CacheBuster(props) {
 	const location = useSelector(selectLocation) || {}
-	const semverGreaterThan = (versionA = '', versionB = '') => {
-		const versionsA = versionA.split(/\./g)
-
-		const versionsB = versionB.split(/\./g)
-		while (versionsA.length || versionsB.length) {
-			const a = Number(versionsA.shift())
-
-			const b = Number(versionsB.shift())
-			// eslint-disable-next-line no-continue
-			if (a === b) continue
-			// eslint-disable-next-line no-restricted-globals
-			return a > b || isNaN(b)
-		}
-		return false
-	}
 
 	useEffect(() => {
 		let r = Math.random()
 			.toString(36)
-			.substring(7)
+			.substring(2)
 		fetch(`/meta.json?r=${r}`, { cache: 'no-cache' })
 			.then(response => response.json())
 			.then(meta => {
 				const { latestVersion, latestTOS } = meta
-				const { currentVersion, currentTOS } = global.meta || {}
-				const shouldForceRefresh =
-					!global.meta || semverGreaterThan(latestVersion, currentVersion)
+				const { currentVersion, currentTOS } = buildMeta
+				const shouldForceRefresh = true || latestVersion !== currentVersion
+
 				if (shouldForceRefresh) {
 					execute(
 						refreshCacheAndReload({
@@ -44,8 +30,7 @@ export default function CacheBuster(props) {
 						})
 					)
 				}
-				const shouldNotifyNewTOS =
-					!global.meta || semverGreaterThan(latestTOS, currentTOS)
+				const shouldNotifyNewTOS = latestTOS !== currentTOS
 				if (shouldNotifyNewTOS) {
 					execute(notifyNewTOS())
 				}
