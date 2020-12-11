168c14237dd4166fb795e55d9c2ebce9c45e6de1
IvoPaunov <ivo.paunov@gmail.com>
2020-12-11 15:10:10 +0200

    add pulse animation to saveBtn

--- src/components/dashboard/containers/ItemCommon/SaveBtn.js
+++ src/components/dashboard/containers/ItemCommon/SaveBtn.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { CircularProgress, Fab } from '@material-ui/core'
 import { Check, Save } from '@material-ui/icons'
+import clsx from 'clsx'
 
 export const styles = theme => ({
 	root: {
@@ -25,6 +26,37 @@ export const styles = theme => ({
 		zIndex: 1,
 		color: theme.palette.grey.main,
 	},
+	'@keyframes pulse': {
+		'0%': {
+			opacity: 1,
+			transform: 'scale(1)',
+		},
+		'50%': {
+			opacity: 0,
+			transform: 'scale(1.420)',
+		},
+		'100%': {
+			opacity: 0,
+			transform: 'scale(1.420)',
+		},
+	},
+	pulse: {
+		overflow: 'visible',
+		position: 'relative',
+		'&::before': {
+			content: '""',
+			display: 'block',
+			position: 'absolute',
+			width: '100%',
+			height: '100%',
+			top: 0,
+			left: 0,
+			backgroundColor: 'inherit',
+			borderRadius: 'inherit',
+			transition: 'opacity .3s, transform .3s',
+			animation: '$pulse 1.69s cubic-bezier(0.23, 0, 0.420, 1) infinite',
+		},
+	},
 })
 
 const useStyles = makeStyles(styles)
@@ -39,14 +71,18 @@ export const SaveBtn = ({
 }) => {
 	const classes = useStyles()
 	const hasErrors = !!Object.keys(validations).length
+	const isDisabled = disabled || spinner || !dirtyProps.length || hasErrors
 	return (
 		!!dirtyProps.length && (
 			<div className={classes.wrapper}>
 				<Fab
+					className={clsx(classes.wrapper, {
+						[classes.pulse]: !isDisabled,
+					})}
 					size='medium'
 					color='secondary'
 					onClick={save}
-					disabled={disabled || spinner || !dirtyProps.length || hasErrors}
+					disabled={isDisabled}
 				>
 					{/*TODO: Success */}
 					{success ? <Check /> : <Save />}
