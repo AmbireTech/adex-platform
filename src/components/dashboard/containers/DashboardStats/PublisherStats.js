63b7d8cf535067242470b603faefa3641b3a81c3
IvoPaunov <ivo.paunov@gmail.com>
2021-01-15 11:30:40 +0200

    cleanup warnings

--- src/components/dashboard/containers/DashboardStats/PublisherStats.js
+++ src/components/dashboard/containers/DashboardStats/PublisherStats.js
@@ -21,7 +21,6 @@ import {
 	t,
 	selectPublisherStatsByCountryTableData,
 	selectPublisherStatsByCountryMapChartData,
-	selectBestEarnersTableData,
 	selectInitialDataLoadedByData,
 } from 'selectors'
 
