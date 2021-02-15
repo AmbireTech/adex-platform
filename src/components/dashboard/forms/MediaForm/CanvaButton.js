106143063137b12b29de50813dea752f4a2ce707
Rangel Stoilov <rangelstoilov@gmail.com>
2021-02-15 18:55:40 +0200

    fix canva issue

--- src/components/dashboard/forms/MediaForm/CanvaButton.js
+++ src/components/dashboard/forms/MediaForm/CanvaButton.js
@@ -2,37 +2,59 @@ import React, { useState, useEffect } from 'react'
 import './CanvaButton.css'
 
 function CanvaButton() {
-	const [loadedCanva, setLoadedCanva] = useState(false)
 	const [canvaApi, setCanvaApi] = useState(null)
 
 	useEffect(() => {
+		var script
 		function initCanva(document, url) {
-			var script = document.createElement('script')
+			script = document.createElement('script')
 			script.src = url
 			script.onload = function() {
 				if (window.Canva && window.Canva.DesignButton) {
 					window.Canva.DesignButton.initialize({
 						apiKey: 'hJWa9jV3224Iq2B54I5RuB3d',
-					}).then(api => setCanvaApi(api))
+					}).then(api => {
+						setCanvaApi(api)
+						window.canvaApi = api
+					})
 				}
 			}
 			document.body.appendChild(script)
 		}
-		initCanva(document, 'https://sdk.canva.com/designbutton/v2/api.js')
+		if (!window.canvaApi) {
+			initCanva(document, 'https://sdk.canva.com/designbutton/v2/api.js')
+		} else {
+			setCanvaApi(window.canvaApi)
+		}
 	}, [])
 
 	const handleCreateCanvaAd = () => {
 		if (canvaApi) {
-			console.log()
+			canvaApi.createDesign({
+				design: {
+					type: 'Poster',
+				},
+				onDesignOpen: ({ designId }) => {
+					// Triggered when editor finishes loading and opens a new design.
+					// You can save designId for future use.
+				},
+				onDesignPublish: ({ exportUrl, designId }) => {
+					// Triggered when design is published to an image.
+					// Save the image to your server as the exportUrl will expire shortly.
+				},
+				onDesignClose: () => {
+					// Triggered when editor is closed.
+				},
+			})
 		}
 	}
 
 	return (
 		<span
-			class='canva-btn canva-btn-theme-default canva-btn-size-m'
+			className='canva-btn canva-btn-theme-default canva-btn-size-m'
 			onClick={() => handleCreateCanvaAd()}
 		>
-			<span class='canva-btn-i'></span>
+			<span className='canva-btn-i'></span>
 			Design on Canva
 		</span>
 	)
