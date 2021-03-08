f591a06e6d440e37f7eac05e4051b21a21a59566
IvoPaunov <ivo.paunov@gmail.com>
2021-03-08 19:05:26 +0200

    add quick project selectors

--- src/selectors/projectSelectors.js
+++ src/selectors/projectSelectors.js
@@ -1,8 +1,19 @@
 import { createSelector } from 'reselect'
+import { PROJECTS } from 'constants/global'
 
 export const getProject = state => state.persist.project
 
 export const selectProject = createSelector(
 	[getProject],
 	project => project
+)
+
+export const selectIsPlatform = createSelector(
+	[selectProject],
+	project => project === PROJECTS.platform
+)
+
+export const selectIsWallet = createSelector(
+	[selectProject],
+	project => project === PROJECTS.wallet
 )
