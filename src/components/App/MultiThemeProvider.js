f013cfe25e603b588083f12fd70ba14aa6448a55
IvoPaunov <ivo.paunov@gmail.com>
2021-04-09 17:56:31 +0300

    config wallet theme

--- src/components/App/MultiThemeProvider.js
+++ src/components/App/MultiThemeProvider.js
@@ -1,37 +1,51 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { darkTheme, lightTheme } from './themeMUi'
+import {
+	darkTheme as darkThemeWallet,
+	lightTheme as lightThemeWallet,
+} from './themeWallet'
 import {
 	loadFromLocalStorage,
 	saveToLocalStorage,
 } from 'helpers/localStorageHelpers'
+import { selectProject } from 'selectors'
+import { PROJECTS } from 'constants/global'
 
 const THEMES = {
-	light: lightTheme,
-	dark: darkTheme,
+	[PROJECTS.platform]: {
+		light: lightTheme,
+		dark: darkTheme,
+	},
+	[PROJECTS.wallet]: {
+		light: lightThemeWallet,
+		dark: darkThemeWallet,
+	},
 }
 
 export const MultiThemeContext = React.createContext()
 
 const MultiThemeProvider = ({ children }) => {
+	const project = useSelector(selectProject)
 	const [themeType, setThemeType] = useState('light')
 	const [theme, setTheme] = useState(darkTheme)
 
 	useEffect(() => {
 		const lastTheme = loadFromLocalStorage('themeType') || 'light'
 		setThemeType(lastTheme)
-		setTheme(THEMES[lastTheme])
-	}, [])
+		setTheme(THEMES[project][lastTheme])
+	}, [project])
 
 	const switchTheme = () => {
 		if (themeType === 'light') {
 			saveToLocalStorage('dark', 'themeType')
 			setThemeType('dark')
-			setTheme(THEMES['dark'])
+			setTheme(THEMES[project]['dark'])
 		} else {
 			saveToLocalStorage('light', 'themeType')
 			setThemeType('light')
-			setTheme(THEMES['light'])
+			setTheme(THEMES[project]['light'])
 		}
 	}
 
