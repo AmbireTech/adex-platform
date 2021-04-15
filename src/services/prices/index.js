22a46837763e8966b6196be204fa54c220bf9dec
IvoPaunov <ivo.paunov@gmail.com>
2021-04-15 17:59:20 +0300

    get multiple prices

--- src/services/prices/index.js
+++ src/services/prices/index.js
@@ -1,18 +1,40 @@
-export const PRICES_API_URL = `https://api.coingecko.com/api/v3/simple/price?ids=ADEX&vs_currencies=usd`
+export const PRICES_API_URL = `https://api.coingecko.com/api/v3/simple/price?ids=ADEX&vs_currencies=usd,eur,dai`
+
+const toStatsValue = data => {
+	return Object.entries(data).reduce((value, [key, prices]) => {
+		const mapped = { ...value }
+		let statsKey = null
+		switch (key) {
+			case 'adex':
+				statsKey = 'ADX'
+				break
+			default:
+				statsKey = key.toUpperCase()
+				break
+		}
+
+		const statsPrices = Object.entries(prices).reduce(
+			(updated, [priceKey, priceVal]) => {
+				updated[priceKey.toUpperCase()] = priceVal
+				return updated
+			},
+			{}
+		)
+
+		mapped[statsKey] = statsPrices
+
+		return mapped
+	}, {})
+}
 
 export async function getPrices() {
 	try {
 		const res = await fetch(PRICES_API_URL)
 		const data = await res.json()
-
 		if (!data.adex) {
 			throw new Error('errors.gettingPrices')
 		} else {
-			return {
-				ADX: {
-					USD: data.adex.usd,
-				},
-			}
+			return toStatsValue(data)
 		}
 	} catch (err) {
 		console.error(err)
