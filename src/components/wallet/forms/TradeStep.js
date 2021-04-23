2dc5b105d18038ae999ae8aa6cf13f75e33926f0
IvoPaunov <ivo.paunov@gmail.com>
2021-04-23 18:24:54 +0300

    add buy esitmated amount to trade step

--- src/components/wallet/forms/TradeStep.js
+++ src/components/wallet/forms/TradeStep.js
@@ -2,7 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import { BigNumber } from 'ethers'
-import { TextField, Button, Box, Grid, Paper } from '@material-ui/core'
+import {
+	TextField,
+	Button,
+	Box,
+	Grid,
+	Paper,
+	InputAdornment,
+} from '@material-ui/core'
 import { AmountWithCurrency } from 'components/common/amount'
 // import { InputLoading } from 'components/common/spinners/'
 import {
@@ -34,7 +41,41 @@ const getMainCurrencyValue = ({ asset, floatAmount, prices, mainCurrency }) => {
 	return value.toFixed(2)
 }
 
+const conversionRate = ({ formAsset, toAsset, prices, mainCurrency }) => {
+	const fromPrice = parseFloat((prices[formAsset] || {})[mainCurrency] || 0)
+	const toPrice = parseFloat((prices[toAsset] || {})[mainCurrency] || 0)
+
+	if (!toPrice) {
+		return null
+	}
+
+	return fromPrice / toPrice
+}
+
+const estimatedConversionValue = ({
+	formAsset,
+	toAsset,
+	formAssetAmount,
+	prices,
+	mainCurrency,
+}) => {
+	const rate = conversionRate({
+		formAsset,
+		toAsset,
+		prices,
+		mainCurrency,
+	})
+
+	const value =
+		rate && !!parseFloat(formAssetAmount)
+			? parseFloat(formAssetAmount) * rate
+			: null
+
+	return value
+}
+
 const WalletTradeStep = ({ stepsId, validateId } = {}) => {
+	// NOTE: RAW DATA - BNs - format in fields
 	const { assetsData = {} } = useSelector(selectAccountStatsRaw)
 	const prices = useSelector(selectBaseAssetsPrices)
 	const assetsFromSource = useSelector(selectTradableAssetsFromSources)
@@ -47,7 +88,8 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 		toAsset = assetsFromSource[1].value,
 	} = useSelector(state => selectNewTransactionById(state, stepsId))
 
-	const selectedFromAsset = assetsData[formAsset]
+	const selectedFromAsset = assetsData[formAsset] || {}
+	const selectedToAsset = assetsData[toAsset] || {}
 	const selectedFormAssetMainCurrencyValue = getMainCurrencyValue({
 		asset: selectedFromAsset.symbol,
 		floatAmount: formAssetAmount,
@@ -63,6 +105,23 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 	const syncSpinner = useSelector(state =>
 		selectWeb3SyncSpinnerByValidateId(state, validateId)
 	)
+
+	const toAssetAmount = estimatedConversionValue({
+		formAssetAmount,
+		formAsset: selectedFromAsset.symbol,
+		toAsset: selectedToAsset.symbol,
+		prices,
+		mainCurrency: mainCurrency.id,
+	})
+
+	const selectedToAssetMainCurrencyValue = toAssetAmount
+		? getMainCurrencyValue({
+				asset: selectedToAsset.symbol,
+				floatAmount: toAssetAmount,
+				prices,
+				mainCurrency: mainCurrency.id,
+		  })
+		: null
 
 	const {
 		formAssetAmount: errFormAssetAmount,
@@ -94,16 +153,16 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 				></FullContentMessage>
 			) : (
 				<ContentBody>
-					<Paper>
+					<Paper elevation={5}>
 						<Box p={1}>
 							<Grid container spacing={2}>
 								<Grid item xs={12}>
 									From
 								</Grid>
-								<Grid item xs={12} md={6}>
+								<Grid item xs={12} sm={3}>
 									<Dropdown
 										fullWidth
-										variant='standard'
+										variant='outlined'
 										required
 										onChange={value => {
 											execute(
@@ -111,6 +170,13 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 													tx: stepsId,
 													key: 'formAsset',
 													value,
+												})
+											)
+											execute(
+												updateNewTransaction({
+													tx: stepsId,
+													key: 'formAssetAmount',
+													value: '0',
 												})
 											)
 										}}
@@ -127,24 +193,31 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 										}
 									/>
 								</Grid>
-								<Grid item xs={12} md={6}>
+								<Grid item xs={12} sm={9}>
 									<Box>
 										<Box>
 											<TextField
 												// disabled={spinner}
+												variant='outlined'
 												type='text'
 												fullWidth
 												required
+												InputProps={{
+													startAdornment: (
+														<InputAdornment position='start'>
+															{selectedFromAsset.symbol}
+														</InputAdornment>
+													),
+												}}
 												label={
 													<Box display='inline'>
 														{t('TRADE_FROM_ASSET_AMOUNT_LABEL')} (
-														{t('AVAILABLE')}
+														{t('AVAILABLE')}{' '}
 														<AmountWithCurrency
-															amount={
-																selectedFromAsset.assetToMainCurrenciesValues[
-																	mainCurrency.id
-																]
-															}
+															amount={formatTokenAmount(
+																fromAssetUserBalance,
+																selectedToAsset.decimals
+															)}
 															unit={mainCurrency.symbol}
 															unitPlace='left'
 															fontSize={14}
@@ -153,26 +226,7 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 													</Box>
 												}
 												name='amountToWithdraw'
-												value={
-													`${formAssetAmount} ${selectedFromAsset.symbol} (${mainCurrency.symbol} ${selectedFormAssetMainCurrencyValue})`
-
-													// <Box display='inline'>
-													// 	<AmountWithCurrency
-													// 		amount={formAssetAmount}
-													// 		unit={selectedFromAsset.symbol}
-													// 		unitPlace='right'
-													// 		fontSize={14}
-													// 	/>
-													// 	(
-													// 	<AmountWithCurrency
-													// 		amount={selectedFormAssetMainCurrencyValue}
-													// 		unit={mainCurrency.symbol}
-													// 		unitPlace='left'
-													// 		fontSize={14}
-													// 	/>
-													// 	)
-													// </Box>
-												}
+												value={`${formAssetAmount}`}
 												onChange={ev =>
 													execute(
 														updateNewTransaction({
@@ -184,13 +238,23 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 												}
 												error={errFormAssetAmount && !!errFormAssetAmount.dirty}
 												helperText={
-													errFormAssetAmount && !!errFormAssetAmount.dirty
-														? errFormAssetAmount.errMsg
-														: ''
+													errFormAssetAmount && !!errFormAssetAmount.dirty ? (
+														errFormAssetAmount.errMsg
+													) : (
+														<AmountWithCurrency
+															amount={selectedFormAssetMainCurrencyValue}
+															unit={mainCurrency.symbol}
+															unitPlace='left'
+															fontSize={14}
+														/>
+													)
 												}
 											/>
 											<Box>
 												<Button
+													variant='contained'
+													size='small'
+													color='default'
 													disabled={!selectedFromAsset}
 													onClick={() => setTradePercent(25)}
 												>
@@ -221,17 +285,17 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 							</Grid>
 						</Box>
 					</Paper>
-					<Box my={2}></Box>
-					<Paper>
+					<Box my={4}></Box>
+					<Paper elevation={5}>
 						<Box p={1}>
 							<Grid container spacing={2}>
 								<Grid item xs={12}>
 									To
 								</Grid>
-								<Grid item xs={12} md={6}>
+								<Grid item xs={12} sm={3}>
 									<Dropdown
 										fullWidth
-										variant='standard'
+										variant='outlined'
 										required
 										onChange={value =>
 											execute(
@@ -252,6 +316,37 @@ const WalletTradeStep = ({ stepsId, validateId } = {}) => {
 											errToAsset && !!errToAsset.dirty
 												? errToAsset.errMsg
 												: t('WALLET_TRADE_FROM_ASSET')
+										}
+									/>
+								</Grid>
+								<Grid item xs={12} sm={9}>
+									<TextField
+										// disabled={spinner}
+										variant='outlined'
+										type='text'
+										fullWidth
+										disabled
+										InputProps={{
+											startAdornment: (
+												<InputAdornment position='start'>
+													{selectedToAsset.symbol}
+												</InputAdornment>
+											),
+										}}
+										label={
+											<Box display='inline'>
+												{t('TRADE_TO_ASSET_ESTIMATED_AMOUNT_LABEL')}}
+											</Box>
+										}
+										name='amountToWithdraw'
+										value={`${toAssetAmount || 'N/A'}`}
+										helperText={
+											<AmountWithCurrency
+												amount={selectedToAssetMainCurrencyValue}
+												unit={mainCurrency.symbol}
+												unitPlace='left'
+												fontSize={14}
+											/>
 										}
 									/>
 								</Grid>
