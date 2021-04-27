0e319b7ba32d76ce645158c998372022590e74ee
IvoPaunov <ivo.paunov@gmail.com>
2021-04-27 15:43:01 +0300

    init wallet validations actions

---
+++ src/actions/walletValidationsActions.js
@@ -0,0 +1,69 @@
+import { BigNumber } from 'ethers'
+import {
+	selectAccountStatsRaw,
+	selectAccountStatsFormatted,
+	selectMainToken,
+} from 'selectors'
+import { validate } from 'actions'
+import { formatTokenAmount } from 'helpers/formatters'
+
+export function validateWalletFees({
+	validateId,
+	feesAmountBN,
+	feeAsset,
+	amountToSpendBN,
+	spendAsset,
+	errorMsg = '',
+	dirty,
+}) {
+	return async function(dispatch, getState) {
+		let isValid = true
+		let msg = errorMsg
+		let args = []
+		const state = getState()
+
+		const { assetsData = {} } = selectAccountStatsRaw(state)
+		const {
+			assetsData: assetsDataFormatted = {},
+		} = selectAccountStatsFormatted(state)
+
+		const feeAssetAsSpendAsset = spendAsset === feeAsset
+
+		const feeAssetData = feeAssetAsSpendAsset
+			? assetsData[spendAsset]
+			: assetsData[feeAsset]
+
+		const availableBalanceFeeAsset = feeAssetData.balance
+
+		const amountNeeded = BigNumber.from(feesAmountBN).add(
+			BigNumber.from(feeAssetAsSpendAsset ? amountToSpendBN : 0)
+		)
+
+		const { symbol, decimals } = selectMainToken(state)
+
+		if (amountNeeded.gt(BigNumber.from(availableBalanceFeeAsset))) {
+			const amountNeededFormatted = formatTokenAmount(
+				amountNeeded,
+				decimals,
+				null,
+				2
+			)
+			isValid = false
+			msg = 'ERR_TX_INSUFFICIENT_BALANCE'
+			args = [
+				amountNeededFormatted,
+				symbol,
+				assetsDataFormatted[feeAsset].balance,
+				symbol,
+			]
+		}
+
+		await validate(validateId, 'fees', {
+			isValid,
+			err: { msg, args },
+			dirty,
+		})(dispatch)
+
+		return isValid
+	}
+}
