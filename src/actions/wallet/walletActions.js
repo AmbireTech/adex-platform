532b03788c065a4af7dfff734fe85a3626bb7118
IvoPaunov <ivo.paunov@gmail.com>
2021-04-27 15:44:13 +0300

    add handle wallet fees data action

--- src/actions/wallet/walletActions.js
+++ src/actions/wallet/walletActions.js
@@ -1,11 +1,14 @@
 // import * as types from 'constants/actionTypes'
+import { getErrorMsg } from 'helpers/errors'
 import {
 	updateSpinner,
 	handleAfterValidation,
 	validateNumberString,
 	beforeWeb3,
 	addToast,
-	handleFeesData,
+	updateNewTransaction,
+	validateWalletFees,
+	validate,
 } from 'actions'
 import {
 	selectNewTransactionById,
@@ -13,6 +16,47 @@ import {
 	t,
 } from 'selectors'
 import { BigNumber } from 'ethers'
+
+export function handleWalletFeesData({
+	stepsId,
+	validateId,
+	dirty,
+	actionName,
+	feeDataAction,
+}) {
+	return async function(dispatch, getState) {
+		let isValid = false
+		try {
+			const feesData = await feeDataAction()
+
+			await updateNewTransaction({
+				tx: stepsId,
+				key: 'feesData',
+				value: feesData,
+			})(dispatch, getState)
+
+			isValid = await validateWalletFees({
+				validateId,
+				feesAmountBN: feesData.feesAmountBN,
+				feeAsset: feesData.feeAsset,
+				spendAsset: feesData.spendAsset,
+				amountToSpendBN: feesData.actualWithdrawAmount || '0',
+				dirty,
+			})(dispatch, getState)
+		} catch (err) {
+			console.error(actionName, err)
+
+			isValid = false
+			await validate(validateId, 'fees', {
+				isValid,
+				err: { msg: getErrorMsg(err) },
+				dirty,
+			})(dispatch, getState)
+		}
+
+		return isValid
+	}
+}
 
 export function validateWalletTrade({
 	stepsId,
@@ -29,9 +73,8 @@ export function validateWalletTrade({
 		const state = getState()
 		// const account = selectAccount(state)
 		const {
-			// formAsset,
+			formAsset,
 			formAssetAmount,
-			toAsset,
 			// toAssetAmount,
 		} = selectNewTransactionById(state, stepsId)
 
@@ -44,27 +87,24 @@ export function validateWalletTrade({
 				value: formAssetAmount,
 				dirty,
 			})(dispatch),
-			validateNumberString({
-				validateId,
-				prop: 'toAsset',
-				value: toAsset,
-				dirty,
-			})(dispatch),
 		])
 
 		let isValid = inputValidations.every(v => v === true)
 
 		if (isValid) {
-			isValid = await handleFeesData({
+			isValid = await handleWalletFeesData({
 				stepsId,
 				validateId,
 				dirty,
 				actionName: 'walletTrade',
-				feeDataAction: () => {
+				feeDataAction: async () => {
 					// TODO
 					return {
 						total: '1',
-						totalBN: BigNumber.from(1),
+						feesAmountBN: BigNumber.from(1),
+						feeAsset: formAsset,
+						spendAsset: formAsset,
+						amountToSpendBN: formAssetAmount,
 						breakdownFormatted: {
 							feeAmount: '1',
 						},
