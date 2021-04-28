cf320811906bc51da9d437f55e7dc2b50383d423
IvoPaunov <ivo.paunov@gmail.com>
2021-04-28 18:02:45 +0300

    update wallet stats cards

--- src/components/wallet/WalletInfoCard.js
+++ src/components/wallet/WalletInfoCard.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Box, Paper } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import { Box, Paper, Typography } from '@material-ui/core'
 
 export const InfoCard = props => {
 	const { title, titleIcon, children } = props
@@ -11,7 +9,7 @@ export const InfoCard = props => {
 			<Box p={3} height={1}>
 				<Box mb={2}>
 					{titleIcon}
-					{title}
+					<Typography variant='h5'>{title}</Typography>
 				</Box>
 				<Box>{children}</Box>
 			</Box>
