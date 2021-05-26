d4a9027bee443f83d8ebbddc1a51aa631eb72e34
IvoPaunov <ivo.paunov@gmail.com>
2021-05-26 19:53:40 +0300

    fix assets selector

--- src/selectors/wallet/assetsSelectors.js
+++ src/selectors/wallet/assetsSelectors.js
@@ -4,7 +4,7 @@ import { assets } from 'services/adex-wallet'
 
 export const selectTradableAssetsFromSources = createSelector(
 	[selectAccountStatsFormatted],
-	({ assetsData }) => {
+	({ assetsData = {} }) => {
 		return Object.values(assetsData).map(x => ({
 			value: x.address,
 			label: `${x.symbol}`,
@@ -15,7 +15,7 @@ export const selectTradableAssetsFromSources = createSelector(
 
 export const selectTradableAssetsToSources = createSelector(
 	[selectAccountStatsFormatted], // TODO: selected from
-	({ assetsData }) => {
+	({ assetsData } = {}) => {
 		return Object.entries(assets)
 			.filter(x => x[1].isBaseAsset)
 			.map(([addr, x]) => ({
