7239ed738ff1b06bd3b695367c04406ec5ba9d60
IvoPaunov <ivo.paunov@gmail.com>
2021-06-17 19:04:36 +0300

    fix some svg icons

--- src/components/common/icons/WalletDashboard.js
+++ src/components/common/icons/WalletDashboard.js
@@ -4,7 +4,6 @@ import createSvgIconCustom from './createSvgIconCustom'
 export default createSvgIconCustom(
 	<g transform='translate(-21 -21)'>
 		<path
-			class='a'
 			d='M45,23.9A2.9,2.9,0,0,0,42.1,21H23.9A2.9,2.9,0,0,0,21,23.9V42.1A2.9,2.9,0,0,0,23.9,45H42.1A2.9,2.9,0,0,0,45,42.1Zm-2.483,0V35.069H34.241V23.483H42.1A.414.414,0,0,1,42.517,23.9ZM23.9,23.483h7.862v4.966H23.483V23.9A.414.414,0,0,1,23.9,23.483ZM23.483,42.1V30.931h8.276V42.517H23.9A.414.414,0,0,1,23.483,42.1Zm18.621.414H34.241V37.552h8.276V42.1A.414.414,0,0,1,42.1,42.517Z'
 			transform='translate(0 0)'
 		/>
