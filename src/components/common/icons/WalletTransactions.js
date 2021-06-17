23611ab99a22772b5701d43b8f3dde8b2f117eee
IvoPaunov <ivo.paunov@gmail.com>
2021-06-17 19:04:36 +0300

    fix some svg icons

--- src/components/common/icons/WalletTransactions.js
+++ src/components/common/icons/WalletTransactions.js
@@ -2,15 +2,13 @@ import React from 'react'
 import createSvgIconCustom from './createSvgIconCustom'
 
 export default createSvgIconCustom(
-	<g class='a' transform='translate(-0.501 -3.486)'>
+	<g transform='translate(-0.501 -3.486)'>
 		<g transform='translate(1.003 4.002)'>
 			<path
-				class='b'
 				d='M26.693,19.361a.84.84,0,0,0-1.168-.21l-7.082,4.957V21.52A2.52,2.52,0,0,0,15.922,19H5.84a.84.84,0,0,0,0,1.68H15.922a.84.84,0,0,1,.84.84v4.2a.84.84,0,0,0,1.319.689l8.4-5.881A.84.84,0,0,0,26.693,19.361Z'
 				transform='translate(-1.642 -6.399)'
 			/>
 			<path
-				class='b'
 				d='M9.4,9.041a2.52,2.52,0,0,0,2.52,2.52H22a.84.84,0,1,0,0-1.68H11.922a.84.84,0,0,1-.84-.84V4.84a.84.84,0,0,0-1.319-.689l-8.4,5.881a.841.841,0,0,0,.966,1.378L9.4,6.453Z'
 				transform='translate(-1.003 -4.002)'
 			/>
