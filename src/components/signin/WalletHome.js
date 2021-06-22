5cd6c16670cc7cee2abacba3958802c482eaea8b
IvoPaunov <ivo.paunov@gmail.com>
2021-06-22 15:47:30 +0300

    update wallet login home

--- src/components/signin/WalletHome.js
+++ src/components/signin/WalletHome.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
-// import Logo from 'components/common/icons/AdexIconTxt'
-// import LogoDark from 'components/common/icons/AdexIconTxtDark'
+import Logo from 'components/common/icons/AdexIconTxt'
+import LogoDark from 'components/common/icons/AdexWalletTxtDark'
 // import Typography from '@material-ui/core/Typography'
 // import Anchor from 'components/common/anchor/anchor'
-import walletVideoBkgSrc from 'resources/wallet/wallet-login-video-background.mp4'
+import walletVideoBkgSrc from 'resources/wallet/wallet-login-video-background-2.mp4'
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -33,7 +33,7 @@ const useStyles = makeStyles(theme => ({
 			right: 0,
 			bottom: 0,
 			background: 'black',
-			opacity: 0.42,
+			opacity: 0.46,
 		},
 	},
 	videoBackground: {
@@ -43,15 +43,15 @@ const useStyles = makeStyles(theme => ({
 		height: '100%',
 		top: 0,
 		left: 0,
-		filter: 'blur(4px)',
+		// filter: 'blur(4px)',
 	},
 }))
 
 export default function Home({ t, ...rest }) {
 	const classes = useStyles()
-	// const theme = useTheme()
+	const theme = useTheme()
 
-	// const AdxIcon = theme.type === 'dark' ? LogoDark : Logo
+	const AdxIcon = theme.type === 'dark' ? LogoDark : Logo
 
 	return (
 		<Box
@@ -64,7 +64,13 @@ export default function Home({ t, ...rest }) {
 			justifyContent='center'
 		>
 			<Box className={classes.videoOverlay}>
-				<video className={classes.videoBackground} autoPlay muted loop>
+				<video
+					className={classes.videoBackground}
+					autoPlay
+					muted
+					loop
+					playbackRate='2'
+				>
 					<source src={walletVideoBkgSrc} type='video/mp4' />
 				</video>
 			</Box>
@@ -75,8 +81,10 @@ export default function Home({ t, ...rest }) {
 				alignItems='center'
 				justifyContent='space-evenly'
 				flexWrap='wrap'
+				px={4}
 			>
-				{'WALLET HERE'}
+				<AdxIcon className={classes.logo} />
+				{/* {'WALLET HERE'} */}
 			</Box>
 		</Box>
 	)
