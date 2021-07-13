b79f46f5968af46bfc349e513d5f355d30b0b47f
IvoPaunov <ivo.paunov@gmail.com>
2021-07-13 17:53:23 +0300

    work on wallet assets data

--- src/components/wallet/WalletAssetsTable.js
+++ src/components/wallet/WalletAssetsTable.js
@@ -1,8 +1,13 @@
 import React, { useState, useCallback, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { ExpandButton, TableBodyRow } from 'mui-datatables'
+import { TreeView, TreeItem } from '@material-ui/lab'
+import { ExpandMore, ChevronRight } from '@material-ui/icons'
 import MUIDataTableEnhanced from 'components/dashboard/containers/Tables/MUIDataTableEnhanced'
-import { Box, Avatar } from '@material-ui/core'
+import { Box, Avatar, TableRow, TableCell } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { AmountWithCurrency } from 'components/common/amount'
+
 import clsx from 'clsx'
 
 import {
@@ -14,40 +19,45 @@ import { useTableData } from 'components/dashboard/containers/Tables/tableHooks'
 
 const styles = theme => {
 	return {
-		row: {
-			'& td': {
-				borderBottom: 0,
-			},
-		},
 		OddRow: {
 			'& td': {
 				backgroundColor: theme.palette.background.default,
 			},
+		},
+		logo: {
+			marginRight: theme.spacing(1),
 		},
 	}
 }
 
 const useStyles = makeStyles(styles)
 
-const getCols = ({ symbol }) => [
-	{
-		name: 'logo',
-		// label: t('logo'),
-		options: {
-			filter: false,
-			sort: false,
-			customBodyRender: ({ logoSrc, name }) => (
-				<Avatar src={logoSrc} alt={name} />
-			),
-		},
-	},
+const getCols = ({ classes }) => [
+	// {
+	// 	name: 'logo',
+	// 	// label: t('logo'),
+	// 	options: {
+	// 		filter: false,
+	// 		sort: false,
+	// 		customBodyRender: (value) => (
+	// 			<Avatar src={logoSrc} alt={name} />
+	// 		),
+	// 	},
+	// },
 	{
 		name: 'name',
 		label: t('PROP_ASSET'),
 		options: {
 			filter: false,
 			sort: true,
-			customBodyRender: (name = '') => name,
+			customBodyRender: (value = []) => {
+				return (
+					<Box display='flex' flexDirection='row' alignItems='center'>
+						<Avatar src={value[1]} alt={value[0]} className={classes.logo} />
+						{value[0]}
+					</Box>
+				)
+			},
 		},
 	},
 	{
@@ -59,14 +69,88 @@ const getCols = ({ symbol }) => [
 			customBodyRender: balance => balance,
 		},
 	},
-	// {
-	// 	name: 'amountMainCurrency',
-	// 	label: t('PROP_AMOUNT_MAIN_CURRENCY'),
-	// 	options: {
-	// 		filter: true,
-	// 		sort: true,
-	// 	},
-	// },
+	{
+		name: 'specific',
+		label: t('specific'),
+		options: {
+			// filter: true,
+			// sort: true,
+			customBodyRender: x => {
+				return (
+					<TreeView
+						defaultCollapseIcon={<ExpandMore />}
+						defaultExpandIcon={<ChevronRight />}
+					>
+						<TreeItem
+							key={x.address}
+							nodeId={x.address + '-' + 1}
+							label={
+								<Box>
+									<AmountWithCurrency
+										amount={x.total}
+										unit={x.symbol}
+										mainFontVariant='h6'
+										decimalsFontVariant='subtitle1'
+									/>
+									{' ('}
+									<AmountWithCurrency
+										amount={
+											0
+											// (x.assetTotalToMainCurrenciesValues || {})[
+											// 	mainCurrency.id
+											// ]
+										}
+										unit={'$'}
+										unitPlace='left'
+										mainFontVariant='body1'
+										decimalsFontVariant='caption'
+									/>
+									{')'}
+								</Box>
+							}
+						>
+							<TreeItem
+								key={x.address}
+								nodeId={x.address}
+								label={
+									<AmountWithCurrency
+										amount={x.balance}
+										unit={x.symbol}
+										mainFontVariant='h6'
+										decimalsFontVariant='subtitle1'
+									/>
+								}
+							/>
+							{(x.specific || []).map((y, j) => (
+								<TreeItem
+									key={y.address + '-' + j}
+									nodeId={`${1}-${j}`}
+									label={
+										<Box>
+											<AmountWithCurrency
+												amount={y.balance}
+												unit={y.symbol}
+												mainFontVariant='h6'
+												decimalsFontVariant='subtitle1'
+											/>
+											{' ('}
+											<AmountWithCurrency
+												amount={y.baseTokenBalance[1]}
+												unit={y.baseTokenBalance[0] || x.symbol}
+												mainFontVariant='body1'
+												decimalsFontVariant='caption'
+											/>
+											{')'}
+										</Box>
+									}
+								/>
+							))}
+						</TreeItem>
+					</TreeView>
+				)
+			},
+		},
+	},
 	{
 		name: 'share',
 		label: t('PROP_SHARE'),
@@ -95,12 +179,12 @@ const getOptions = ({ classes }) => ({
 		direction: 'desc',
 	},
 	selectableRows: 'none',
-	setRowProps: (row, _dataIndex, rowIndex) => {
+	setRowProps: (_row, _dataIndex, rowIndex) => {
 		return {
-			className: clsx(row, {
+			className: clsx(classes.row, {
 				[classes.OddRow]: rowIndex % 2 === 0,
 			}),
-			style: { border: 0 },
+			style: { border: 0, fontSize: '2rem' },
 		}
 	},
 })
@@ -114,9 +198,9 @@ function WalletAssetsTable(props) {
 	const getColumns = useCallback(
 		() =>
 			getCols({
-				// symbol,
+				classes,
 			}),
-		[]
+		[classes]
 	)
 
 	const { data, columns } = useTableData({
