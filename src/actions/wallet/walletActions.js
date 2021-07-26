dd61f838ba0d1771c74ae211cf2bf60569c78586
IvoPaunov <ivo.paunov@gmail.com>
2021-07-26 16:44:56 +0300

    add privileges change to wallet actions

--- src/actions/wallet/walletActions.js
+++ src/actions/wallet/walletActions.js
@@ -11,11 +11,14 @@ import {
 	validate,
 	validateWalletDiversificationAssets,
 	validateEthAddress,
+	validatePrivLevel,
+	validatePrivilegesAddress,
 } from 'actions'
 import {
 	selectNewTransactionById,
 	selectAccount,
 	selectAuthType,
+	selectWalletAddress,
 	t,
 	selectAccountStatsRaw,
 } from 'selectors'
@@ -24,6 +27,7 @@ import {
 	getTradeOutData,
 	walletDiversificationTransaction,
 	walletWithdrawTransaction,
+	walletSetIdentityPrivilege,
 } from 'services/smart-contracts/actions/wallet'
 
 function checkStepId({ stepsId, functionName }) {
@@ -486,6 +490,119 @@ export function walletWithdraw({
 					args: [err.message],
 				}),
 				timeout: 5000,
+			})(dispatch)
+		}
+	}
+}
+
+export function walletValidatePrivilegesChange({
+	stepsId,
+	validateId,
+	txId,
+	dirty,
+	onValid,
+	onInvalid,
+}) {
+	return async function(dispatch, getState) {
+		await updateSpinner(validateId, true)(dispatch)
+		if (!dirty) {
+			await beforeWeb3(validateId)(dispatch, getState)
+		}
+		const state = getState()
+		const { setAddr, warningAccepted, privLevel } = selectNewTransactionById(
+			state,
+			stepsId
+		)
+		const walletAddr = selectWalletAddress(state)
+		const authType = selectAuthType(state)
+
+		const inputValidations = await Promise.all([
+			validateEthAddress({
+				validateId,
+				addr: setAddr,
+				prop: 'setAddr',
+				nonERC20: true,
+				nonZeroAddr: true,
+				authType,
+				dirty,
+				quickCheck: !dirty,
+			})(dispatch),
+			validatePrivLevel({
+				validateId,
+				privLevel,
+				dirty,
+			})(dispatch),
+		])
+
+		let isValid = inputValidations.every(v => v === true)
+
+		if (isValid) {
+			const validation = await validatePrivilegesAddress({
+				validateId,
+				setAddr,
+				walletAddr,
+				warningAccepted,
+				privLevel,
+				dirty,
+				authType,
+			})(dispatch)
+
+			isValid = validation.isValid
+
+			await updateNewTransaction({
+				tx: stepsId,
+				key: 'warningMsg',
+				value: validation.msg,
+			})(dispatch)
+		}
+
+		if (isValid) {
+			const account = selectAccount(state)
+			isValid = await handleWalletFeesData({
+				stepsId,
+				validateId,
+				dirty,
+				actionName: 'validatePrivilegesChange',
+				feeDataAction: () =>
+					walletSetIdentityPrivilege({
+						privLevel,
+						setAddr,
+						getFeesOnly: true,
+						account,
+					}),
+			})(dispatch, getState)
+		}
+
+		await handleAfterValidation({ isValid, onValid, onInvalid })
+
+		await updateSpinner(validateId, false)(dispatch)
+	}
+}
+
+export function walletUpdateIdentityPrivilege({ setAddr, privLevel }) {
+	return async function(dispatch, getState) {
+		try {
+			const account = selectAccount(getState())
+			const result = await walletSetIdentityPrivilege({
+				account,
+				setAddr,
+				privLevel,
+			})
+			addToast({
+				type: 'accept',
+				label: t('IDENTITY_SET_ADDR_PRIV_NOTIFICATION', {
+					args: [result],
+				}),
+				timeout: 20000,
+			})
+		} catch (err) {
+			console.error('ERR_IDENTITY_SET_ADDR_PRIV_NOTIFICATION', err)
+			addToast({
+				type: 'cancel',
+				label: t('ERR_IDENTITY_SET_ADDR_PRIV_NOTIFICATION', {
+					args: [getErrorMsg(err)],
+				}),
+				timeout: 20000,
 			})(dispatch)
 		}
 	}
