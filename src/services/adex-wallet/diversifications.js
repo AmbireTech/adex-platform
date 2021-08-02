ad67b570687f9a60075fbd4af32371f8912bd359
IvoPaunov <ivo.paunov@gmail.com>
2021-08-03 00:05:08 +0300

    add moainnet diversification presets

--- src/services/adex-wallet/diversifications.js
+++ src/services/adex-wallet/diversifications.js
@@ -1,6 +1,52 @@
 import { tokens } from 'services/adex-wallet/assets'
 
-export const diversificationPresets = [
+export const mainnet = [
+	{
+		label: 'DIV_PRESET_CONSERVATIVE',
+		assets: [
+			{
+				address: tokens.USDC,
+				share: 70,
+			},
+			{
+				address: tokens.WBTC,
+				share: 30,
+			},
+		],
+	},
+	{
+		label: 'DIV_PRESET_DEGEN',
+		assets: [
+			{
+				address: tokens.USDC,
+				share: 50,
+			},
+			{
+				address: tokens.WBTC,
+				share: 25,
+			},
+			{
+				address: tokens.WETH,
+				share: 25,
+			},
+		],
+	},
+	{
+		label: 'DIV_PRESET_RISKY',
+		assets: [
+			{
+				address: tokens.WETH,
+				share: 69,
+			},
+			{
+				address: tokens.WBTC,
+				share: 31,
+			},
+		],
+	},
+]
+
+export const kovan = [
 	{
 		label: 'DIV_PRESET_CONSERVATIVE',
 		assets: [
@@ -61,3 +107,6 @@ export const diversificationPresets = [
 		],
 	},
 ]
+
+export const diversificationPresets =
+	process.env.NODE_ENV === 'production' ? mainnet : kovan
