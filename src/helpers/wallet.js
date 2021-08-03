3af558bb4948a7e4534df6d8f64a4b33a0f542dc
IvoPaunov <ivo.paunov@gmail.com>
2021-08-03 16:40:27 +0300

    add wallet helpers file

---
+++ src/helpers/wallet.js
@@ -0,0 +1,10 @@
+export const getMainCurrencyValue = ({
+	asset,
+	floatAmount,
+	prices,
+	mainCurrency = '',
+}) => {
+	const price = (prices[asset] || {})[mainCurrency.id || mainCurrency] || 0
+	const value = parseFloat(floatAmount) * price
+	return value.toFixed(2)
+}
