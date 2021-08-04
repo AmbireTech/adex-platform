896a9dcfdbe2d2849a9d828f329212c5a017b576
IvoPaunov <ivo.paunov@gmail.com>
2021-08-04 15:02:08 +0300

    update assets cfg

--- src/services/adex-wallet/assets.js
+++ src/services/adex-wallet/assets.js
@@ -1,5 +1,12 @@
 import { assetsKovan } from './assets-kovan'
 import { assetsMainnet } from './assets-mainnet'
+import ADX_WALLET_LOGO from 'resources/wallet/logo.png'
 
-export const { assets, tokens, mappers, getLogo } =
+export const { assets, tokens, mappers, logos } =
 	process.env.NODE_ENV === 'production' ? assetsMainnet : assetsKovan
+
+export const getLogo = addressOrSymbol => {
+	return (
+		logos[addressOrSymbol] || logos[tokens[addressOrSymbol]] || ADX_WALLET_LOGO
+	)
+}
