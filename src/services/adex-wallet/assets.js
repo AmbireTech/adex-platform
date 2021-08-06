b355fc45ba5619a23f082cad92733dcb192f0205
IvoPaunov <ivo.paunov@gmail.com>
2021-08-06 17:32:14 +0300

    update getLogo func and, add ETH_TOKENS  const

--- src/services/adex-wallet/assets.js
+++ src/services/adex-wallet/assets.js
@@ -2,11 +2,17 @@ import { assetsKovan } from './assets-kovan'
 import { assetsMainnet } from './assets-mainnet'
 import ADX_WALLET_LOGO from 'resources/wallet/logo.png'
 
+export const ETH_TOKENS = ['ETH', 'WETH', 'aETH', 'aWETH']
+
 export const { assets, tokens, mappers, logos } =
 	process.env.NODE_ENV === 'production' ? assetsMainnet : assetsKovan
 
 export const getLogo = addressOrSymbol => {
 	return (
-		logos[addressOrSymbol] || logos[tokens[addressOrSymbol]] || ADX_WALLET_LOGO
+		logos[addressOrSymbol] ||
+		logos[tokens[addressOrSymbol]] ||
+		logos['a' + addressOrSymbol] ||
+		logos[tokens['a' + addressOrSymbol]] ||
+		ADX_WALLET_LOGO
 	)
 }
