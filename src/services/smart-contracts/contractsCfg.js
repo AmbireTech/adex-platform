783b96bff6df8bfc17881ab3b70d6aa106a0253b
IvoPaunov <ivo.paunov@gmail.com>
2021-10-04 17:00:00 +0300

    remove addreses from env config ar contacts cfg

--- src/services/smart-contracts/contractsCfg.js
+++ src/services/smart-contracts/contractsCfg.js
@@ -24,75 +24,75 @@ export const contracts = {
 		abi: IdentityPayable,
 	},
 	AdExENSManager: {
-		address: process.env.ADEX_ENS_ADDR,
+		// address: process.env.ADEX_ENS_ADDR,
 		publicResolver: process.env.REVERSE_REGISTRAR_PUBLIC_RESOLVER,
 		abi: AdExENSManager,
 	},
 	ReverseRegistrar: {
-		address: process.env.REVERSE_REGISTRAR_ADDR,
+		// address: process.env.REVERSE_REGISTRAR_ADDR,
 		parentDomain: process.env.REVERSE_REGISTRAR_PARENT,
 		abi: ReverseRegistrar,
 	},
 	AdExCore: {
-		address: process.env.ADEX_CORE_ADDR,
+		// address: process.env.ADEX_CORE_ADDR,
 		abi: AdExCore.abi,
 	},
 	DAI: {
-		address: process.env.DAI_TOKEN_ADDR,
+		// address: process.env.DAI_TOKEN_ADDR,
 		abi: Dai.abi,
 		decimals: 18,
 	},
 	IdentityFactory: {
-		address: process.env.IDENTITY_FACTORY_ADDR,
+		// address: process.env.IDENTITY_FACTORY_ADDR,
 		abi: IdentityFactory,
 	},
 	ERC20: {
 		abi: ERC20,
 	},
 	ADXLoyaltyPoolToken: {
-		address: process.env.ADDR_ADX_LOYALTY_POOL_TOKEN_ADDR,
+		// address: process.env.ADDR_ADX_LOYALTY_POOL_TOKEN_ADDR,
 		abi: ADXLoyaltyPoolTokenABI,
 		decimalsMultiplier: 1e18,
 		symbol: 'ADX-LOYALTY',
 		decimals: 18,
 	},
 	ADXToken: {
-		address: process.env.ADX_TOKEN_ADDR,
+		// address: process.env.ADX_TOKEN_ADDR,
 		abi: ERC20,
 		symbol: 'ADX',
 		decimals: 18,
 	},
 	StakingPool: {
-		address: process.env.ADDR_ADX_STAKING_POOL_TOKEN_ADDR,
+		// address: process.env.ADDR_ADX_STAKING_POOL_TOKEN_ADDR,
 		abi: StakingPoolABI,
 		decimalsMultiplier: 1e18,
 		symbol: 'ADX-STAKING',
 		decimals: 18,
 	},
 	WalletZapper: {
-		address: process.env.ADDR_WALLET_ZAPPER,
+		// address: process.env.ADDR_WALLET_ZAPPER,
 		abi: WalletZapperAbi,
 	},
 	UniSwapRouterV2: {
-		address: process.env.UNISWAP_ROUTER_V2,
+		// address: process.env.UNISWAP_ROUTER_V2,
 		abi: UniSwapRouterV2Abi,
 	},
 	UniSwapRouterV3: {
-		address: process.env.UNISWAP_ROUTER_V3,
+		// address: process.env.UNISWAP_ROUTER_V3,
 		abi: UniSwapRouterV3Abi,
 	},
 	UniSwapQuoterV3: {
-		address: process.env.UNISWAP_QUOTER_V3,
+		// address: process.env.UNISWAP_QUOTER_V3,
 		// Abi modified
 		// https://twitter.com/dcfgod/status/1405608315011411970?s=20
 		abi: UniSwapQuoterV3Abi,
 	},
 	AaveLendingPool: {
-		address: process.env.AAVE_LENDING_POOL_ADDR,
+		// address: process.env.AAVE_LENDING_POOL_ADDR,
 		abi: AAVELendingPoolAbi,
 	},
 	WETH: {
-		address: process.env.WETH_ADDR,
+		// address: process.env.WETH_ADDR,
 		abi: WETHAbi,
 	},
 }
