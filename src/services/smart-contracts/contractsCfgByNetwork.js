828b68a9ab44b4d839528adf2e2e35eec80543a3
IvoPaunov <ivo.paunov@gmail.com>
2021-10-05 16:03:01 +0300

    add contracts addresses by network cfg

---
+++ src/services/smart-contracts/contractsCfgByNetwork.js
@@ -0,0 +1,27 @@
+export const contractsAddressesByNetwork = {
+	ethereum: {
+		swapRouterV2: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+		swapRouterV3: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
+		quoterV3: '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6',
+		walletZapper: '0x54CFeE41A9760134aFd99d33443b6dF8088A9317',
+		aaveLendingPoll: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
+		wrappedCurrency: {
+			symbol: 'WETH',
+			address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+		},
+	},
+	// TODO:  get polygon addresses
+	polygon: {
+		swapRouterV2: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+		swapRouterV3: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
+		quoterV3: '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6',
+		walletZapper: '0x54CFeE41A9760134aFd99d33443b6dF8088A9317',
+		aaveLendingPoll: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
+		wrappedCurrency: {
+			symbol: 'WETH',
+			address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+		},
+	},
+	fantom: {},
+	bsc: {},
+}
