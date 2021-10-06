7a4fc60f51b952ef12f5dc42cc959c6dd4c4acfe
IvoPaunov <ivo.paunov@gmail.com>
2021-10-06 17:38:48 +0300

    update polygon contracts addresses

--- src/services/smart-contracts/contractsCfgByNetwork.js
+++ src/services/smart-contracts/contractsCfgByNetwork.js
@@ -12,11 +12,11 @@ export const contractsAddressesByNetwork = {
 	},
 	// TODO:  get polygon addresses
 	polygon: {
-		swapRouterV2: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
-		swapRouterV3: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-		quoterV3: '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6',
+		swapRouterV2: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',
+		swapRouterV3: null,
+		quoterV3: null,
 		walletZapper: '0x54CFeE41A9760134aFd99d33443b6dF8088A9317',
-		aaveLendingPoll: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
+		aaveLendingPoll: '0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fc',
 		wrappedCurrency: {
 			symbol: 'WETH',
 			address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
