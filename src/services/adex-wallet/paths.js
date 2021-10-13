436019d28712c5a68c741bb4a7668f9b855b905a
IvoPaunov <ivo.paunov@gmail.com>
2021-10-13 16:56:41 +0300

    update paths and init polygon paths cfg

--- src/services/adex-wallet/paths.js
+++ src/services/adex-wallet/paths.js
@@ -1,5 +1,26 @@
 import { kovanPathsData } from './paths-kovan'
 import { mainnetPathsData } from './paths-mainnet'
+import { polygonPathsData } from './paths-polygon'
+import { selectNetwork } from 'selectors'
 
-export const { getPath, TICK_SPACINGS, FeeAmount, uniswapRouters } =
-	process.env.NODE_ENV === 'production' ? mainnetPathsData : kovanPathsData
+const pathsByNetwork = {
+	kovan: kovanPathsData,
+	ethereum: mainnetPathsData,
+	polygon: polygonPathsData,
+}
+
+export const getPath = args => {
+	const { id } = selectNetwork()
+	if (!id) {
+		throw new Error('getPath - network id not provided')
+	}
+	const getPathFn = pathsByNetwork[id].getPath
+	return getPathFn(args)
+}
+
+export const {
+	//  getPath,
+	TICK_SPACINGS,
+	FeeAmount,
+	uniswapRouters,
+} = process.env.NODE_ENV === 'production' ? mainnetPathsData : kovanPathsData
