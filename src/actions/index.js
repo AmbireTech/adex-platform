9b9d753530a5a7e7a53c7e889bf94d1c480907db
IvoPaunov <ivo.paunov@gmail.com>
2021-10-26 18:05:35 +0300

    delete platform related actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,9 @@ import * as common from './common'
 import * as projectActions from './projectActions'
 import * as uiActions from './uiActions'
 import * as validationActions from './validationActions'
-import * as mediaValidations from './mediaValidations'
-import * as itemActions from './itemActions'
 import * as accountActions from './accountActions'
 import * as identityActions from './identityActions'
-import * as bidActions from './bidActions'
 import * as transactionActions from './transactionActions'
-import * as analyticsActions from './analyticsActions'
-import * as newItemsActions from './newItemsActions'
-import * as audienceActions from './audienceActions'
-import * as campaignActions from './campaignActions'
-import * as slotActions from './slotActions'
-import * as unitActions from './unitActions'
 import * as walletActions from './wallet/walletActions'
 import * as walletValidationsActions from './wallet/walletActions'
 
@@ -24,18 +15,9 @@ export default {
 	...projectActions,
 	...uiActions,
 	...validationActions,
-	...mediaValidations,
-	...itemActions,
 	...accountActions,
 	...identityActions,
-	...bidActions,
 	...transactionActions,
-	...analyticsActions,
-	...newItemsActions,
-	...audienceActions,
-	...campaignActions,
-	...slotActions,
-	...unitActions,
 	...walletActions,
 	...walletValidationsActions,
 }
@@ -46,18 +28,9 @@ export default {
 export * from './common'
 export * from './projectActions'
 export * from './validationActions'
-export * from './mediaValidations'
-export * from './uiActions'
 export * from './itemActions'
 export * from './accountActions'
 export * from './identityActions'
-export * from './bidActions'
 export * from './transactionActions'
-export * from './analyticsActions'
-export * from './newItemsActions'
-export * from './audienceActions'
-export * from './campaignActions'
-export * from './slotActions'
-export * from './unitActions'
 export * from './wallet/walletActions'
 export * from './walletValidationsActions'
