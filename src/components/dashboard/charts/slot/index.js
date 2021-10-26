5e17a0d8d40f2050c7555865073e711ec8bb25b3
IvoPaunov <ivo.paunov@gmail.com>
2021-10-26 17:57:15 +0300

    dalete platform charts

--- src/components/dashboard/charts/slot/index.js
+++
@@ -1,5 +0,0 @@
-import BidsStatusPie from './bidsStatus'
-import { BidsTimeStatistics } from './bidsStatistics'
-import BidsLegend from './bidsLegend'
-
-export { BidsStatusPie, BidsTimeStatistics, BidsLegend }
