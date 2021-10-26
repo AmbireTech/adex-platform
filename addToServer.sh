328e0ff91a24deb8ee57d453905c7474ec8a6765
IvoPaunov <ivo.paunov@gmail.com>
2021-10-26 18:01:35 +0300

    delete old scripts

--- addToServer.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-USERNAME=$1
-HOSTNAME=$2
-
-npm run-script build
-
-rsync -av ./build ${USERNAME}@${HOSTNAME}:/var/lib/nginx/adex-dapp/
-
-ssh ${USERNAME}@${HOSTNAME} "chown -R www-data:www-data /var/lib/nginx/adex-dapp/build"
--- buildOnServer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-npm run-script build
-cp -r ./build /var/lib/nginx/adex-dapp/
-chown -R www-data:www-data /var/lib/nginx/adex-dapp/build
