42e0cf64251c1e199534ef869ca435c9aa2b65d4
IvoPaunov <ivo.paunov@gmail.com>
2021-10-26 17:00:24 +0300

    delete market, validator, demo and gas services

--- src/services/gas/actions.js
+++
@@ -1,8 +0,0 @@
-import { getEthersReadOnly } from 'services/smart-contracts/ethers'
-import { utils } from 'ethers'
-
-export const getGasPrice = async (format = 'wei') => {
-	const { provider } = await getEthersReadOnly()
-	const gasPrice = await provider.getGasPrice()
-	return utils.formatUnits(gasPrice.toNumber(), format)
-}
