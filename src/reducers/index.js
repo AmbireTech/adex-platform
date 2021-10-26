ce6b0c2c073f13a45e17d18f77664886db85d0fa
IvoPaunov <ivo.paunov@gmail.com>
2021-10-26 18:19:59 +0300

    delete platform related reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,6 @@ import account from './accountReducer'
 import signin from './signinReducer'
 import identity from './identityReducer'
 import wallet from './walletReducer'
-import items from './itemsReducer'
-import newItem from './newItemsReducer'
-// import currentItem from './currentItemsReducer'
 import spinners from './spinnersReducer'
 import ui from './uiReducer'
 import uiMemory from './uiMemoryReducer'
@@ -15,12 +12,7 @@ import nav from './navReducer'
 import language from './languageReducer'
 import validations from './validateItemsReducer'
 import newTransactions from './newTransactionsReducer'
-import web3Transactions from './web3TransactionsReducer'
-import targeting from './targetingReducer'
 import config from './configReducer'
-import analytics from './analyticsReducer'
-import channels from './channelsReducer'
-import selectedItems from './selectedItemsReducer'
 import ensAddresses from './ensReducer'
 import tablesState from './tablesStateReducer'
 import network from './networkReducer'
@@ -31,10 +23,7 @@ export const persistReducers = {
 	project,
 	account,
 	ui, //: filterActions(ui, [types.UPDATE_UI]),
-	items: filterActions(items, action => action.type.match(/_ITEM/)),
 	language,
-	web3Transactions,
-	targeting,
 	config,
 	ensAddresses,
 	tablesState,
@@ -43,17 +32,13 @@ export const persistReducers = {
 
 export const memoryReducers = {
 	uiMemory,
-	channels,
-	analytics,
 	signin,
 	identity,
 	wallet,
-	newItem: filterActions(newItem, action => action.type.match(/_NEWITEM/)),
 	spinners: filterActions(spinners, [types.UPDATE_SPINNER]),
 	toasts,
 	confirm: filterActions(confirm, [types.CONFIRM_ACTION]),
 	nav: filterActions(nav, [types.UPDATE_NAV]),
 	validations,
-	selectedItems,
 	newTransactions,
 }
