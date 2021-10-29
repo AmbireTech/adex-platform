76183c6dc5e99f7f4a85934ceeeca361488ef874
IvoPaunov <ivo.paunov@gmail.com>
2021-10-29 16:39:52 +0300

    cleanup selectors

--- src/selectors/configSelectors.js
+++ src/selectors/configSelectors.js
@@ -29,94 +29,19 @@ export const selectNetworkByChainId = createCachedSelector(
 )((_state, chainId) => chainId)
 
 // - relayer cfg
-// network,
-// feeTokenWhitelist:
-// [
-// 	{
-// 		address,
-// 		symbol,
-// 		decimals,
-// 		standard,
-// 		min,
-// 		minDeploy,
-// 		minRecommended,
-// 		executeBaseFee: {
-// 			WITHDRAW: '4200000000000000000',
-// 			PRIVILEGES_CHANGE: '1000000000000000000',
-// 			ENS_CHANGE: '1000000000000000000',
-// 			DEFAULT: '1000000000000000000',
-// 		},
-// 	},
-// ],
-// routineWithdrawTokens, // [{ address, symbol, decimals, standard, minWeekly, minFinal, minPlatform }],
-// coreAddr,
-// baseIdentityAddr,
-// identityFactoryAddr,
-// registryAddr,
-// daiAddr,
-// marketUrl,
-// identityRecoveryAddr,
-// weeklyFeeAmount,
-// mainToken, // { address, symbol, decimals, standard },
-// advertiserMinGrantAmount
-// relayerAddr
-// gasPriceCap
-// gasPriceRatio
 
-// walletCfg: {
-// 	feeTokens: cfg.feeTokens,
-// 	feeCollector: cfg.feeCollector,
-// 	whitelistedFactories: cfg.whitelistedFactories,
-// 	whitelistedBaseIdentities: cfg.whitelistedBaseIdentities,
-// 	marginGas: cfg.marginGas,
-// 	gasMultipliers: cfg.gasMultipliers,
-// 	networks: cfg.networks,
-// 	pricesUpdateIntervalSeconds: cfg.pricesUpdateIntervalSeconds
-
-// }
+// networks: cfg.networks,
+// feeTokens: cfg.feeTokens,
+// whitelistedFactories: cfg.whitelistedFactories,
+// whitelistedBaseIdentities: cfg.whitelistedBaseIdentities,
+// // @TODO
+// // relayerAddr: wallet.address,
+// protocolVersion: version,
+// feeCollector: cfg.feeCollector
 
 export const selectFeeTokens = createSelector(
 	[selectRelayerConfig, selectNetwork],
 	({ feeTokens }, { id }) => feeTokens.filter(x => x.network === id)
-)
-
-export const selectFeeTokenWhitelist = createSelector(
-	[selectRelayerConfig],
-	({ feeTokenWhitelist }) => {
-		return feeTokenWhitelist.reduce((all, t) => {
-			const tokens = { ...all }
-			tokens[t.address] = t
-
-			return tokens
-		}, {})
-	}
-)
-
-export const selectRoutineWithdrawTokens = createSelector(
-	[selectRelayerConfig],
-	({ routineWithdrawTokens }) => {
-		return routineWithdrawTokens.reduce((all, t) => {
-			const tokens = { ...all }
-			tokens[t.address] = t
-
-			return tokens
-		}, {})
-	}
-)
-
-export const selectRoutineWithdrawTokenByAddress = createCachedSelector(
-	selectRelayerConfig,
-	(_, address) => address,
-	({ routineWithdrawTokens }, address) => {
-		return routineWithdrawTokens.find(token => token.address === address)
-	}
-)((_state, address = '-') => address)
-
-export const selectRoutineWithdrawTokensAddresses = createSelector(
-	[selectRelayerConfig],
-	({ routineWithdrawTokens }) => {
-		return routineWithdrawTokens.map(t => t.address)
-	}
 )
 
 export const selectConfig = createSelector(
@@ -132,21 +57,4 @@ export const selectMainToken = createSelector(
 export const selectIdentityRecoveryAddr = createSelector(
 	[selectRelayerConfig],
 	({ identityRecoveryAddr }) => identityRecoveryAddr
-)
-
-export const selectGasPriceCap = createSelector(
-	[selectRelayerConfig],
-	({ gasPriceCap }) => gasPriceCap
-)
-
-export const selectSaiToken = createSelector(
-	[selectRelayerConfig, selectRoutineWithdrawTokens],
-	({ saiAddr }, routineWithdrawTokens) => routineWithdrawTokens[saiAddr]
-)
-
-export const selectMainFeeToken = createSelector(
-	[selectRelayerConfig, selectFeeTokenWhitelist],
-	({ mainToken }, feeTokenWhitelist) => {
-		return feeTokenWhitelist[mainToken.address]
-	}
 )
